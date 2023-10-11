40b41be05485adcef24b9a91fb854bf799cabc23
Kai Davenport <kaiyadavenport@gmail.com>
2023-10-11 22:23:27 +0100

    move keycloak stuff into it's own file

---
+++ api/pkg/server/keycloak.go
@@ -0,0 +1,108 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"strings"
+
+	gocloak "github.com/Nerzal/gocloak/v13"
+	jwt "github.com/golang-jwt/jwt/v4"
+)
+
+const CLIENT_ID = "api"
+const REALM = "lilypad"
+
+type keycloak struct {
+	gocloak      *gocloak.GoCloak // keycloak client
+	externalUrl  string           // the URL of the keycloak server
+	clientId     string           // clientId specified in Keycloak
+	clientSecret string           // client secret specified in Keycloak
+	realm        string           // realm specified in Keycloak
+}
+
+func newKeycloak(options ServerOptions) *keycloak {
+	externalUrl := options.KeyCloakURL
+	keycloak := &keycloak{
+		gocloak:      gocloak.NewClient(externalUrl),
+		externalUrl:  externalUrl,
+		clientId:     CLIENT_ID,
+		clientSecret: options.KeyCloakToken,
+		realm:        REALM,
+	}
+	return keycloak
+}
+
+type keyCloakMiddleware struct {
+	keycloak *keycloak
+	options  ServerOptions
+}
+
+func newMiddleware(keycloak *keycloak, options ServerOptions) *keyCloakMiddleware {
+	return &keyCloakMiddleware{keycloak: keycloak, options: options}
+}
+
+func extractBearerToken(token string) string {
+	return strings.Replace(token, "Bearer ", "", 1)
+}
+
+func (auth *keyCloakMiddleware) userFromRequest(r *http.Request) (*jwt.Token, error) {
+	// try to extract Authorization parameter from the HTTP header
+	token := r.Header.Get("Authorization")
+	if token == "" {
+		return nil, fmt.Errorf("authorization header missing")
+	}
+
+	// extract Bearer token
+	token = extractBearerToken(token)
+	if token == "" {
+		return nil, fmt.Errorf("bearer token missing")
+	}
+
+	result, err := auth.keycloak.gocloak.RetrospectToken(r.Context(), token, CLIENT_ID, auth.options.KeyCloakToken, REALM)
+	if err != nil {
+		return nil, fmt.Errorf("RetrospectToken: invalid or malformed token: %s", err.Error())
+	}
+
+	j, _, err := auth.keycloak.gocloak.DecodeAccessToken(r.Context(), token, REALM)
+	if err != nil {
+		return nil, fmt.Errorf("DecodeAccessToken: invalid or malformed token: %s", err.Error())
+	}
+
+	// check if the token isn't expired and valid
+	if !*result.Active {
+		return nil, fmt.Errorf("invalid or expired token")
+	}
+
+	return j, nil
+}
+
+func getUserIdFromJWT(tok *jwt.Token) string {
+	mc := tok.Claims.(jwt.MapClaims)
+	uid := mc["sub"].(string)
+	return uid
+}
+
+func setRequestUser(ctx context.Context, userid string) context.Context {
+	return context.WithValue(ctx, "userid", userid)
+}
+
+func getRequestUser(req *http.Request) string {
+	// return the "userid" value from the request context
+	return req.Context().Value("userid").(string)
+}
+
+func (auth *keyCloakMiddleware) verifyToken(next http.Handler) http.Handler {
+
+	f := func(w http.ResponseWriter, r *http.Request) {
+		token, err := auth.userFromRequest(r)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusUnauthorized)
+			return
+		}
+		r = r.WithContext(setRequestUser(r.Context(), getUserIdFromJWT(token)))
+		next.ServeHTTP(w, r)
+	}
+
+	return http.HandlerFunc(f)
+}
--- api/pkg/server/server.go
+++ api/pkg/server/server.go
@@ -4,14 +4,11 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"strings"
 	"time"
 
-	gocloak "github.com/Nerzal/gocloak/v13"
 	"github.com/bacalhau-project/lilysaas/api/pkg/controller"
 	"github.com/bacalhau-project/lilysaas/api/pkg/store"
 	"github.com/bacalhau-project/lilysaas/api/pkg/system"
-	jwt "github.com/golang-jwt/jwt/v4"
 	"github.com/gorilla/mux"
 )
 
@@ -57,103 +54,6 @@ func NewServer(
 	}, nil
 }
 
-type keycloak struct {
-	gocloak      *gocloak.GoCloak // keycloak client
-	externalUrl  string           // the URL of the keycloak server
-	clientId     string           // clientId specified in Keycloak
-	clientSecret string           // client secret specified in Keycloak
-	realm        string           // realm specified in Keycloak
-}
-
-const CLIENT_ID = "api"
-const REALM = "lilypad"
-
-func (apiServer *LilysaasAPIServer) newKeycloak() *keycloak {
-	externalUrl := apiServer.Options.KeyCloakURL
-	keycloak := &keycloak{
-		gocloak:      gocloak.NewClient(externalUrl),
-		externalUrl:  externalUrl,
-		clientId:     CLIENT_ID,
-		clientSecret: apiServer.Options.KeyCloakToken,
-		realm:        REALM,
-	}
-	return keycloak
-}
-
-type keyCloakMiddleware struct {
-	keycloak *keycloak
-	options  ServerOptions
-}
-
-func newMiddleware(keycloak *keycloak, options ServerOptions) *keyCloakMiddleware {
-	return &keyCloakMiddleware{keycloak: keycloak, options: options}
-}
-
-func extractBearerToken(token string) string {
-	return strings.Replace(token, "Bearer ", "", 1)
-}
-
-func (auth *keyCloakMiddleware) userFromRequest(r *http.Request) (*jwt.Token, error) {
-	// try to extract Authorization parameter from the HTTP header
-	token := r.Header.Get("Authorization")
-	if token == "" {
-		return nil, fmt.Errorf("authorization header missing")
-	}
-
-	// extract Bearer token
-	token = extractBearerToken(token)
-	if token == "" {
-		return nil, fmt.Errorf("bearer token missing")
-	}
-
-	result, err := auth.keycloak.gocloak.RetrospectToken(r.Context(), token, CLIENT_ID, auth.options.KeyCloakToken, REALM)
-	if err != nil {
-		return nil, fmt.Errorf("RetrospectToken: invalid or malformed token: %s", err.Error())
-	}
-
-	j, _, err := auth.keycloak.gocloak.DecodeAccessToken(r.Context(), token, REALM)
-	if err != nil {
-		return nil, fmt.Errorf("DecodeAccessToken: invalid or malformed token: %s", err.Error())
-	}
-
-	// check if the token isn't expired and valid
-	if !*result.Active {
-		return nil, fmt.Errorf("invalid or expired token")
-	}
-
-	return j, nil
-}
-
-func getUserIdFromJWT(tok *jwt.Token) string {
-	mc := tok.Claims.(jwt.MapClaims)
-	uid := mc["sub"].(string)
-	return uid
-}
-
-func setRequestUser(ctx context.Context, userid string) context.Context {
-	return context.WithValue(ctx, "userid", userid)
-}
-
-func getRequestUser(req *http.Request) string {
-	// return the "userid" value from the request context
-	return req.Context().Value("userid").(string)
-}
-
-func (auth *keyCloakMiddleware) verifyToken(next http.Handler) http.Handler {
-
-	f := func(w http.ResponseWriter, r *http.Request) {
-		token, err := auth.userFromRequest(r)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusUnauthorized)
-			return
-		}
-		r = r.WithContext(setRequestUser(r.Context(), getUserIdFromJWT(token)))
-		next.ServeHTTP(w, r)
-	}
-
-	return http.HandlerFunc(f)
-}
-
 func (apiServer *LilysaasAPIServer) ListenAndServe(ctx context.Context, cm *system.CleanupManager) error {
 	router := mux.NewRouter()
 	router.Use(apiServer.corsMiddleware)
@@ -165,7 +65,7 @@ func (apiServer *LilysaasAPIServer) ListenAndServe(ctx context.Context, cm *syst
 		return true
 	}).Subrouter()
 
-	kc := apiServer.newKeycloak()
+	kc := newKeycloak(apiServer.Options)
 	mdw := newMiddleware(kc, apiServer.Options)
 	authRouter.Use(mdw.verifyToken)
 
