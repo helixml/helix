3530dcc25229d75356dd61ebe20be6e3ed0c1d2c
Kai Davenport <kaiyadavenport@gmail.com>
2023-10-11 14:15:25 +0100

    add new tables and db methods for jobs

---
+++ api/pkg/store/migrations/0002_balance_transfer.up.sql
@@ -0,0 +1,10 @@
+create table balance_transfer (
+  id varchar(255) PRIMARY KEY,
+  created timestamp default current_timestamp,
+  owner varchar(255) NOT NULL,
+  owner_type varchar(255) NOT NULL,
+  payment_type varchar(255) NOT NULL,
+  amount int NOT NULL,
+  -- this is the JSON representation of the job data
+  data json not null
+);
