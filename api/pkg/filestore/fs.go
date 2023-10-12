0a04badd63906c86f8332d0c9abd4680860946f7
Kai Davenport <kaiyadavenport@gmail.com>
2023-10-12 16:29:12 +0100

    add filestore interface, gcs and local fs implementations and wrap the paths with the controller

---
+++ api/pkg/filestore/fs.go
@@ -0,0 +1,105 @@
+package filestore
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+type FileSystemStorage struct {
+	basePath string
+	baseURL  string
+}
+
+func NewFileSystemStorage(basePath string, baseURL string) *FileSystemStorage {
+	return &FileSystemStorage{
+		basePath: basePath,
+		baseURL:  baseURL,
+	}
+}
+
+func (s *FileSystemStorage) List(ctx context.Context, prefix string) ([]FileStoreItem, error) {
+	fullPath := filepath.Join(s.basePath, prefix)
+	files, err := os.ReadDir(fullPath)
+	if err != nil {
+		return nil, fmt.Errorf("error reading directory: %w", err)
+	}
+
+	var items []FileStoreItem
+	for _, f := range files {
+		path := filepath.Join(prefix, f.Name())
+		items = append(items, FileStoreItem{
+			Directory: f.IsDir(),
+			Name:      f.Name(),
+			Path:      path,
+			URL:       fmt.Sprintf("%s/%s", s.baseURL, path),
+		})
+	}
+
+	return items, nil
+}
+
+func (s *FileSystemStorage) Get(ctx context.Context, path string) (FileStoreItem, error) {
+	fullPath := filepath.Join(s.basePath, path)
+	info, err := os.Stat(fullPath)
+	if err != nil {
+		return FileStoreItem{}, fmt.Errorf("error fetching file info: %w", err)
+	}
+	return FileStoreItem{
+		Directory: info.IsDir(),
+		Name:      info.Name(),
+		Path:      path,
+		URL:       fmt.Sprintf("%s/%s", s.baseURL, path),
+	}, nil
+}
+
+func (s *FileSystemStorage) Upload(ctx context.Context, path string, r io.Reader) (FileStoreItem, error) {
+	fullPath := filepath.Join(s.basePath, path)
+	file, err := os.Create(fullPath)
+	if err != nil {
+		return FileStoreItem{}, fmt.Errorf("error creating file: %w", err)
+	}
+	defer file.Close()
+
+	if _, err := io.Copy(file, r); err != nil {
+		return FileStoreItem{}, fmt.Errorf("failed to copy content to file: %w", err)
+	}
+
+	return s.Get(ctx, path)
+}
+
+func (s *FileSystemStorage) Rename(ctx context.Context, path string, newPath string) (FileStoreItem, error) {
+	src := filepath.Join(s.basePath, path)
+	dst := filepath.Join(s.basePath, newPath)
+
+	if err := os.Rename(src, dst); err != nil {
+		return FileStoreItem{}, fmt.Errorf("failed to rename file or directory: %w", err)
+	}
+
+	return s.Get(ctx, newPath)
+}
+
+func (s *FileSystemStorage) Delete(ctx context.Context, path string) error {
+	fullPath := filepath.Join(s.basePath, path)
+
+	if err := os.RemoveAll(fullPath); err != nil {
+		return fmt.Errorf("failed to delete file or directory: %w", err)
+	}
+
+	return nil
+}
+
+func (s *FileSystemStorage) CreateFolder(ctx context.Context, path string) (FileStoreItem, error) {
+	fullPath := filepath.Join(s.basePath, path)
+
+	if err := os.MkdirAll(fullPath, os.ModePerm); err != nil {
+		return FileStoreItem{}, fmt.Errorf("failed to create folder: %w", err)
+	}
+
+	return s.Get(ctx, path)
+}
+
+// Compile-time interface check:
+var _ FileStore = (*FileSystemStorage)(nil)
---
+++ api/pkg/filestore/gcs.go
@@ -0,0 +1,167 @@
+package filestore
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"strings"
+
+	"cloud.google.com/go/storage"
+	"google.golang.org/api/iterator"
+	"google.golang.org/api/option"
+)
+
+type GCSStorage struct {
+	client *storage.Client
+	bucket *storage.BucketHandle
+}
+
+func NewGCSStorage(ctx context.Context, bucketName, serviceAccountKeyFile string) (*GCSStorage, error) {
+	client, err := storage.NewClient(ctx, option.WithCredentialsFile(serviceAccountKeyFile))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create storage client: %w", err)
+	}
+
+	return &GCSStorage{
+		client: client,
+		bucket: client.Bucket(bucketName),
+	}, nil
+}
+
+func (s *GCSStorage) List(ctx context.Context, prefix string) ([]FileStoreItem, error) {
+	it := s.bucket.Objects(ctx, &storage.Query{Prefix: prefix})
+	var items []FileStoreItem
+
+	for {
+		attrs, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return nil, fmt.Errorf("error iterating over GCS objects: %w", err)
+		}
+
+		item := FileStoreItem{
+			Directory: strings.HasSuffix(attrs.Name, "/"),
+			Name:      attrs.Name,
+			Path:      attrs.Name,
+			URL:       attrs.MediaLink,
+		}
+		items = append(items, item)
+	}
+
+	return items, nil
+}
+
+func (s *GCSStorage) Get(ctx context.Context, path string) (FileStoreItem, error) {
+	attrs, err := s.bucket.Object(path).Attrs(ctx)
+	if err != nil {
+		return FileStoreItem{}, fmt.Errorf("error fetching GCS object attributes: %w", err)
+	}
+
+	return FileStoreItem{
+		Directory: strings.HasSuffix(attrs.Name, "/"),
+		Name:      attrs.Name,
+		Path:      attrs.Name,
+		URL:       attrs.MediaLink,
+	}, nil
+}
+
+func (s *GCSStorage) Upload(ctx context.Context, path string, r io.Reader) (FileStoreItem, error) {
+	obj := s.bucket.Object(path)
+	writer := obj.NewWriter(ctx)
+	if _, err := io.Copy(writer, r); err != nil {
+		return FileStoreItem{}, fmt.Errorf("failed to copy content to GCS: %w", err)
+	}
+	if err := writer.Close(); err != nil {
+		return FileStoreItem{}, fmt.Errorf("failed to finalize GCS object upload: %w", err)
+	}
+	attrs, err := obj.Attrs(ctx)
+	if err != nil {
+		return FileStoreItem{}, fmt.Errorf("error fetching GCS object attributes after upload: %w", err)
+	}
+	return FileStoreItem{
+		Directory: strings.HasSuffix(attrs.Name, "/"),
+		Name:      attrs.Name,
+		Path:      attrs.Name,
+		URL:       attrs.MediaLink,
+	}, nil
+}
+
+func (s *GCSStorage) Rename(ctx context.Context, path string, newPath string) (FileStoreItem, error) {
+	src := s.bucket.Object(path)
+	dst := s.bucket.Object(newPath)
+
+	// For directories, iterate over each item and copy then delete
+	if strings.HasSuffix(path, "/") {
+		it := s.bucket.Objects(ctx, &storage.Query{Prefix: path})
+		for {
+			attrs, err := it.Next()
+			if err == iterator.Done {
+				break
+			}
+			if err != nil {
+				return FileStoreItem{}, fmt.Errorf("error iterating over GCS objects during rename: %w", err)
+			}
+			newObjPath := strings.Replace(attrs.Name, path, newPath, 1)
+			_, err = s.bucket.Object(newObjPath).CopierFrom(src).Run(ctx)
+			if err != nil {
+				return FileStoreItem{}, fmt.Errorf("error copying GCS object during rename: %w", err)
+			}
+			if err := src.Delete(ctx); err != nil {
+				return FileStoreItem{}, fmt.Errorf("error deleting original GCS object post rename: %w", err)
+			}
+		}
+	} else { // For single objects
+		if _, err := dst.CopierFrom(src).Run(ctx); err != nil {
+			return FileStoreItem{}, fmt.Errorf("failed to rename GCS object: %w", err)
+		}
+		if err := src.Delete(ctx); err != nil {
+			return FileStoreItem{}, fmt.Errorf("failed to delete original GCS object after renaming: %w", err)
+		}
+	}
+	return s.Get(ctx, newPath)
+}
+
+func (s *GCSStorage) Delete(ctx context.Context, path string) error {
+	if strings.HasSuffix(path, "/") { // If it's a directory
+		it := s.bucket.Objects(ctx, &storage.Query{Prefix: path})
+		for {
+			attrs, err := it.Next()
+			if err == iterator.Done {
+				break
+			}
+			if err != nil {
+				return fmt.Errorf("error iterating over GCS objects during delete: %w", err)
+			}
+			if err := s.bucket.Object(attrs.Name).Delete(ctx); err != nil {
+				return fmt.Errorf("error deleting GCS object: %w", err)
+			}
+		}
+	} else { // For single objects
+		if err := s.bucket.Object(path).Delete(ctx); err != nil {
+			return fmt.Errorf("failed to delete GCS object: %w", err)
+		}
+	}
+	return nil
+}
+
+func (s *GCSStorage) CreateFolder(ctx context.Context, path string) (FileStoreItem, error) {
+	obj := s.bucket.Object(path + "/")
+	if _, err := obj.NewWriter(ctx).Write([]byte("")); err != nil {
+		return FileStoreItem{}, fmt.Errorf("failed to create GCS folder: %w", err)
+	}
+	attrs, err := obj.Attrs(ctx)
+	if err != nil {
+		return FileStoreItem{}, fmt.Errorf("error fetching GCS object attributes after folder creation: %w", err)
+	}
+	return FileStoreItem{
+		Directory: strings.HasSuffix(attrs.Name, "/"),
+		Name:      attrs.Name,
+		Path:      attrs.Name,
+		URL:       attrs.MediaLink,
+	}, nil
+}
+
+// Compile-time interface check:
+var _ FileStore = (*GCSStorage)(nil)
