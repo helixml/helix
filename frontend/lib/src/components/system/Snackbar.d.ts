0a87fe8a1a85b519f8843d80c96a88e475bedff2
Kai Davenport <kaiyadavenport@gmail.com>
2023-10-15 15:28:44 +0100

    remove lib folder

--- frontend/lib/src/components/system/Snackbar.d.ts
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-declare const Snackbar: React.FC;
-export default Snackbar;
-//# sourceMappingURL=Snackbar.d.ts.map
--- frontend/lib/src/pages/Home.d.ts
+++
@@ -1,4 +0,0 @@
-import { FC } from 'react';
-declare const Dashboard: FC;
-export default Dashboard;
-//# sourceMappingURL=Home.d.ts.map
