7d679934bbdc81fcad3ea396f6cf37f3411ae22f
Luke Marsden <me@lukemarsden.net>
2023-10-22 08:21:22 +0100

    slight improvement

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -4,19 +4,32 @@ import { styled } from '@mui/system';
 const Container = styled('div')({
     maxWidth: '1200px',
     margin: '0 auto',
+    padding: '20px',
 });
 
 const Header = styled('div')({
-    padding: '20px',
-    textAlign: 'center',
-    backgroundColor: '#f5f5f5',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100px',
 });
 
 const Block = styled('div')({
-    padding: '20px',
-    textAlign: 'center',
-    marginBottom: '20px',
+    display: 'flex',
+    alignItems: 'center',
+    padding: '40px 20px',
+    marginBottom: '40px',
     boxShadow: '0 4px 8px rgba(0,0,0,0.1)',
+});
+
+const Media = styled('div')({
+    flex: '1',
+    paddingRight: '40px',
+});
+
+const Content = styled('div')({
+    flex: '1',
+    textAlign: 'left',
 });
 
 const Button = styled('button')({
@@ -25,14 +38,14 @@ const Button = styled('button')({
     backgroundColor: '#007BFF',
     color: 'white',
     borderRadius: '4px',
-    // cursor: 'pointer',
+    marginTop: '20px',
 });
 
 function App() {
     return (
         <Container>
             <Header>
-                <img src="/img/helix-text-logo.png" alt="Helix Logo" style={{width:"400px"}} />
+                <img src="/img/helix-text-logo.png" alt="Helix Logo" style={{width:"250px"}} />
             </Header>
             <OpenAIBlock />
             <ImageModelsBlock />
@@ -46,12 +59,16 @@ function App() {
 function OpenAIBlock() {
     return (
         <Block>
-            <video autoPlay loop muted  style={{width:"400px"}}>
-                <source src="/img/typing.mp4" type="video/mp4"/>
-            </video>
-            <h2>Open AI 😉</h2>
-            <p>Deploy the latest open source models securely into your cloud</p>
-            <p>Or let us run them for you</p>
+            <Media>
+                <video autoPlay loop muted style={{width:"300px"}}>
+                    <source src="/img/typing.mp4" type="video/mp4"/>
+                </video>
+            </Media>
+            <Content>
+                <h2>Open AI 😉</h2>
+                <p>Deploy the latest open source models securely into your cloud</p>
+                <p>Or let us run them for you</p>
+            </Content>
         </Block>
     );
 }
@@ -59,10 +76,14 @@ function OpenAIBlock() {
 function ImageModelsBlock() {
     return (
         <Block>
-            <img src="/img/sdxl.png" alt="Stable Diffusion XL" style={{width:"400px"}} />
-            <h2>Image models</h2>
-            <p>Train your own SDXL customized to your brand or style</p>
-            <Button>FINE TUNE SDXL</Button>
+            <Media>
+                <img src="/img/sdxl.png" alt="Stable Diffusion XL" style={{width:"300px"}} />
+            </Media>
+            <Content>
+                <h2>Image models</h2>
+                <p>Train your own SDXL customized to your brand or style</p>
+                <Button>FINE TUNE SDXL</Button>
+            </Content>
         </Block>
     );
 }
@@ -70,10 +91,14 @@ function ImageModelsBlock() {
 function LanguageModelsBlock() {
     return (
         <Block>
-            <img src="/img/mistral.png" alt="Mistral-7B" style={{width:"400px"}} />
-            <h2>Language models</h2>
-            <p>Small open source LLMs are beating proprietary models</p>
-            <Button>FINE TUNE MISTRAL-7B</Button>
+            <Media>
+                <img src="/img/mistral.png" alt="Mistral-7B" style={{width:"300px"}} />
+            </Media>
+            <Content>
+                <h2>Language models</h2>
+                <p>Small open source LLMs are beating proprietary models</p>
+                <Button>FINE TUNE MISTRAL-7B</Button>
+            </Content>
         </Block>
     );
 }
@@ -81,14 +106,18 @@ function LanguageModelsBlock() {
 function DeploymentBlock() {
     return (
         <Block>
-            <img src="/img/servers.png" alt="Servers in a data center" style={{width:"400px"}} />
-            <h2>Deployment</h2>
-            <ul>
-                <li>GPU scheduler</li>
-                <li>Smart runners</li>
-                <li>Autoscaler</li>
-            </ul>
-            <Button>DEPLOY ON YOUR INFRA</Button>
+            <Media>
+                <img src="/img/servers.png" alt="Servers in a data center" style={{width:"300px"}} />
+            </Media>
+            <Content>
+                <h2>Deployment</h2>
+                <ul style={{ listStyleType: 'none', padding: '0' }}>
+                    <li>GPU scheduler</li>
+                    <li>Smart runners</li>
+                    <li>Autoscaler</li>
+                </ul>
+                <Button>DEPLOY ON YOUR INFRA</Button>
+            </Content>
         </Block>
     );
 }
@@ -96,11 +125,15 @@ function DeploymentBlock() {
 function Footer() {
     return (
         <Block>
-            <img src="/img/github.png" alt="GitHub users collaborating" style={{width:"400px"}} />
-            <h2>Clone us on GitHub</h2>
-            <p>Bring new models to the open stack</p>
-            <a href="https://github.com/helix-ml/helix">GITHUB.COM/HELIX-ML/HELIX</a>
-            <Button>JOIN MLOPS.COMMUNITY SLACK</Button>
+            <Media>
+                <img src="/img/github.png" alt="GitHub users collaborating" style={{width:"250px"}} />
+            </Media>
+            <Content>
+                <h2>Clone us on GitHub</h2>
+                <p>Bring new models to the open stack</p>
+                <a href="https://github.com/helix-ml/helix" style={{ display: 'block', marginBottom: '20px' }}>GITHUB.COM/HELIX-ML/HELIX</a>
+                <Button>JOIN MLOPS.COMMUNITY SLACK</Button>
+            </Content>
         </Block>
     );
 }
