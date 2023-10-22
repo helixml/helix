0235459ad3c77e791e5614f95075b30ac6dafc29
Luke Marsden <me@lukemarsden.net>
2023-10-22 16:08:00 +0100

    tweak styles

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import App from "./App"
-import CssBaseline from "@mui/material/CssBaseline"
 
 let render = () => {
   ReactDOM.render(
     <>
-      <CssBaseline />
       <App />
     </>,
     document.getElementById("root")
--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import LoginIcon from '@mui/icons-material/Login'
+import Button from '@mui/material/Button'
 import { styled } from '@mui/system';
 
 const Container = styled('div')({
-    maxWidth: '1200px',
+    maxWidth: '750px',
     margin: '0 auto',
     padding: '20px',
 });
@@ -19,34 +21,44 @@ const Block = styled('div')({
     alignItems: 'center',
     padding: '40px 20px',
     marginBottom: '40px',
-    boxShadow: '0 4px 8px rgba(0,0,0,0.1)',
+    // boxShadow: '0 4px 8px rgba(0,0,0,0.1)',
 });
 
-const Media = styled('div')({
+const RightMedia = styled('div')({
+    flex: '1',
+    // paddingRight: '40px',
+});
+
+const RightContent = styled('div')({
+    flex: '1',
+    textAlign: 'left',
+    fontWeight: 500,
+});
+
+const LeftMedia = styled('div')({
     flex: '1',
     paddingRight: '40px',
 });
 
-const Content = styled('div')({
+const LeftContent = styled('div')({
     flex: '1',
     textAlign: 'left',
+    fontWeight: 500,
+    paddingRight: '40px',
 });
 
-const Button = styled('button')({
-    padding: '10px 20px',
-    border: 'none',
-    backgroundColor: '#007BFF',
-    color: 'white',
-    borderRadius: '4px',
-    marginTop: '20px',
-});
+// const Button = styled('button')({
+//     padding: '10px 20px',
+//     border: 'none',
+//     backgroundColor: '#007BFF',
+//     color: 'white',
+//     borderRadius: '4px',
+//     marginTop: '20px',
+// });
 
 function App() {
     return (
         <Container>
-            <Header>
-                <img src="/img/helix-text-logo.png" alt="Helix Logo" style={{width:"250px"}} />
-            </Header>
             <OpenAIBlock />
             <ImageModelsBlock />
             <LanguageModelsBlock />
@@ -59,16 +71,17 @@ function App() {
 function OpenAIBlock() {
     return (
         <Block>
-            <Media>
-                <video autoPlay loop muted style={{width:"300px"}}>
-                    <source src="/img/typing.mp4" type="video/mp4"/>
-                </video>
-            </Media>
-            <Content>
+            <LeftContent>
+                <img src="/img/helix-text-logo.png" alt="Helix Logo" style={{width:"250px"}} />
                 <h2>Open AI 😉</h2>
                 <p>Deploy the latest open source models securely into your cloud</p>
                 <p>Or let us run them for you</p>
-            </Content>
+            </LeftContent>
+            <RightMedia>
+                <video autoPlay loop muted style={{width:"350px", float: "right", marginRight: "-50px"}}>
+                    <source src="/img/typing.mp4" type="video/mp4"/>
+                </video>
+            </RightMedia>
         </Block>
     );
 }
@@ -76,14 +89,20 @@ function OpenAIBlock() {
 function ImageModelsBlock() {
     return (
         <Block>
-            <Media>
+            <LeftMedia>
                 <img src="/img/sdxl.png" alt="Stable Diffusion XL" style={{width:"300px"}} />
-            </Media>
-            <Content>
+            </LeftMedia>
+            <RightContent>
                 <h2>Image models</h2>
                 <p>Train your own SDXL customized to your brand or style</p>
-                <Button>FINE TUNE SDXL</Button>
-            </Content>
+                <Button
+                  variant="contained"
+                  onClick={ () => {
+                    // endIcon={<LoginIcon />}
+                    // account.onLogin()
+                  }}
+                >FINE TUNE SDXL</Button>
+            </RightContent>
         </Block>
     );
 }
@@ -91,14 +110,20 @@ function ImageModelsBlock() {
 function LanguageModelsBlock() {
     return (
         <Block>
-            <Media>
-                <img src="/img/mistral.png" alt="Mistral-7B" style={{width:"300px"}} />
-            </Media>
-            <Content>
+            <LeftContent>
                 <h2>Language models</h2>
                 <p>Small open source LLMs are beating proprietary models</p>
-                <Button>FINE TUNE MISTRAL-7B</Button>
-            </Content>
+                <Button
+                  variant="contained"
+                  onClick={ () => {
+                    // endIcon={<LoginIcon />}
+                    // account.onLogin()
+                  }}
+                >FINE TUNE MISTRAL-7B</Button>
+            </LeftContent>
+            <RightMedia>
+                <img src="/img/mistral.png" alt="Mistral-7B" style={{width:"300px", float: "right"}} />
+            </RightMedia>
         </Block>
     );
 }
@@ -106,18 +131,24 @@ function LanguageModelsBlock() {
 function DeploymentBlock() {
     return (
         <Block>
-            <Media>
+            <LeftMedia>
                 <img src="/img/servers.png" alt="Servers in a data center" style={{width:"300px"}} />
-            </Media>
-            <Content>
+            </LeftMedia>
+            <RightContent>
                 <h2>Deployment</h2>
                 <ul style={{ listStyleType: 'none', padding: '0' }}>
                     <li>GPU scheduler</li>
                     <li>Smart runners</li>
                     <li>Autoscaler</li>
                 </ul>
-                <Button>DEPLOY ON YOUR INFRA</Button>
-            </Content>
+                <Button
+                  variant="contained"
+                  onClick={ () => {
+                    // endIcon={<LoginIcon />}
+                    // account.onLogin()
+                  }}
+                >DEPLOY ON YOUR INFRA</Button>
+            </RightContent>
         </Block>
     );
 }
@@ -125,15 +156,26 @@ function DeploymentBlock() {
 function Footer() {
     return (
         <Block>
-            <Media>
-                <img src="/img/github.png" alt="GitHub users collaborating" style={{width:"250px"}} />
-            </Media>
-            <Content>
+            <LeftContent>
                 <h2>Clone us on GitHub</h2>
                 <p>Bring new models to the open stack</p>
-                <a href="https://github.com/helix-ml/helix" style={{ display: 'block', marginBottom: '20px' }}>GITHUB.COM/HELIX-ML/HELIX</a>
-                <Button>JOIN MLOPS.COMMUNITY SLACK</Button>
-            </Content>
+                <Button
+                  variant="contained"
+                  onClick={ () => {
+                    // endIcon={<LoginIcon />}
+                    // account.onLogin()
+                  }}
+                >GITHUB.COM/HELIX-ML/HELIX</Button>
+                <Button
+                  variant="contained"
+                  onClick={ () => {
+                  }}
+                  sx={{mt:2}}
+                >JOIN MLOPS.COMMUNITY SLACK</Button>
+            </LeftContent>
+            <RightMedia>
+                <img src="/img/github.png" alt="GitHub users collaborating" style={{width:"250px", float: "right"}} />
+            </RightMedia>
         </Block>
     );
 }
