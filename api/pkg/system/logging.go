6671e50dc309dd07f608a647d73beb648dc63a15
Kai Davenport <kaiyadavenport@gmail.com>
2023-10-24 16:30:50 +0100

    setup logging

---
+++ api/pkg/system/logging.go
@@ -0,0 +1,29 @@
+package system
+
+import (
+	"os"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func SetupLogging() {
+	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
+	logLevelString := os.Getenv("LOG_LEVEL")
+	if logLevelString == "" {
+		logLevelString = "info"
+	}
+	logLevel := zerolog.InfoLevel
+	if logLevelString == "none" {
+		logLevel = zerolog.NoLevel
+	}
+	parsedLogLevel, err := zerolog.ParseLevel(logLevelString)
+	if err == nil {
+		logLevel = parsedLogLevel
+	}
+
+	// edit to change the level of the stack we report
+	// zerolog.CallerSkipFrameCount = 3 // Skip 3 frames (this function, log.Output, log.Logger)
+	log.Logger = log.Output(output).With().Caller().Logger().Level(logLevel)
+}
