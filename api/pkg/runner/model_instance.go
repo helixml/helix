0eb38013372940db8e4139f2f6aea96b63781d63
Kai Davenport <kaiyadavenport@gmail.com>
2023-10-24 20:53:25 +0100

    initial answer is streaming nicely - we need to still pay attention to the text stream buffer when we get the answer because it has processed the output nicely

--- api/pkg/runner/model_instance.go
+++ api/pkg/runner/model_instance.go
@@ -100,8 +100,8 @@ func getLastInteractionID(session *types.Session) (string, error) {
 		return "", fmt.Errorf("session has no messages")
 	}
 	interaction := session.Interactions[len(session.Interactions)-1]
-	if interaction.Creator != types.CreatorTypeUser {
-		return "", fmt.Errorf("session does not have user interaction as last message")
+	if interaction.Creator != types.CreatorTypeSystem {
+		return "", fmt.Errorf("session does not have a system interaction as last message")
 	}
 	return interaction.ID, nil
 }
