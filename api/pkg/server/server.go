674aca776e2afd5a1d997dfc32b1bfb1c3949adb
Kai Davenport <kaiyadavenport@gmail.com>
2023-10-25 16:48:52 +0100

    upload files to the filestore and map their paths

--- api/pkg/server/server.go
+++ api/pkg/server/server.go
@@ -101,12 +101,16 @@ func (apiServer *HelixAPIServer) ListenAndServe(ctx context.Context, cm *system.
 	authRouter.HandleFunc("/sessions/{id}", Wrapper(apiServer.deleteSession)).Methods("DELETE")
 
 	// TODO: this has no auth right now
-	// we need to add JWTs to the urls we are using to connect models to the worker
+	// we need to add JWTs to the urls we are using to connect models to the workers
 	// the task filters (mode, type and modelName) are all given as query params
 	subrouter.HandleFunc("/runner/{runnerid}/nextsession", WrapperWithConfig(apiServer.getNextRunnerSession, WrapperConfig{
 		SilenceErrors: true,
 	})).Methods("GET")
 	subrouter.HandleFunc("/runner/{runnerid}/response", Wrapper(apiServer.respondRunnerSession)).Methods("POST")
+
+	// we should have an upload "tar" route also
+	// all files uploaded will be put under the "sessions/{sessionid}" folder in the filestore
+	subrouter.HandleFunc("/runner/{runnerid}/session/{sessionid}/upload", Wrapper(apiServer.runnerSessionUploadFiles)).Methods("POST")
 
 	StartWebSocketServer(
 		ctx,
