98870a3503a2ecc8b0890e6686293e4a5d5937db
Luke Marsden <me@lukemarsden.net>
2023-10-29 14:42:57 +0000

    damn sql

--- api/pkg/store/migrations/0003_api_key.up.sql
+++ api/pkg/store/migrations/0003_api_key.up.sql
@@ -2,5 +2,5 @@ create table api_key (
   owner varchar(255) NOT NULL,
   owner_type varchar(255) NOT NULL,
   key varchar(255) PRIMARY KEY,
-  name varchar(255) NOT NULL,
+  name varchar(255) NOT NULL
 );
