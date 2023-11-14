192564db3f7311ea97a41c74c402cdc993766dcd
Kai Davenport <kaiyadavenport@gmail.com>
2023-11-14 16:01:22 +0000

    fix null pointer

--- api/pkg/controller/controller.go
+++ api/pkg/controller/controller.go
@@ -100,7 +100,7 @@ func (c *Controller) Initialize() error {
 				return
 			case event := <-c.RunnerWebsocketEventChanReader:
 				go func() {
-					log.Debug().Msgf("Runner websocket event: %+v", *event.WorkerTaskResponse)
+					log.Debug().Msgf("Runner websocket event: %+v", *event)
 				}()
 				_, err := c.ReadRunnerWebsocketEvent(context.Background(), event)
 				if err != nil {
