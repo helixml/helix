359c38395c8372db2d20882af7347e29e42218a5
Kai Davenport <kaiyadavenport@gmail.com>
2023-11-14 17:47:47 +0000

    websocket messages are getting to the browser now

--- api/pkg/server/runner_websocket_client.go
+++ api/pkg/server/runner_websocket_client.go
@@ -4,48 +4,78 @@ import (
 	"context"
 	"time"
 
+	"github.com/gorilla/websocket"
 	"github.com/lukemarsden/helix/api/pkg/types"
-	"github.com/recws-org/recws"
 	"github.com/rs/zerolog/log"
 )
 
-// this is designed to write messages from the runner to the api server
+// ConnectWebSocket establishes a new WebSocket connection
 func ConnectRunnerWebSocketClient(
 	url string,
 	websocketEventChan chan *types.WebsocketEvent,
 	ctx context.Context,
 ) {
+	closed := false
 
-	ws := recws.RecConn{
-		KeepAliveTimeout: 60 * time.Second,
+	var conn *websocket.Conn
+
+	connectWebsocket := func() {
+		for {
+			var err error
+			log.Debug().Msgf("WebSocket connection connecting: %s", url)
+			conn, _, err = websocket.DefaultDialer.Dial(url, nil)
+			if err != nil {
+				log.Error().Msgf("WebSocket connection failed: %s\nReconnecting in 2 seconds...", err)
+				if closed {
+					break
+				}
+				time.Sleep(2 * time.Second)
+				continue
+			}
+			break
+		}
 	}
-	ws.Dial(url, nil)
 
-	for {
-		select {
-		case <-ctx.Done():
-			go ws.Close()
-			return
-		case <-time.After(10 * time.Second):
-			ev := &types.WebsocketEvent{
-				Type:      types.WebsocketEventSessionPing,
-				SessionID: "",
-				Owner:     "",
-			}
-			err := ws.WriteJSON(ev)
-			if err != nil {
-				log.Error().Msgf("Error writing ping to websocket: %s %+v", err.Error(), ev)
-				continue
-			}
-		case ev := <-websocketEventChan:
-			log.Debug().
-				Str("action", "Websocket WRITE").
-				Msgf("payload %+v", ev)
-			err := ws.WriteJSON(ev)
-			if err != nil {
-				log.Error().Msgf("Error writing data to websocket: %s %+v", err.Error(), ev)
-				continue
+	connectWebsocket()
+
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				closed = true
+				if conn != nil {
+					conn.Close()
+				}
+
+				return
+			case ev := <-websocketEventChan:
+				if conn == nil {
+					continue
+				}
+				conn.WriteJSON(ev)
 			}
 		}
+	}()
+
+	if !closed {
+		go func() {
+			for {
+				messageType, p, err := conn.ReadMessage()
+				if err != nil || messageType == websocket.CloseMessage {
+					if closed {
+						return
+					}
+					log.Error().Msgf("Read error: %s\nReconnecting in 2 seconds...", err)
+					time.Sleep(2 * time.Second)
+					connectWebsocket()
+					continue
+				} else if messageType == websocket.TextMessage {
+					log.Debug().
+						Str("action", "runner websocket READ").
+						Str("payload", string(p)).
+						Msgf("")
+				}
+			}
+		}()
 	}
 }
