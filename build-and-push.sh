0f7e8dac315847e34b91b1e0cc45c3c5768aa610
Luke Marsden <me@lukemarsden.net>
2023-11-30 22:12:23 +0000

    bump

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xeuo pipefail
-IMAGE="quay.io/lukemarsden/helix-runner:v0.0.5"
+IMAGE="quay.io/lukemarsden/helix-runner:v0.0.6"
 docker build -f Dockerfile.runner -t $IMAGE .
 docker push $IMAGE
