a916d44118892479e2fe4f13581dea025bd2d382
Kai Davenport <kaiyadavenport@gmail.com>
2023-12-13 22:03:54 +0000

    payments working well in local mode

--- frontend/src/pages/Account.tsx
+++ frontend/src/pages/Account.tsx
@@ -1,7 +1,7 @@
-import React, { FC } from 'react'
-import axios from 'axios'
+import React, { FC, useState, useEffect, useCallback } from 'react'
 import {CopyToClipboard} from 'react-copy-to-clipboard'
 import Container from '@mui/material/Container'
+import Button from '@mui/material/Button'
 import Box from '@mui/material/Box'
 import ListItemSecondaryAction from '@mui/material/ListItemSecondaryAction'
 import Typography from '@mui/material/Typography'
@@ -15,23 +15,62 @@ import Grid from '@mui/material/Grid'
 import DeleteIcon from '@mui/icons-material/Delete'
 import CopyIcon from '@mui/icons-material/CopyAll'
 
-import Subscription from '../components/account/Subscription'
-
 import useSnackbar from '../hooks/useSnackbar'
 import useAccount from '../hooks/useAccount'
+import useApi from '../hooks/useApi'
 
 const Account: FC = () => {
   const account = useAccount()
+  const api = useApi()
   const snackbar = useSnackbar()
-  
-  if(!account.user) return null
-  const handleDeleteApiKey = async (key: string) => {
-    try {
-      await axios.delete(`/api/v1/api_keys?key=${key}`)
-    } catch (error) {
-      console.error(error)
+
+  let [success, setSuccess] = useState(false)
+
+  const paymentsActive = account.serverConfig.stripe_enabled
+  const colSize = paymentsActive ? 6 : 12
+
+  const handleDeleteApiKey = useCallback(async (key: string) => {
+    await api.delete(`/api/v1/api_keys`, {
+      params: {
+        key,
+      }
+    }, {
+      loading: true,
+      snackbar: true,
+    })
+  }, [])
+
+  const handleSubscribe = useCallback(async () => {
+    const result = await api.post(`/api/v1/subscription/new`, undefined, {}, {
+      loading: true,
+      snackbar: true,
+    })
+    if(!result) return
+    document.location = result
+  }, [
+    account.user,
+  ])
+
+  const handleManage = useCallback(async () => {
+    const result = await api.post(`/api/v1/subscription/manage`, undefined, {}, {
+      loading: true,
+      snackbar: true,
+    })
+    if(!result) return
+    document.location = result
+  }, [
+    account.user,
+  ])
+
+  useEffect(() => {
+    const query = new URLSearchParams(window.location.search)
+    if (query.get('success')) {
+      setSuccess(true)
+      snackbar.success('Subscription successful')
     }
-  }
+  }, [])
+
+  if(!account.user) return null
 
   return (
     <Box
@@ -54,7 +93,7 @@ const Account: FC = () => {
       >
         <Container maxWidth="lg">
           <Grid container spacing={2}>
-            <Grid item xs={12} md={6}>
+            <Grid item xs={12} md={colSize}>
               <Paper sx={{ p: 2 }}>
                 <Typography variant="h6">API Keys</Typography>
                 <List>
@@ -91,11 +130,49 @@ const Account: FC = () => {
                 </List>
               </Paper>
             </Grid>
-            <Grid item xs={12} md={6}>
-              <Paper sx={{ p: 2 }}>
-                <Subscription />
-              </Paper>
-            </Grid>
+            {
+              paymentsActive && (
+                <Grid item xs={12} md={colSize}>
+                  <Paper sx={{ p: 2 }}>
+                    {
+                      account.userConfig.stripe_subscription_active ? (
+                        <Box alignItems="center" justifyContent="center">
+                          <Typography variant="h6" gutterBottom>Subscription Active</Typography>
+                          <Typography variant="subtitle1" gutterBottom>Helix Premium : $20.00 / month</Typography>
+                          <Typography variant="body2" gutterBottom>You have priority access to the Helix GPU cloud</Typography>
+                          <Button
+                            variant="contained"
+                            color="primary"
+                            sx={{
+                              mt: 2,
+                            }}
+                            onClick={ handleManage }
+                          >
+                            Manage Subscription
+                          </Button>
+                        </Box>
+                      ) : (
+                        <Box alignItems="center" justifyContent="center">
+                          <Typography variant="h6" gutterBottom>Helix Premium</Typography>
+                          <Typography variant="subtitle1" gutterBottom>$20.00 / month</Typography>
+                          <Typography variant="body2" gutterBottom>Get priority access to the Helix GPU cloud</Typography>
+                          <Button
+                            variant="contained"
+                            color="primary"
+                            sx={{
+                              mt: 2,
+                            }}
+                            onClick={ handleSubscribe }
+                          >
+                            Start Subscription
+                          </Button>
+                        </Box>
+                      )
+                    }
+                  </Paper>
+                </Grid>
+              )
+            }
           </Grid>
         </Container>
       </Box>
