6f1fb420c79ebdfa543266f8c2f1b80062635218
Luke Marsden <me@lukemarsden.net>
2023-12-14 17:46:00 +0000

    release runner filters

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xeuo pipefail
-IMAGE="europe-docker.pkg.dev/helixml/helix/runner:v0.1.4"
+IMAGE="europe-docker.pkg.dev/helixml/helix/runner:v0.1.5"
 docker build -f Dockerfile.runner -t $IMAGE .
 docker push $IMAGE
