78b23601e78a42478b9aeb13c1f5a373581a371f
Luke Marsden <me@lukemarsden.net>
2023-12-20 06:45:22 +0000

    tweak rendering of messages to trim whitespace and not dump html totally unsafely

--- frontend/src/components/session/InteractionInference.tsx
+++ frontend/src/components/session/InteractionInference.tsx
@@ -39,7 +39,7 @@ export const InteractionMessage: FC<{
     <>
       {
         message && (
-          <Typography className="interactionMessage" dangerouslySetInnerHTML={{__html: message.replace(/\n/g, '<br/>')}}></Typography>
+          <Typography className="interactionMessage" dangerouslySetInnerHTML={{__html: message.trim().replace(/</g, '&lt;').replace(/\n/g, '<br/>')}}></Typography>
         )
       }
       {
