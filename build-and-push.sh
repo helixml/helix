bdd3768880d30469bf036784a31ab54d5335d5b0
Luke Marsden <me@lukemarsden.net>
2023-12-29 17:24:17 +0000

    bump

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xeuo pipefail
-IMAGE="europe-docker.pkg.dev/helixml/helix/runner:v0.2.4"
+IMAGE="europe-docker.pkg.dev/helixml/helix/runner:v0.2.5"
 docker build -f Dockerfile.runner -t $IMAGE .
 docker push $IMAGE
