cd1f71353f4901d84d6c130ce89995887b4cf347
Kai Davenport <kaiyadavenport@gmail.com>
2024-01-15 15:58:16 +0000

    post to eebeddings is working and populating database table

--- llamaindex/src/main.py
+++ llamaindex/src/main.py
@@ -2,23 +2,31 @@ from flask import Flask, request, jsonify
 import os
 import tempfile
 import requests
-from sql import getEngine
-
-####################
-#
-# APP ROUTES
-#
-####################
+import pprint
+from sql import getEngine, checkDocumentChunkData, insertData, getRow
+from embedding import getEmbedding
 
 app = Flask(__name__)
 engine = getEngine()
 
-@app.route('/api/v1/test', methods=['GET'])
+# curl -X POST -H "Content-Type: application/json" -d '{
+#   "session_id": "123",
+#   "interaction_id": "456",
+#   "filename": "test.txt",
+#   "offset": 0,
+#   "text": "hello world"
+# }' http://localhost:6000/api/v1/chunk
+@app.route('/api/v1/chunk', methods=['POST'])
 def test():
-  import pprint; pprint.pprint(request.json)
-  return jsonify({
-    "ok": True,
-  }), 200
+  data = request.json
+  pprint.pprint(data)
+  checkDocumentChunkData(data)
+  data["embedding"] = getEmbedding(data["text"])
+  pprint.pprint(data["embedding"])
+  id = insertData(engine, data)
+  pprint.pprint(id)
+  result = getRow(engine, id)
+  return jsonify(result), 200
 
 if __name__ == '__main__':
   app.run(debug=True, port=6000, host='0.0.0.0')
