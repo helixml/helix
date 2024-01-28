4c16fe994c4288a369061f58d1d8c6b23886e22c
Karolis Rusenas <karolis@rusenas.dev>
2024-01-28 00:26:13 +0000

    impl

---
+++ api/pkg/tools/informative_or_actionable.go
@@ -0,0 +1,181 @@
+package tools
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"html/template"
+	"strings"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/helixml/helix/api/pkg/types"
+	openai "github.com/sashabaranov/go-openai"
+)
+
+type Config struct {
+	OpenAIApiKey  string `envconfig:"OPENAI_API_KEY"`
+	OpenAIBaseURL string `envconfig:"OPENAI_BASE_URL" default:"https://api.openai.com/v1"`
+	Model         string `envconfig:"OPENAI_MODEL" default:""`
+}
+
+type ChainStrategy struct {
+	apiClient *openai.Client
+}
+
+func NewChainStrategy(cfg *Config) (*ChainStrategy, error) {
+	config := openai.DefaultConfig(cfg.OpenAIApiKey)
+	config.BaseURL = cfg.OpenAIBaseURL
+
+	return &ChainStrategy{
+		apiClient: openai.NewClientWithConfig(config),
+	}, nil
+}
+
+type IsActionableResponse struct {
+	NeedsApi      string `json:"needs_api"`
+	Api           string `json:"api"`
+	Justification string `json:"justification"`
+}
+
+func (c *ChainStrategy) IsActionable(ctx context.Context, tools []*types.Tool, history []*types.Interaction, currentMessage string) (*IsActionableResponse, error) {
+	if len(tools) == 0 {
+		return &IsActionableResponse{
+			NeedsApi:      "no",
+			Justification: "No tools available to check if the user input is actionable or not",
+		}, nil
+	}
+
+	systemPrompt, err := c.getSystemPrompt(tools)
+	if err != nil {
+		return nil, fmt.Errorf("failed to prepare system prompt: %w", err)
+	}
+
+	var messages []openai.ChatCompletionMessage
+
+	messages = append(messages, systemPrompt)
+
+	for _, interaction := range history {
+		switch interaction.Creator {
+		case types.CreatorTypeUser:
+			messages = append(messages, openai.ChatCompletionMessage{
+				Role:    openai.ChatMessageRoleUser,
+				Content: interaction.Message,
+			})
+		case types.CreatorTypeSystem:
+			messages = append(messages, openai.ChatCompletionMessage{
+				Role:    openai.ChatMessageRoleAssistant,
+				Content: interaction.Message,
+			})
+		}
+	}
+
+	// Adding current message
+	messages = append(messages,
+		openai.ChatCompletionMessage{
+			Role:    openai.ChatMessageRoleUser,
+			Content: currentMessage,
+		},
+		openai.ChatCompletionMessage{
+			Role:    openai.ChatMessageRoleUser,
+			Content: "Return the corresponding json for the last user input",
+		},
+	)
+
+	spew.Dump(messages)
+
+	resp, err := c.apiClient.CreateChatCompletion(
+		ctx,
+		openai.ChatCompletionRequest{
+			Stream:    false,
+			MaxTokens: 100,
+			Model:     openai.GPT4Turbo1106,
+			Messages:  messages,
+		},
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get response from inference API: %w", err)
+	}
+
+	fmt.Println("=================")
+	spew.Dump(resp)
+
+	var actionableResponse IsActionableResponse
+	err = json.Unmarshal([]byte(resp.Choices[0].Message.Content), &actionableResponse)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse response from inference API: %w", err)
+	}
+
+	return &actionableResponse, nil
+}
+
+func (c *ChainStrategy) getSystemPrompt(tools []*types.Tool) (openai.ChatCompletionMessage, error) {
+	// Render template
+	tmpl, err := template.New("system_prompt").Parse(isInformativeOrActionablePrompt)
+	if err != nil {
+		return openai.ChatCompletionMessage{}, err
+	}
+
+	// Render template
+	var sb strings.Builder
+	err = tmpl.Execute(&sb, struct {
+		Tools []*types.Tool
+	}{
+		Tools: tools,
+	})
+
+	if err != nil {
+		return openai.ChatCompletionMessage{}, err
+	}
+
+	return openai.ChatCompletionMessage{
+		Role:    openai.ChatMessageRoleSystem,
+		Content: sb.String(),
+	}, nil
+}
+
+const isInformativeOrActionablePrompt = `You are an AI tool that classifies whether user input requires an API call or not. You should recommend using an API if the user request matches one of the APIs descriptions below. The user requests that can be fulfilled by calling an external API to either execute something or fetch more data to help in answering the question. Also, if the user question is asking you to perform actions (e.g. list, create, update, delete) then you will need to use an API.
+
+Examples:  
+
+**User Input:** Create a B-1 visa application
+
+**Available APIs:**  
+- API(createVisaApplication): This API creates a B-1 visa application. 
+- API(getVisaStatus): This API queries B-1 visa status.   
+
+**Verdict:** Needs API call so the response should be {"needs_api": "yes", "justification": "The reason behind your verdict", "api": "createVisaApplication"}
+
+**Justification:** The user is asking to create a visa application and the (createVisaApplication) API can be used to satisfy the user requirement.  
+
+**Another Example:**
+
+**User Input:** How to renew a B-1 visa  
+
+**Available APIs:**   
+- API(createVisaApplication): This API creates a B-1 visa application.  
+- API(renewVisa): This API renews an existing B-1 visa.
+
+**Verdict:** Does not need API call so the response should be {"needs_api": "no", "justification": "The reason behind your verdict", "api": ""}  
+
+**Justification:** The user is asking how to renew a B-1 visa, which is an informational question that does not require an API call.
+
+**One More Example:**
+
+**User Input:** Get status of my B-1 visa application  
+
+**Available APIs:**    
+- API(getVisaStatus): This API queries status of a B-1 visa application.
+
+**Verdict:** Needs API call so the response should be {"needs_api": "yes", "justification": "The user is asking to get visa status", "api": "getVisaStatus"}
+
+**Response Format:** Always respond with JSON without any commentary, for example: {"needs_api": "no", "justification": "The reason behind your verdict", "api": "apiName"}  
+
+===END EXAMPLES===
+The available tools:
+
+{{ range $index, $tool := .Tools }}
+{{ $index }}. {{ $tool.Name }} ({{ $tool.Description }})
+{{ end }}
+
+Based on the above, here is the user input/questions:
+`
