da51733a96c3016f621363d323c81c6062ff5b96
Karolis Rusenas <karolis@rusenas.dev>
2024-01-29 23:05:13 +0000

    db

--- api/pkg/store/tools.go
+++ api/pkg/store/tools.go
@@ -1,1 +1,29 @@
 package store
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/helixml/helix/api/pkg/system"
+	"github.com/helixml/helix/api/pkg/types"
+)
+
+func (s *PostgresStore) CreateTool(ctx context.Context, tool *types.Tool) (*types.Tool, error) {
+	if tool.ID == "" {
+		tool.ID = system.GenerateToolID()
+	}
+
+	if tool.Owner == "" {
+		return nil, fmt.Errorf("owner not specified")
+	}
+
+	err := s.gdb.WithContext(ctx).Create(&tool).Error
+	if err != nil {
+		return nil, err
+	}
+	return s.GetTool(ctx, tool.ID)
+}
+
+func (s *PostgresStore) GetTool(ctx context.Context, id string) (*types.Tool, error) {
+
+}
