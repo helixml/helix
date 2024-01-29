34a77310fef59d991bd9e81f7aaeb3657f357599
Karolis Rusenas <karolis@rusenas.dev>
2024-01-29 21:36:28 +0000

    organization

--- api/pkg/tools/tools_api.go
+++ api/pkg/tools/tools_api.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"html/template"
-	"io"
 	"net/http"
 	"strings"
 
@@ -14,74 +13,6 @@ import (
 
 	openai "github.com/sashabaranov/go-openai"
 )
-
-type RunActionResponse struct {
-	Message    string `json:"message"`     // Interpreted message
-	RawMessage string `json:"raw_message"` // Raw message from the API
-	Error      string `json:"error"`
-}
-
-func (c *ChainStrategy) RunAction(ctx context.Context, tool *types.Tool, history []*types.Interaction, currentMessage, action string) (*RunActionResponse, error) {
-	switch tool.ToolType {
-	case types.ToolTypeFunction:
-		return nil, fmt.Errorf("function tool type is not supported yet")
-	case types.ToolTypeAPI:
-		return c.runApiAction(ctx, tool, history, currentMessage, action)
-	default:
-		return nil, fmt.Errorf("unknown tool type: %s", tool.ToolType)
-	}
-}
-
-func (c *ChainStrategy) runApiAction(ctx context.Context, tool *types.Tool, history []*types.Interaction, currentMessage, action string) (*RunActionResponse, error) {
-	// Validate whether action is valid
-	if action == "" {
-		return nil, fmt.Errorf("action is required")
-	}
-
-	found := false
-
-	for _, ac := range tool.Config.API.Actions {
-		if ac.Name == action {
-			found = true
-			break
-		}
-	}
-
-	if !found {
-		return nil, fmt.Errorf("action %s is not found in the tool %s", action, tool.Name)
-	}
-
-	// Get API request parameters
-	params, err := c.getAPIRequestParameters(ctx, tool, history, currentMessage, action)
-	if err != nil {
-		return nil, fmt.Errorf("failed to get api request parameters: %w", err)
-	}
-
-	req, err := c.prepareRequest(ctx, tool, action, params)
-	if err != nil {
-		return nil, fmt.Errorf("failed to prepare request: %w", err)
-	}
-
-	// Make API call
-	resp, err := c.httpClient.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("failed to make api call: %w", err)
-	}
-	defer resp.Body.Close()
-
-	bts, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read response body: %w", err)
-	}
-
-	// TODO: interpret msg
-
-	return &RunActionResponse{
-		Message:    string(bts),
-		RawMessage: string(bts),
-		Error:      "", // TODO
-	}, nil
-}
 
 func (c *ChainStrategy) prepareRequest(ctx context.Context, tool *types.Tool, action string, pathParams map[string]string) (*http.Request, error) {
 	loader := openapi3.NewLoader()
