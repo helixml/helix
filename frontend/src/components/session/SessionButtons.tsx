74ff881fbe6ae30dabb78816fd6a478d48153d56
bigadamknight <adam.knight@astonishemail.com>
2024-01-29 08:00:00 +0000

    refactor header to switch properly between new and existing sessions

---
+++ frontend/src/components/session/SessionButtons.tsx
@@ -0,0 +1,132 @@
+import React, { FC, useState, useCallback } from 'react'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import Link from '@mui/material/Link'
+import JsonWindowLink from '../widgets/JsonWindowLink'
+import Row from '../widgets/Row'
+import FolderOpenIcon from '@mui/icons-material/Folder'
+import DeleteConfirmWindow from '../widgets/DeleteConfirmWindow'
+import InfoIcon from '@mui/icons-material/Info'
+import DeleteIcon from '@mui/icons-material/Delete'
+import EditIcon from '@mui/icons-material/Edit'
+import PublishIcon from '@mui/icons-material/Publish'
+
+import {
+  ISession,
+} from '../../types'
+
+import useRouter from '../../hooks/useRouter'
+import useSessions from '../../hooks/useSessions'
+import useSnackbar from '../../hooks/useSnackbar'
+import useLoading from '../../hooks/useLoading'
+
+export const SessionButtons: FC<{
+  session: ISession,
+}> = ({
+  session,
+}) => {
+  const {
+    navigate,
+    setParams,
+  } = useRouter()
+  const sessions = useSessions()
+  const snackbar = useSnackbar()
+  const loading = useLoading()
+
+  const [deletingSession, setDeletingSession] = useState<ISession>()
+
+  const onDeleteSessionConfirm = useCallback(async (session_id: string) => {
+    loading.setLoading(true)
+    try {
+      const result = await sessions.deleteSession(session_id)
+      if(!result) return
+      setDeletingSession(undefined)
+      snackbar.success(`Session deleted`)
+      navigate('home')
+    } catch(e) {}
+    loading.setLoading(false)
+  }, [])
+
+  return (
+    <Row>
+      <Link
+        href="/files?path=%2Fsessions"
+        onClick={(e) => {
+          e.preventDefault()
+          navigate('files', {
+            path: `/sessions/${session?.id}`
+          })
+        }}
+      >
+        <Typography
+          sx={{
+            fontSize: "small",
+            flexGrow: 0,
+            textDecoration: 'underline',
+          }}
+        >
+          <Box component="span" sx={{ display: 'flex', alignItems: 'center' }}>
+            <FolderOpenIcon sx={{ mr: 2 }} />
+          </Box>
+        </Typography>
+      </Link>
+      <JsonWindowLink
+        data={ session } 
+      >
+        <Typography
+          sx={{
+            fontSize: "small",
+            flexGrow: 0,
+            textDecoration: 'underline',
+          }}
+        >
+          <Box component="span" sx={{ display: 'flex', alignItems: 'center' }}>
+            <InfoIcon sx={{ mr: 2 }} />
+          </Box>
+        </Typography>
+      </JsonWindowLink>
+      <Link
+        href="/files?path=%2Fsessions"
+        onClick={(e) => {
+          e.preventDefault()
+          setDeletingSession(session)
+        }}
+      >
+        <Box component="span" sx={{ display: 'flex', alignItems: 'center' }}>
+          <DeleteIcon sx={{ mr: 2 }} />
+        </Box>
+      </Link>
+      {/* { session.lora_dir && !session.parent_bot && (
+          <Link
+            href="/create_bot"
+            onClick={(e) => {
+              e.preventDefault()
+              setParams({
+                editBot: 'yes',
+              })
+            }}
+          >
+            <Box component="span" sx={{ display: 'flex', alignItems: 'center' }}>
+              { session.parent_bot ? <EditIcon sx={{ mr: 2 }} /> : <PublishIcon sx={{ mr: 2 }} /> }
+            </Box>
+          </Link>
+        )
+      } */}
+      {
+        deletingSession && (
+          <DeleteConfirmWindow
+            title={`Delete session ${deletingSession.name}?`}
+            onCancel={ () => {
+              setDeletingSession(undefined) 
+            }}
+            onSubmit={ () => {
+              onDeleteSessionConfirm(deletingSession.id)
+            }}
+          />
+        )
+      }
+    </Row>
+  )
+}
+
+export default SessionButtons
---
+++ frontend/src/components/system/NewAppbar.tsx
@@ -0,0 +1,221 @@
+import React from 'react'
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import Tooltip from '@mui/material/Tooltip'
+import Button from '@mui/material/Button'
+import Link from '@mui/material/Link'
+import Box from '@mui/material/Box'
+import Brightness7Icon from '@mui/icons-material/Brightness7'
+import Brightness4Icon from '@mui/icons-material/Brightness4'
+import HelpIcon from '@mui/icons-material/Help'
+import LoginIcon from '@mui/icons-material/Login'
+import MenuIcon from '@mui/icons-material/Menu'
+import EditIcon from '@mui/icons-material/Edit'
+import useAccount from '../../hooks/useAccount'
+import { useTheme } from '@mui/material/styles'
+import useThemeConfig from '../../hooks/useThemeConfig'
+import { ThemeContext } from '../../contexts/theme'
+
+interface NewAppBarProps {
+  getTitle?: () => React.ReactNode;
+  getToolbarElement?: () => React.ReactNode;
+  meta: { title?: string };
+  handleDrawerToggle: () => void;
+  bigScreen: boolean;
+  drawerWidth: number;
+}
+
+const NewAppBar: React.FC<NewAppBarProps> = ({ getTitle, getToolbarElement, meta, handleDrawerToggle, bigScreen, drawerWidth }) => {
+  const theme = useTheme()
+  const account = useAccount()
+  const themeConfig = useThemeConfig()
+  const { mode, toggleMode } = React.useContext(ThemeContext);
+
+  const handleThemeChange = () => {
+    toggleMode()
+  }
+
+  return (
+    <AppBar
+        elevation={0}
+        position="fixed"
+        color="default"
+        sx={{
+            height: '78px',
+            px: 0,
+            borderBottom: theme.palette.mode === 'light' ? themeConfig.lightBorder: themeConfig.darkBorder,
+            width: { xs: '100%', sm: '100%', md: `calc(100% - ${drawerWidth}px)` },
+            ml: { xs: '0px', sm: '0px', md: `${drawerWidth}px` },
+        }}
+    >
+        <Toolbar
+            sx={{
+                pr: '24px', // keep right padding when drawer closed
+                height: '100%',
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+                width: '100%',
+                backgroundColor: theme.palette.mode === 'light' ? themeConfig.lightBackgroundColor : themeConfig.darkBackgroundColor,
+            }}
+        >
+            {
+            bigScreen ? (
+                <Box
+                sx={{
+                    flexGrow: 0,
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                }}
+                >
+                {
+                    getTitle ?
+                    getTitle() :
+                    (
+                        <Typography
+                            className="inferenceTitle"
+                            component="h1"
+                            variant="h6"
+                            color="inherit"
+                            noWrap
+                            sx={{
+                                flexGrow: 1,
+                                ml: 1,
+                                color: 'text.primary',
+                            }}
+                        >
+                        {meta.title || ''}
+                        </Typography>
+                    )
+                }
+                <IconButton
+                    color="inherit"
+                    aria-label="rename session"
+                >
+                    <EditIcon />
+                </IconButton>
+                </Box>
+            ) : (
+                <Box
+                sx={{
+                    flexGrow: 0,
+                    display: 'flex',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                }}
+                >
+                <IconButton
+                    color="inherit"
+                    aria-label="open drawer"
+                    edge="start"
+                    onClick={handleDrawerToggle}
+                    sx={{
+                    mr: 1,
+                    ml: 1,
+                    }}
+                >
+                    <MenuIcon />
+                </IconButton>
+                {/* { themeConfig.logo() } */}
+                </Box>
+            )
+            }
+            <Box
+            sx={{
+                flexGrow: 1,
+                textAlign: 'right',
+            }}
+            >
+            {
+                bigScreen && getToolbarElement && account.user ? getToolbarElement() : null
+            }
+            {
+                bigScreen ? (
+                <>
+                    <Tooltip title={theme.palette.mode === 'dark' ? "Switch to light mode" : "Switch to dark mode"}>
+                    <IconButton
+                        color="inherit"
+                        aria-label="toggle theme"
+                        onClick={handleThemeChange}
+                        sx={{
+                        mr: 1,
+                        }}
+                    >
+                        {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+                    </IconButton>
+                    </Tooltip>
+                    {
+                    account.user ? (
+                        <Link
+                        href="https://docs.helix.ml/docs/overview"
+                        target="_blank"
+                        >
+                        <Tooltip title="Helix Docs">
+                            <Box component="span">
+                            <HelpIcon sx={{ mr: 2 }} />
+                            </Box>
+                        </Tooltip>
+                        </Link>
+                    ) : (
+                        <Button
+                        variant="contained"
+                        color="secondary"
+                        endIcon={<LoginIcon />}
+                        onClick={account.onLogin}
+                        >
+                        Login / Register
+                        </Button>
+                    )
+                    }
+                </>
+                ) : (
+                <>
+                    {
+                    account.user ? (
+                        <Link
+                        href="https://docs.helix.ml"
+                        target="_blank"
+                        >
+                        <Typography
+                            sx={{
+                            fontSize: "small",
+                            flexGrow: 0,
+                            textDecoration: 'underline',
+                            }}
+                        >
+                            View Docs
+                        </Typography>
+                        </Link>
+                    ) : (
+                        <Link
+                        href="/login"
+                        onClick={(e) => {
+                            e.preventDefault()
+                            account.onLogin()
+                        }}
+                        >
+                        <Typography
+                            sx={{
+                            fontSize: "small",
+                            flexGrow: 0,
+                            textDecoration: 'underline',
+                            }}
+                        >
+                            Login / Register
+                        </Typography>
+                        </Link>
+                    )
+                    }
+                </>
+                )
+            }
+            </Box>
+        </Toolbar>
+    </AppBar>
+  )
+}
+
+export default NewAppBar
