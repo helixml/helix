8d51bc13be9a4beb2ce92273192945f3eb5952e2
Karolis Rusenas <karolis@rusenas.dev>
2024-01-30 23:10:20 +0000

    tests

--- api/pkg/store/store_session_tools_test.go
+++ api/pkg/store/store_session_tools_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"testing"
 
+	"github.com/helixml/helix/api/pkg/system"
 	"github.com/helixml/helix/api/pkg/types"
 	"github.com/stretchr/testify/suite"
 )
@@ -34,9 +35,11 @@ func (suite *PostgresStoreTestSuite) SetupTest() {
 }
 
 func (suite *PostgresStoreTestSuite) TestCreateTool() {
+	ownerID := "test-" + system.GenerateUUID()
+
 	tool := &types.Tool{
 		Name:      "test",
-		Owner:     "test",
+		Owner:     ownerID,
 		OwnerType: types.OwnerTypeUser,
 		Config: types.ToolConfig{
 			API: &types.ToolApiConfig{
@@ -59,6 +62,54 @@ func (suite *PostgresStoreTestSuite) TestCreateTool() {
 
 	suite.T().Cleanup(func() {
 		err := suite.db.DeleteTool(suite.ctx, createdTool.ID)
+		suite.NoError(err)
+	})
+}
+
+func (suite *PostgresStoreTestSuite) Test_ListSessionTools() {
+	ownerID := "test-" + system.GenerateUUID()
+
+	tool := &types.Tool{
+		Name:      "test",
+		Owner:     ownerID,
+		OwnerType: types.OwnerTypeUser,
+		Config: types.ToolConfig{
+			API: &types.ToolApiConfig{
+				URL: "http://test.com",
+				Headers: map[string]string{
+					"Authorization": "Bearer 123",
+				},
+				Schema: "123",
+			},
+		},
+	}
+
+	createdTool, err := suite.db.CreateTool(suite.ctx, tool)
+	suite.NoError(err)
+	suite.NotNil(createdTool)
+	suite.Equal(tool.Name, createdTool.Name)
+	suite.Equal(tool.Owner, createdTool.Owner)
+	suite.Equal(tool.OwnerType, createdTool.OwnerType)
+	suite.NotEmpty(createdTool.ID)
+
+	sessionID := "session-test" + system.GenerateUUID()
+
+	// Now, creating a session tool binding
+	err = suite.db.CreateSessionToolBinding(suite.ctx, sessionID, createdTool.ID)
+	suite.NoError(err)
+
+	// Now, listing all tools for the session
+	tools, err := suite.db.ListSessionTools(suite.ctx, sessionID)
+	suite.NoError(err)
+
+	suite.Equal(1, len(tools))
+	suite.Equal(createdTool.ID, tools[0].ID)
+
+	suite.T().Cleanup(func() {
+		err := suite.db.DeleteTool(suite.ctx, createdTool.ID)
+		suite.NoError(err)
+
+		err = suite.db.DeleteSessionToolBinding(suite.ctx, sessionID, createdTool.ID)
 		suite.NoError(err)
 	})
 }
