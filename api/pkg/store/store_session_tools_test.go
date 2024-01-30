fc6a65b61d6422b7abf3ed7b59877880e29c148e
Karolis Rusenas <karolis@rusenas.dev>
2024-01-30 23:45:04 +0000

    setup file

--- api/pkg/store/store_session_tools_test.go
+++ api/pkg/store/store_session_tools_test.go
@@ -1,46 +1,9 @@
 package store
 
 import (
-	"context"
-	"os"
-	"testing"
-
 	"github.com/helixml/helix/api/pkg/system"
 	"github.com/helixml/helix/api/pkg/types"
-	"github.com/stretchr/testify/suite"
 )
-
-func TestPostgresStoreSuite(t *testing.T) {
-	suite.Run(t, new(PostgresStoreTestSuite))
-}
-
-type PostgresStoreTestSuite struct {
-	suite.Suite
-	ctx context.Context
-	db  *PostgresStore
-}
-
-func (suite *PostgresStoreTestSuite) SetupTest() {
-	suite.ctx = context.Background()
-
-	// TODO: move server options to envconfig
-	host := os.Getenv("POSTGRES_HOST")
-	if host == "" {
-		host = "localhost"
-	}
-
-	store, err := NewPostgresStore(StoreOptions{
-		Host:        host,
-		Port:        5432,
-		Username:    "postgres",
-		Password:    "postgres",
-		Database:    "postgres",
-		AutoMigrate: true,
-	})
-	suite.NoError(err)
-
-	suite.db = store
-}
 
 func (suite *PostgresStoreTestSuite) Test_ListSessionTools() {
 	ownerID := "test-" + system.GenerateUUID()
