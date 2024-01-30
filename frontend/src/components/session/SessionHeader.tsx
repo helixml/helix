e793eb2e51fa8da81bebf6275635222c51397a07
bigadamknight <adam.knight@astonishemail.com>
2024-01-30 07:30:29 +0000

    various additions

--- frontend/src/components/session/SessionHeader.tsx
+++ frontend/src/components/session/SessionHeader.tsx
@@ -16,6 +16,9 @@ import HelpIcon from '@mui/icons-material/Help'
 import IconButton from '@mui/material/IconButton'
 import Tooltip from '@mui/material/Tooltip'
 import Chip from '@mui/material/Chip'
+import Button from '@mui/material/Button'
+import ScreenShareIcon from '@mui/icons-material/ScreenShare'
+
 import { useTheme } from '@mui/material/styles'
 import useThemeConfig from '../../hooks/useThemeConfig'
 
@@ -27,6 +30,7 @@ import useRouter from '../../hooks/useRouter'
 import useSessions from '../../hooks/useSessions'
 import useSnackbar from '../../hooks/useSnackbar'
 import useLoading from '../../hooks/useLoading'
+import useAccount from '../../hooks/useAccount'
 
 export const SessionHeader: FC<{
   session: ISession,
@@ -42,6 +46,15 @@ export const SessionHeader: FC<{
   const loading = useLoading()
   const theme = useTheme()
   const themeConfig = useThemeConfig()
+  const account = useAccount()
+
+  const isOwner = account.user?.id === session.owner
+
+  const onShare = useCallback(() => {
+    setParams({
+      sharing: 'yes',
+    })
+  }, [setParams])
 
   const [deletingSession, setDeletingSession] = useState<ISession>()
 
@@ -64,12 +77,13 @@ export const SessionHeader: FC<{
       }}
     >
       <Cell flexGrow={ 1 }>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center'
-        }}>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center'
+          }}
+        >
           <Box
             sx={{
               display: 'flex',
@@ -95,7 +109,6 @@ export const SessionHeader: FC<{
         </Box>
       </Cell>
       <Cell>
-        {/* Label "IMAGE" added to the left side of the icons */}
         {session.type === 'image' && (
           <Chip
             label="IMAGE"
@@ -126,102 +139,182 @@ export const SessionHeader: FC<{
         )}
       </Cell>
       <Cell>
-        <Row>
-          <Tooltip title="Open Session">
-            <Link
-              href="/files?path=%2Fsessions"
-              onClick={(e) => {
-                e.preventDefault()
-                navigate('files', {
-                  path: `/sessions/${session?.id}`
-                })
-              }}
-            >
-              <Typography
-                sx={{
-                  fontSize: "small",
-                  flexGrow: 0,
-                  textDecoration: 'underline',
-                }}
-              >
-                <Box component="span" sx={{ display: 'flex', alignItems: 'center' }}>
-                  <FolderOpenIcon sx={{ mr: 2 }} />
-                </Box>
-              </Typography>
-            </Link>
-          </Tooltip>
-          <JsonWindowLink
-            data={ session } 
+        <Tooltip title="Open Session">
+          <Link
+            href="/files?path=%2Fsessions"
+            onClick={(e) => {
+              e.preventDefault()
+              navigate('files', {
+                path: `/sessions/${session?.id}`
+              })
+            }}
           >
-            <Tooltip title="Show JSON Info">
-              <Typography
-                sx={{
-                  fontSize: "small",
-                  flexGrow: 0,
-                  textDecoration: 'underline',
-                }}
-              >
-                <Box component="span" sx={{ display: 'flex', alignItems: 'center' }}>
-                  <InfoIcon sx={{ mr: 2 }} />
-                </Box>
-              </Typography>
-            </Tooltip>
-          </JsonWindowLink>
-          <Tooltip title="Delete Session">
-            <Link
-              href="/files?path=%2Fsessions"
-              onClick={(e) => {
-                e.preventDefault()
-                setDeletingSession(session)
+            <Typography
+              sx={{
+                fontSize: "small",
+                flexGrow: 0,
+                textDecoration: 'underline',
               }}
             >
               <Box component="span" sx={{ display: 'flex', alignItems: 'center' }}>
-                <DeleteIcon sx={{ mr: 2 }} />
-              </Box>
-            </Link>
-          </Tooltip>
-          { session.lora_dir && !session.parent_bot && (
-              <Tooltip title={session.parent_bot ? "Edit Bot" : "Publish Bot"}>
-                <Link
-                  href="/create_bot"
-                  onClick={(e) => {
-                    e.preventDefault()
-                    setParams({
-                      editBot: 'yes',
-                    })
+                <FolderOpenIcon 
+                  sx={{
+                    color:theme.palette.mode === 'light' ? themeConfig.lightIcon : themeConfig.darkIcon, mr: 2,
+                    '&:hover': {
+                      color: theme.palette.mode === 'light' ? themeConfig.lightIconHover : themeConfig.darkIconHover
+                    }
                   }}
-                >
-                  <Box component="span" sx={{ display: 'flex', alignItems: 'center' }}>
-                    { session.parent_bot ? <EditIcon sx={{ mr: 2 }} /> : <PublishIcon sx={{ mr: 2 }} /> }
-                  </Box>
-                </Link>
-              </Tooltip>
-            )
-          }
-          {
-            deletingSession && (
-              <DeleteConfirmWindow
-                title={`Delete session ${deletingSession.name}?`}
-                onCancel={ () => {
-                  setDeletingSession(undefined) 
-                }}
-                onSubmit={ () => {
-                  onDeleteSessionConfirm(deletingSession.id)
+                />
+              </Box>
+            </Typography>
+          </Link>
+        </Tooltip>
+      </Cell>
+      <Cell>
+        <JsonWindowLink
+          data={ session } 
+        >
+          <Tooltip title="Show JSON Info">
+            <Typography
+              sx={{
+                fontSize: "small",
+                flexGrow: 0,
+                textDecoration: 'underline',
+              }}
+            >
+              <Box component="span" sx={{ display: 'flex', alignItems: 'center' }}>
+                <InfoIcon
+                  sx={{
+                    color:theme.palette.mode === 'light' ? themeConfig.lightIcon : themeConfig.darkIcon,
+                    mr: 2,
+                    '&:hover': {
+                      color: theme.palette.mode === 'light' ? themeConfig.lightIconHover : themeConfig.darkIconHover
+                    }
+                  }}
+                />
+              </Box>
+            </Typography>
+          </Tooltip>
+        </JsonWindowLink>
+      </Cell>
+      <Cell>
+        <Tooltip title="Delete Session">
+          <Link
+            href="/files?path=%2Fsessions"
+            onClick={(e) => {
+              e.preventDefault()
+              setDeletingSession(session)
+            }}
+          >
+            <Box component="span" sx={{ display: 'flex', alignItems: 'center' }}>
+              <DeleteIcon
+                sx={{
+                  color:theme.palette.mode === 'light' ? themeConfig.lightIcon : themeConfig.darkIcon,
+                  mr: 2,
+                  '&:hover': {
+                    color: theme.palette.mode === 'light' ? '#FF0000' : '#FF0000'
+                  }
                 }}
               />
-            )
-          }
-          <Tooltip title="Helix Docs">
-            <Link
-              href="https://docs.helix.ml/docs/overview"
-              target="_blank"
-            >
-              <Box component="span" sx={{ display: 'flex', alignItems: 'center' }}>
-                <HelpIcon sx={{ mr: 2 }} />
-              </Box>
-            </Link>
-          </Tooltip>
-        </Row>
+            </Box>
+          </Link>
+        </Tooltip>
+      </Cell>
+      {
+        session.lora_dir && !session.parent_bot && (
+          <Cell>
+            <Tooltip title={session.parent_bot ? "Edit Bot" : "Publish Bot"}>
+              <Link
+                href="/create_bot"
+                onClick={(e) => {
+                  e.preventDefault()
+                  setParams({
+                    editBot: 'yes',
+                  })
+                }}
+              >
+                {
+                  session.parent_bot ?
+                    <EditIcon
+                      sx={{
+                        color:theme.palette.mode === 'light' ? themeConfig.lightIcon : themeConfig.darkIcon, mr: 2,
+                        '&:hover': {
+                          color: theme.palette.mode === 'light' ? themeConfig.lightIconHover : themeConfig.darkIconHover
+                        }
+                      }}
+                    />
+                    :
+                    <PublishIcon
+                      sx={{
+                        color:theme.palette.mode === 'light' ? themeConfig.lightIcon : themeConfig.darkIcon, mr: 2,
+                        '&:hover': {
+                          color: theme.palette.mode === 'light' ? themeConfig.lightIconHover : themeConfig.darkIconHover
+                        }
+                      }}
+                    />
+                }
+              </Link>
+            </Tooltip>
+          </Cell>
+        )
+      }
+      {
+        deletingSession && (
+          <DeleteConfirmWindow
+            title={`Delete session ${deletingSession.name}?`}
+            onCancel={ () => {
+              setDeletingSession(undefined) 
+            }}
+            onSubmit={ () => {
+              onDeleteSessionConfirm(deletingSession.id)
+            }}
+          />
+        )
+      }
+      {
+        isOwner && (
+          <Cell>
+            <Tooltip title="Share Session">
+              <Link
+                href="#"
+                onClick={(e) => {
+                  e.preventDefault()
+                  onShare()
+                }}
+              >
+                <Box component="span" sx={{ display: 'flex', alignItems: 'center' }}>
+                  <ScreenShareIcon
+                    sx={{
+                      color:theme.palette.mode === 'light' ? themeConfig.lightIcon : themeConfig.darkIcon, mr: 2,
+                      '&:hover': {
+                        color: theme.palette.mode === 'light' ? themeConfig.lightIconHover : themeConfig.darkIconHover
+                      }
+                    }}
+                  />
+                </Box>
+              </Link>
+            </Tooltip>
+          </Cell>
+        )
+      }
+      <Cell>
+        <Tooltip title="Helix Docs">
+          <Link
+            href="https://docs.helix.ml/docs/overview"
+            target="_blank"
+          >
+            <Box component="span" sx={{ display: 'flex', alignItems: 'center' }}>
+              <HelpIcon
+                sx={{
+                  color:theme.palette.mode === 'light' ? themeConfig.lightIcon : themeConfig.darkIcon, mr: 2,
+                  '&:hover': {
+                    color: theme.palette.mode === 'light' ? themeConfig.lightIconHover : themeConfig.darkIconHover
+                  }
+                }}
+              />
+            </Box>
+          </Link>
+        </Tooltip>
       </Cell>
     </Row>
   )
