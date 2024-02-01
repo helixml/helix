ddc5f66e023fa73fd78d6d9e14e7d0dd0a207ad9
Karolis Rusenas <karolis@rusenas.dev>
2024-02-01 10:13:13 +0000

    update tool

--- api/pkg/store/store_tools.go
+++ api/pkg/store/store_tools.go
@@ -26,6 +26,22 @@ func (s *PostgresStore) CreateTool(ctx context.Context, tool *types.Tool) (*type
 	return s.GetTool(ctx, tool.ID)
 }
 
+func (s *PostgresStore) UpdateTool(ctx context.Context, tool *types.Tool) (*types.Tool, error) {
+	if tool.ID == "" {
+		return nil, fmt.Errorf("id not specified")
+	}
+
+	if tool.Owner == "" {
+		return nil, fmt.Errorf("owner not specified")
+	}
+
+	err := s.gdb.WithContext(ctx).Save(&tool).Error
+	if err != nil {
+		return nil, err
+	}
+	return s.GetTool(ctx, tool.ID)
+}
+
 func (s *PostgresStore) GetTool(ctx context.Context, id string) (*types.Tool, error) {
 	var tool types.Tool
 	err := s.gdb.WithContext(ctx).Where("id = ?", id).First(&tool).Error
