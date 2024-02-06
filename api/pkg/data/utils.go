d983347d55dd619b34222bcd4a51fcbbfce45734
Karolis Rusenas <karolis@rusenas.dev>
2024-02-06 21:58:04 +0000

    get last interactions

--- api/pkg/data/utils.go
+++ api/pkg/data/utils.go
@@ -45,6 +45,17 @@ func GetInteraction(session *types.Session, id string) (*types.Interaction, erro
 	return nil, fmt.Errorf("interaction not found: %s", id)
 }
 
+func GetLastInteractions(session *types.Session, limit int) []*types.Interaction {
+	interactions := session.Interactions
+	if len(interactions) == 0 {
+		return interactions
+	}
+	if len(interactions) < limit {
+		limit = len(interactions)
+	}
+	return interactions[len(interactions)-limit:]
+}
+
 func GetLastUserInteraction(interactions []*types.Interaction) (*types.Interaction, error) {
 	for i := len(interactions) - 1; i >= 0; i-- {
 		interaction := interactions[i]
