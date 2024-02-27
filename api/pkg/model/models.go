05d7fc763faf7602ddaefbe32bb8486daa7af2af
Karolis Rusenas <karolis@rusenas.dev>
2024-02-27 23:49:31 +0000

    cleanup

--- api/pkg/model/models.go
+++ api/pkg/model/models.go
@@ -8,23 +8,15 @@ import (
 
 func GetModel(modelName types.ModelName) (Model, error) {
 	switch modelName {
-	case types.Model_Mistral7b:
+	case types.Model_Axolotl_Mistral7b:
 		return &Mistral7bInstruct01{}, nil
-	case types.Model_SDXL:
+	case types.Model_Axolotl_SDXL:
 		return &CogSDXL{}, nil
 	case types.Model_Ollama_Mistral7b:
 		return &OllamaMistral7bInstruct01{}, nil
 	default:
 		return nil, fmt.Errorf("no model for model name %s", modelName)
 	}
-
-	// if modelName == types.Model_Mistral7b {
-	// 	return &Mistral7bInstruct01{}, nil
-	// } else if modelName == types.Model_SDXL {
-	// 	return &CogSDXL{}, nil
-	// } else {
-	// 	return nil, fmt.Errorf("no model for model name %s", modelName)
-	// }
 }
 
 // rather then keep processing model names from sessions into instances of the model struct
@@ -32,21 +24,12 @@ func GetModel(modelName types.ModelName) (Model, error) {
 // this gives us an in memory cache of model instances we can quickly lookup from
 func GetModels() (map[types.ModelName]Model, error) {
 	models := map[types.ModelName]Model{}
-	models[types.Model_Mistral7b] = &Mistral7bInstruct01{}
-	models[types.Model_SDXL] = &CogSDXL{}
+	models[types.Model_Axolotl_Mistral7b] = &Mistral7bInstruct01{}
+	models[types.Model_Axolotl_SDXL] = &CogSDXL{}
 
 	// Ollama
 	models[types.Model_Ollama_Mistral7b] = &OllamaMistral7bInstruct01{}
 	return models, nil
-}
-
-func GetModelNameForSession(sessionType types.SessionType) (types.ModelName, error) {
-	if sessionType == types.SessionTypeImage {
-		return types.Model_SDXL, nil
-	} else if sessionType == types.SessionTypeText {
-		return types.Model_Mistral7b, nil
-	}
-	return types.Model_None, fmt.Errorf("no model for session type %s", sessionType)
 }
 
 func GetLowestMemoryRequirement() (uint64, error) {
