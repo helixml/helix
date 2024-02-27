9407ce131d1e0044cb94c08b17c6994dfb8f505d
Karolis Rusenas <karolis@rusenas.dev>
2024-02-27 23:47:01 +0000

    models

---
+++ api/pkg/types/models.go
@@ -0,0 +1,48 @@
+package types
+
+import "fmt"
+
+type ModelName string
+
+const (
+	Model_None              ModelName = ""
+	Model_Axolotl_Mistral7b ModelName = "mistralai/Mistral-7B-Instruct-v0.1"
+	Model_Axolotl_SDXL      ModelName = "stabilityai/stable-diffusion-xl-base-1.0"
+	Model_Ollama_Mistral7b  ModelName = "mistral:7b-instruct"
+)
+
+func (m ModelName) String() string {
+	return string(m)
+}
+
+func (m ModelName) InferenceRuntime() InferenceRuntime {
+	switch m {
+	case // Axolotl
+		Model_Axolotl_Mistral7b,
+		Model_Axolotl_SDXL:
+		return InferenceRuntimeAxolotl
+	case // Ollama
+		Model_Ollama_Mistral7b:
+		return InferenceRuntimeOllama
+	// TODO: vllm
+	default:
+		return InferenceRuntimeAxolotl
+	}
+}
+
+func ValidateModelName(modelName string, acceptEmpty bool) (ModelName, error) {
+	switch modelName {
+	case string(Model_Axolotl_Mistral7b):
+		return Model_Axolotl_Mistral7b, nil
+	case string(Model_Ollama_Mistral7b):
+		return Model_Ollama_Mistral7b, nil
+	case string(Model_Axolotl_SDXL):
+		return Model_Axolotl_SDXL, nil
+	default:
+		if acceptEmpty && modelName == string(Model_None) {
+			return Model_None, nil
+		} else {
+			return Model_None, fmt.Errorf("invalid model name: %s", modelName)
+		}
+	}
+}
