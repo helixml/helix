67cf852828fd718b53482541b94bdd3afc5a5d33
bigadamknight <adam.knight@astonishemail.com>
2024-03-06 23:46:04 +0000

    widget progress

---
+++ frontend/src/components/widgets/Embed.tsx
@@ -0,0 +1,217 @@
+import React, { FC, useState, useEffect } from 'react'
+import Dialog from '@mui/material/Dialog'
+import Box from '@mui/material/Box'
+import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
+import { DialogTitle, DialogActions, InputAdornment, IconButton } from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search'
+import RefreshIcon from '@mui/icons-material/Refresh'
+import useThemeConfig from '../../hooks/useThemeConfig'
+
+type AnswerType = {
+  id: string
+  object: string
+  created: number
+  model: string
+  usage: {
+    prompt_tokens: number
+    completion_tokens: number
+    total_tokens: number
+  }
+  choices: {
+    message: {
+      role: string
+      content: string
+    }
+    finish_reason: string
+    index: number
+  }[]
+} | null
+
+type VerifiedSourceType = {
+  id: string
+  name: string
+  url: string
+}
+
+interface AskQuestionModalProps {
+  verifiedSources: VerifiedSourceType[]
+  onSubmitQuestion: (question: string) => Promise<AnswerType>
+  open: boolean
+  handleClose: () => void
+  answer: AnswerType
+}
+
+const AskQuestionModal: FC<AskQuestionModalProps> = ({ verifiedSources, onSubmitQuestion, open, handleClose, answer }) => {
+  const themeConfig = useThemeConfig()
+  const [question, setQuestion] = useState('')
+  const [showAnswer, setShowAnswer] = useState(false)
+
+  useEffect(() => {
+    if (answer) setShowAnswer(true)
+  }, [answer])
+
+  const handleQuestionChange = (event: React.ChangeEvent<HTMLInputElement>) => setQuestion(event.target.value)
+  const handleSubmitQuestion = async () => {
+    if (question.trim() !== '') {
+      try {
+        const submissionResult = await onSubmitQuestion(question)
+        if (submissionResult) {
+          setShowAnswer(true)
+        }
+      } catch (error) {
+        console.error('Error submitting question:', error)
+        alert('Submission failed. Please attempt again later.')
+      }
+    }
+  }
+
+  const resetToInitialState = () => {
+    setQuestion('')
+    setShowAnswer(false)
+  }
+
+  const handleKeyPress = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      handleSubmitQuestion()
+    }
+  }
+
+  return (
+    <Dialog
+      open={open}
+      onClose={() => {
+        handleClose()
+        setShowAnswer(false)
+      }}
+      aria-labelledby="ask-question-modal-title"
+      aria-describedby="ask-question-modal-description"
+      maxWidth="md"
+      fullWidth
+    >
+      <DialogTitle>
+        <Box sx={{ display: 'flex', alignItems: 'center', gap: 1, flexDirection: 'row', justifyContent: 'flex-start' }}>
+          {themeConfig.logo()}Ask Helix
+          <Box sx={{flexGrow: 1,}}></Box>
+          {showAnswer && (
+            <Box sx={{alignSelf: 'flex-end'}}>
+              <IconButton onClick={resetToInitialState} aria-label="reset">
+                <RefreshIcon color="action" />
+              </IconButton>
+            </Box>
+          )}
+        </Box>
+      </DialogTitle>
+      {!showAnswer ? (
+        <Box
+          sx={{
+            minWidth: '100%',
+            width: '100%',
+            boxShadow: 24,
+            py: 2,
+            px: 4,
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 2,
+            zIndex: 'modal',
+          }}
+        >
+          <TextField
+            autoFocus
+            margin="dense"
+            id="question"
+            label="Your Query"
+            type="text"
+            fullWidth
+            variant="standard"
+            placeholder="How to deploy my application?"
+            value={question}
+            onChange={handleQuestionChange}
+            onKeyPress={handleKeyPress}
+            InputProps={{
+              disableUnderline: true,
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton onClick={handleSubmitQuestion} aria-label="submit question" type="button">
+                    <SearchIcon
+                      color="primary"
+                      fontSize="small"
+                    />
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
+        </Box>
+      ) : (
+        <Box
+          sx={{
+            minWidth: '100%',
+            width: '100%',
+            boxShadow: 24,
+            py: 2,
+            px: 4,
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 2,
+            zIndex: 'modal',
+          }}
+        >
+          {/* Displaying verified sources and answer */}
+          <Box sx={{ typography: 'body1', my: 2 }}>Verified Sources</Box>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'flex-start',
+              gap: 1,
+            }}
+          >
+            {verifiedSources.map((source, index) => (
+              <Box
+                key={source.id}
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center',
+                  gap: 2,
+                  border: 1,
+                  borderColor: 'primary.main',
+                  borderRadius: 3,
+                  px: 2,
+                  py: 1,
+                  width: '33%',
+                  overflow: 'hidden',
+                }}
+              >
+                <Box
+                  component="a"
+                  href={source.url}
+                  sx={{
+                    color: themeConfig.darkText,
+                    textDecoration: 'none',
+                    width: '100%',
+                    textAlign: 'center',
+                  }}
+                >
+                  <Box sx={{ typography: 'body1', overflow: 'hidden', textOverflow: 'ellipsis' }}>{index + 1}. {source.name}</Box>
+                  <Box sx={{ typography: 'body1', overflow: 'hidden', textOverflow: 'ellipsis' }}>{source.url}</Box>
+                </Box>
+              </Box>
+            ))}
+          </Box>
+          <Box sx={{ typography: 'body1', my: 2 }}>Answer</Box>
+          <Box sx={{ typography: 'body2', mt: 2, mb: 4, }}>{answer?.choices[0]?.message?.content}</Box>
+        </Box>
+      )}
+      <DialogActions>
+        <Button onClick={() => {
+          handleClose()
+          setShowAnswer(false)
+        }}>Close</Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
+
+export default AskQuestionModal
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +9,21 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 8081,
   },
-  publicDir: 'assets'
+  publicDir: 'assets',
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/components/widgets/Embed.tsx'), // Adjust this path to your component
+      name: 'Embed', // The global variable name when included via a script tag
+      fileName: (format) => `embed.${format}.js`
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        },
+      },
+    },
+  },
 })
