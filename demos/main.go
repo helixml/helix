e8396091cfbd5eaa8c2516cf2044e7bd876659d4
Kai Davenport <kaiyadavenport@gmail.com>
2024-03-15 12:30:34 +0000

    boot products demo alongside the rest of the stack

---
+++ demos/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/kelseyhightower/envconfig"
+)
+
+type Config struct {
+	Port string `envconfig:"PORT" default:"8080"`
+}
+
+func main() {
+	var config Config
+	err := envconfig.Process("", &config)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/products/v1/list", listProducts).Methods("GET")
+	r.HandleFunc("/products/v1/book", bookProduct).Methods("POST")
+
+	fmt.Println("Server will listen on port", config.Port)
+	http.ListenAndServe(fmt.Sprintf(":%s", config.Port), r)
+}
