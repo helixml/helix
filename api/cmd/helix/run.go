0c8962755610a6b33c233d9634037feb5ebf047b
Luke Marsden <me@lukemarsden.net>
2024-03-21 12:15:44 +0100

    log api key

--- api/cmd/helix/run.go
+++ api/cmd/helix/run.go
@@ -122,6 +122,8 @@ func runCLI(cmd *cobra.Command, options *RunOptions) error {
 	// Set the bearer token API key
 	req.Header.Set("Authorization", "Bearer "+options.ApiKey)
 
+	fmt.Println("Using API key", options.ApiKey)
+
 	if err != nil {
 		fmt.Println("Error creating request:", err)
 		return err
