792520a5c0204fff810c8d52b725e65e2da7424d
Karolis Rusenas <karolis@rusenas.dev>
2024-03-23 19:43:44 +0000

    move into helper func

--- api/pkg/tools/utils.go
+++ api/pkg/tools/utils.go
@@ -6,6 +6,18 @@ import (
 )
 
 func unmarshalJSON(data string, v interface{}) error {
+	if strings.Contains(data, "```json") {
+		data = strings.Split(data, "```json")[1]
+	}
+	// sometimes LLMs in their wisdom puts a message after the enclosing ```json``` block
+	parts := strings.Split(data, "```")
+	data = parts[0]
+
+	// LLMs are sometimes bad at correct JSON escaping, trying to escape
+	// characters like _ that don't need to be escaped. Just remove all
+	// backslashes for now...
+	data = strings.Replace(data, "\\", "", -1)
+
 	// LLMs are sometimes bad at correct JSON escaping, trying to escape
 	// characters like _ that don't need to be escaped. Just remove all
 	// backslashes for now...
