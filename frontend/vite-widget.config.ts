bfb79a6b3e96f828ce67b7b7a26ddb5bc76d8651
Kai Davenport <kaiyadavenport@gmail.com>
2024-04-09 09:46:28 +0100

    fix frontend widget build

--- frontend/vite-widget.config.ts
+++ frontend/vite-widget.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 export default defineConfig({
   plugins: [react()],
   build: {
+    emptyOutDir: false,
     lib: {
       entry: path.resolve(__dirname, 'src/widget.tsx'),
       formats: ['iife'],
