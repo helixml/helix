5839d9d25c2f4705e7c7c527a84bb8df6e36859f
Karolis Rusenas <karolis@rusenas.dev>
2024-05-04 22:32:32 +0100

    cleanup

--- api/pkg/dataprep/qapairs/qapairs.go
+++ api/pkg/dataprep/qapairs/qapairs.go
@@ -12,20 +12,15 @@ import (
 	"text/template"
 	"time"
 
+	"github.com/helixml/helix/api/pkg/openai"
 	"github.com/helixml/helix/api/pkg/types"
-	openai "github.com/lukemarsden/go-openai2"
+
+	ext_openai "github.com/lukemarsden/go-openai2"
 	"gopkg.in/yaml.v3"
 )
 
 //go:embed qapair_config.yaml
 var qapairConfig string
-
-type Target struct {
-	Name         string `yaml:"name"`
-	ApiUrl       string `yaml:"api_url"`
-	Model        string `yaml:"model"`
-	TokenFromEnv string `yaml:"token_from_env"`
-}
 
 type Prompt struct {
 	Name       string                 `yaml:"name"`
@@ -54,7 +49,6 @@ type Log struct {
 
 type Config struct {
 	Prompts      []Prompt `yaml:"prompts"`
-	Targets      []Target `yaml:"targets"`
 	Texts        []Text   `yaml:"texts"`
 	Concurrency  int      `yaml:"concurrency"`
 	ChunkSize    int      `yaml:"chunk_size"`
@@ -120,47 +114,15 @@ func FindPrompt(name string) (Prompt, error) {
 	return Prompt{}, fmt.Errorf("could not find prompt with name %s", name)
 }
 
-func FindTarget(name string) (Target, error) {
+func Run(client openai.Client, model string, promptFilter, textFilter []string) error {
 	var config Config
 	err := yaml.Unmarshal([]byte(qapairConfig), &config)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("failed to unmarshal qapair config: %v", err)
 	}
 
-	for _, target := range config.Targets {
-		if target.Name == name {
-			return target, nil
-		}
-	}
-
-	log.Fatalf("Could not find target with name %s", name)
-	return Target{}, fmt.Errorf("could not find target with name %s", name)
-}
-
-func Run(targetFilter, promptFilter, textFilter []string) {
-	var config Config
-	err := yaml.Unmarshal([]byte(qapairConfig), &config)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-
-	targets := config.Targets
 	prompts := config.Prompts
 	texts := config.Texts
-
-	filteredTargets := []Target{}
-	if len(targetFilter) > 0 {
-		for _, name := range targetFilter {
-			for _, t := range targets {
-				if t.Name == name {
-					filteredTargets = append(filteredTargets, t)
-				}
-			}
-		}
-	} else {
-		filteredTargets = targets
-	}
 
 	filteredPrompts := []Prompt{}
 	if len(promptFilter) > 0 {
@@ -187,28 +149,24 @@ func Run(targetFilter, promptFilter, textFilter []string) {
 	} else {
 		filteredTexts = texts
 	}
-	// log.Printf("There are %d filteredPrompts", len(filteredPrompts))
-	// log.Printf("There are %d filteredTargets", len(filteredTargets))
-	// log.Printf("There are %d filteredTexts", len(filteredTexts))
 
-	for _, target := range filteredTargets {
-		for _, prompt := range filteredPrompts {
-			for _, text := range filteredTexts {
-				fmt.Printf("Running helix qapairs --target=\"%s\" --prompt=\"%s\" --text=\"%s\"\n", target.Name, prompt.Name, text.Name)
-				resp, err := Query(target, prompt, text, "", "", 0)
-				if err != nil {
-					fmt.Println("Error:", err)
-					return
-				}
-				bs, err := yaml.Marshal(resp)
-				if err != nil {
-					fmt.Println("Error:", err)
-					return
-				}
-				fmt.Println(string(bs))
+	// for _, target := range filteredTargets {
+	for _, prompt := range filteredPrompts {
+		for _, text := range filteredTexts {
+			fmt.Printf("Running helix qapairs --target=\"%s\" --prompt=\"%s\" --text=\"%s\"\n", model, prompt.Name, text.Name)
+			resp, err := Query(client, model, prompt, text, "", "", 0)
+			if err != nil {
+				return fmt.Errorf("error querying model: %v", err)
 			}
+			bs, err := yaml.Marshal(resp)
+			if err != nil {
+				return fmt.Errorf("error marshalling response to yaml (%v): %w ", resp, err)
+			}
+			fmt.Println(string(bs))
 		}
 	}
+
+	return nil
 }
 
 type TemplateData struct {
@@ -218,7 +176,7 @@ type TemplateData struct {
 	DocumentChunk   string
 }
 
-func Query(target Target, prompt Prompt, text Text, documentID, documentGroupID string, numQuestions int) ([]types.DataPrepTextQuestionRaw, error) {
+func Query(client openai.Client, model string, prompt Prompt, text Text, documentID, documentGroupID string, numQuestions int) ([]types.DataPrepTextQuestionRaw, error) {
 	// Perform the query for the given target and prompt
 
 	var contents string
@@ -273,10 +231,10 @@ func Query(target Target, prompt Prompt, text Text, documentID, documentGroupID 
 	startTime := time.Now()
 	debug := fmt.Sprintf("prompt %s", prompt.Name)
 	// try not enforcing json schema initially, only retry if we fail to parse
-	resp, err := chatWithModel(target.ApiUrl, os.Getenv(target.TokenFromEnv), target.Model, systemPrompt, userPrompt, debug, nil)
+	resp, err := chatWithModel(client, model, systemPrompt, userPrompt, debug, nil)
 	if err != nil {
 		log.Printf("ChatCompletion error non-JSON mode, trying again (%s): %v\n", debug, err)
-		resp, err = chatWithModel(target.ApiUrl, os.Getenv(target.TokenFromEnv), target.Model, systemPrompt, userPrompt, debug, prompt.JsonSchema)
+		resp, err = chatWithModel(client, model, systemPrompt, userPrompt, debug, prompt.JsonSchema)
 		if err != nil {
 			log.Printf("ChatCompletion error JSON mode, giving up, but not propagating the error further for now. (%s): %v\n", debug, err)
 			latency := time.Since(startTime).Milliseconds()
@@ -294,7 +252,7 @@ func Query(target Target, prompt Prompt, text Text, documentID, documentGroupID 
 	}
 
 	timestamp := time.Now().Unix()
-	filename := fmt.Sprintf("runs/%d_%s_%s_%s.yaml", timestamp, target.Name, prompt.Name, text.Name)
+	filename := fmt.Sprintf("runs/%d_%s_%s_%s.yaml", timestamp, model, prompt.Name, text.Name)
 
 	respBytes, err := yaml.Marshal(resp)
 	if err != nil {
@@ -303,8 +261,7 @@ func Query(target Target, prompt Prompt, text Text, documentID, documentGroupID 
 
 	logData := Log{
 		Date:      time.Now().String(),
-		ApiUrl:    target.ApiUrl,
-		Model:     target.Model,
+		Model:     model,
 		System:    systemPrompt,
 		User:      userPrompt,
 		Text:      contents,
@@ -333,36 +290,31 @@ func loadFile(filePath string) (string, error) {
 	return string(content), nil
 }
 
-func chatWithModel(apiUrl, token, model, system, user, debug string, jsonSchema map[string]interface{}) ([]types.DataPrepTextQuestionRaw, error) {
-	cfg := openai.DefaultConfig(token)
-	cfg.BaseURL = apiUrl
-	client := openai.NewClientWithConfig(cfg)
-
-	req := openai.ChatCompletionRequest{
+func chatWithModel(client openai.Client, model, system, user, debug string, jsonSchema map[string]interface{}) ([]types.DataPrepTextQuestionRaw, error) {
+	req := ext_openai.ChatCompletionRequest{
 		Model: model,
-		Messages: []openai.ChatCompletionMessage{
+		Messages: []ext_openai.ChatCompletionMessage{
 			{
-				Role:    openai.ChatMessageRoleSystem,
+				Role:    ext_openai.ChatMessageRoleSystem,
 				Content: system,
 			},
 			{
-				Role:    openai.ChatMessageRoleUser,
+				Role:    ext_openai.ChatMessageRoleUser,
 				Content: user,
 			},
 		},
 	}
 
 	if jsonSchema != nil {
-		req.ResponseFormat = &openai.ChatCompletionResponseFormat{
-			Type:   openai.ChatCompletionResponseFormatTypeJSONObject,
+		req.ResponseFormat = &ext_openai.ChatCompletionResponseFormat{
+			Type:   ext_openai.ChatCompletionResponseFormatTypeJSONObject,
 			Schema: jsonSchema,
 		}
 	}
 
 	resp, err := client.CreateChatCompletion(context.Background(), req)
 	if err != nil {
-		fmt.Printf("ChatCompletion error (%s): %v\n", debug, err)
-		return nil, err
+		return nil, fmt.Errorf("ChatCompletion error (%s): %v", debug, err)
 	}
 
 	answer := resp.Choices[0].Message.Content
