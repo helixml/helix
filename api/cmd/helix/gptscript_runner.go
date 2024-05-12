0bd08874e3e6f7ffcc7c076dca90b9f586b043b8
Karolis Rusenas <karolis@rusenas.dev>
2024-05-12 14:25:35 +0100

    stop sig

--- api/cmd/helix/gptscript_runner.go
+++ api/cmd/helix/gptscript_runner.go
@@ -1,9 +1,16 @@
 package helix
 
 import (
-	"github.com/helixml/helix/api/pkg/config"
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
+
+	"github.com/helixml/helix/api/pkg/config"
+	"github.com/helixml/helix/api/pkg/gptscript"
 )
 
 func newGptScriptRunnerCmd() *cobra.Command {
@@ -13,7 +20,7 @@ func newGptScriptRunnerCmd() *cobra.Command {
 		Long:    "Start the helix gptscript runner.",
 		Example: "TBD",
 		RunE: func(cmd *cobra.Command, _ []string) error {
-			return gptscript(cmd)
+			return gptScriptRunner(cmd)
 		},
 	}
 	return runCmd
@@ -26,5 +33,18 @@ func gptScriptRunner(_ *cobra.Command) error {
 		return err
 	}
 
-	return nil
+	runner := gptscript.NewRunner(&cfg)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
+	stopSigCh := make(chan os.Signal, 1)
+	signal.Notify(stopSigCh, syscall.SIGQUIT, syscall.SIGTERM, os.Interrupt)
+
+	go func() {
+		log.Info().Msg("received stop signal, stopping")
+		<-stopSigCh
+		cancel()
+	}()
+
+	return runner.Run(ctx)
 }
