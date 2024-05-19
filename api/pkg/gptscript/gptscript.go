41a6d3f3ddee1e134c2beedbabd8368b324e346e
Karolis Rusenas <karolis@rusenas.dev>
2024-05-19 09:45:14 +0100

    switch to req

--- api/pkg/gptscript/gptscript.go
+++ api/pkg/gptscript/gptscript.go
@@ -49,7 +49,7 @@ func (e *DefaultExecutor) ExecuteApp(ctx context.Context, app *types.GptScriptGi
 		ctx, cancel := context.WithTimeout(ctx, 15*time.Second)
 		defer cancel()
 
-		resp, err := e.pubsub.StreamRequest(ctx, pubsub.ScriptRunnerStream, pubsub.AppQueue, bts, 10*time.Second)
+		resp, err := e.pubsub.Request(ctx, pubsub.ScriptRunnerStream, pubsub.AppQueue, bts, e.cfg.GPTScript.Runner.RequestTimeout)
 		if err != nil {
 			log.Warn().Err(err).Str("app_repo", app.Repo).Msg("failed to request GPTScript app")
 			return nil, fmt.Errorf("failed to request GPTScript app: %w", err)
@@ -97,7 +97,7 @@ func (e *DefaultExecutor) ExecuteScript(ctx context.Context, script *types.GptSc
 	var retries int
 
 	resp, err := retry.DoWithData(func() ([]byte, error) {
-		resp, err := e.pubsub.StreamRequest(ctx, pubsub.ScriptRunnerStream, pubsub.ToolQueue, bts, 30*time.Second)
+		resp, err := e.pubsub.Request(ctx, pubsub.ScriptRunnerStream, pubsub.ToolQueue, bts, 30*time.Second)
 		if err != nil {
 			return nil, fmt.Errorf("failed to request GPTScript app: %w", err)
 		}
