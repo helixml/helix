127620c5d5fd7b853765bf453b68da9460ead04e
Luke Marsden <me@lukemarsden.net>
2024-06-14 16:10:58 +0100

    Stop loading sdxl by default, neatly sidestepping the headache that was warmup models over-filling GPU memory. Also fix bug that was stopping llama3:instruct getting loaded as a warmup model. Finally, fix frontend websocket bug

--- api/pkg/config/runner_config.go
+++ api/pkg/config/runner_config.go
@@ -28,14 +28,12 @@ type Models struct {
 type Runtimes struct {
 	Axolotl struct {
 		Enabled      bool          `envconfig:"RUNTIME_AXOLOTL_ENABLED" default:"true"`
-		WarmupModels []string      `envconfig:"RUNTIME_AXOLOTL_WARMUP_MODELS" default:"mistralai/Mistral-7B-Instruct-v0.1,stabilityai/stable-diffusion-xl-base-1.0"`
-		InstanceTTL  time.Duration `envconfig:"RUNTIME_AXOLOTL_INSTANCE_TTL" default:"60s"`
+		WarmupModels []string      `envconfig:"RUNTIME_AXOLOTL_WARMUP_MODELS" default:"mistralai/Mistral-7B-Instruct-v0.1"`
+		InstanceTTL  time.Duration `envconfig:"RUNTIME_AXOLOTL_INSTANCE_TTL" default:"10s"`
 	}
 	Ollama struct {
-		Enabled      bool     `envconfig:"RUNTIME_OLLAMA_ENABLED" default:"true"`
-		WarmupModels []string `envconfig:"RUNTIME_OLLAMA_WARMUP_MODELS" default:"llama3:instruct,mixtral:instruct,codellama:70b-instruct-q2_K,adrienbrault/nous-hermes2theta-llama3-8b:q8_0,phi3:instruct"`
-		// Ollama instance can be kept for much longer as it automatically unloads
-		// the model from memory when it's not used
-		InstanceTTL time.Duration `envconfig:"RUNTIME_OLLAMA_INSTANCE_TTL" default:"60s"`
+		Enabled      bool          `envconfig:"RUNTIME_OLLAMA_ENABLED" default:"true"`
+		WarmupModels []string      `envconfig:"RUNTIME_OLLAMA_WARMUP_MODELS" default:"llama3:instruct,mixtral:instruct,codellama:70b-instruct-q2_K,adrienbrault/nous-hermes2theta-llama3-8b:q8_0,phi3:instruct"`
+		InstanceTTL  time.Duration `envconfig:"RUNTIME_OLLAMA_INSTANCE_TTL" default:"10s"`
 	}
 }
