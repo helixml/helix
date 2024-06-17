b3b5eade4724653b7f2a42eac047224b73daaf43
Karolis Rusenas <karolis@rusenas.dev>
2024-06-17 08:37:43 +0100

    more

--- api/pkg/trigger/discord/trigger_discord.go
+++ api/pkg/trigger/discord/trigger_discord.go
@@ -6,7 +6,6 @@ import (
 	"strings"
 	"time"
 
-	"github.com/davecgh/go-spew/spew"
 	"github.com/helixml/helix/api/pkg/config"
 	"github.com/helixml/helix/api/pkg/store"
 	"github.com/rs/zerolog/log"
@@ -17,6 +16,8 @@ import (
 type Discord struct {
 	cfg   *config.ServerConfig
 	store store.Store
+
+	botID string
 }
 
 func New(cfg *config.ServerConfig, store store.Store) *Discord {
@@ -36,68 +37,18 @@ func (d *Discord) Start(ctx context.Context) error {
 		return fmt.Errorf("failed to create discord session: %w", err)
 	}
 
+	u, err := s.User("@me")
+	if err != nil {
+		return fmt.Errorf("error obtaining account details: %w", err)
+	}
+	d.botID = u.ID
+
 	logger := log.With().Str("trigger", "discord").Logger()
 
 	logger.Info().Msg("starting Discord bot")
 
-	// TODO:
-	// 1. Look for bot name in the message
-	// 2. Check the trigger database whether bot is configured, ignore non configured bots
-	// 3. Start the session with the bot
+	s.AddHandler(d.messageHandler)
 
-	s.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
-		if m.Author.ID == s.State.User.ID {
-			return
-		}
-
-		logger.Info().
-			Str("content", m.Content).
-			Str("bot_id", s.State.User.ID).
-			Str("message_author_id", m.Author.ID).
-			Msg("received message")
-
-		fmt.Println("XX msg", m.Content)
-		spew.Dump(m)
-		if strings.Contains(m.Content, "ping") {
-			if ch, err := s.State.Channel(m.ChannelID); err != nil || !ch.IsThread() {
-				thread, err := s.MessageThreadStartComplex(m.ChannelID, m.ID, &discordgo.ThreadStart{
-					Name:                "Pong game with " + m.Author.Username,
-					AutoArchiveDuration: 60,
-					Invitable:           false,
-					RateLimitPerUser:    10,
-				})
-				if err != nil {
-					log.Err(err).Msg("failed to create thread")
-					return
-				}
-				_, err = s.ChannelMessageSend(thread.ID, "pong")
-				if err != nil {
-					log.Err(err).Msg("failed to send message")
-				}
-
-				m.ChannelID = thread.ID
-			} else {
-				_, err = s.ChannelMessageSendReply(m.ChannelID, "pong", m.Reference())
-				if err != nil {
-					log.Err(err).Msg("failed to send message")
-				}
-
-			}
-			games[m.ChannelID] = time.Now()
-			<-time.After(timeout)
-			if time.Since(games[m.ChannelID]) >= timeout {
-				archived := true
-				locked := true
-				_, err := s.ChannelEditComplex(m.ChannelID, &discordgo.ChannelEdit{
-					Archived: &archived,
-					Locked:   &locked,
-				})
-				if err != nil {
-					log.Err(err).Msg("failed to archive channel")
-				}
-			}
-		}
-	})
 	s.Identify.Intents = discordgo.MakeIntent(discordgo.IntentsAllWithoutPrivileged)
 
 	err = s.Open()
@@ -109,4 +60,80 @@ func (d *Discord) Start(ctx context.Context) error {
 	<-ctx.Done()
 
 	return nil
+}
+
+func (d *Discord) isDirectedAtBot(m *discordgo.MessageCreate) bool {
+	if strings.Contains(m.Content, "<@!"+d.botID+">") {
+		return true
+	}
+	if m.MessageReference != nil && m.MessageReference.MessageID != "" {
+		return true
+	}
+
+	return false
+}
+
+// TODO:
+// 1. Look for bot name in the message
+// 2. Check the trigger database whether bot is configured, ignore non configured bots
+// 3. Start the session with the bot
+func (d *Discord) messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+	logger := log.With().Str("trigger", "discord").Logger()
+
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	logger.Info().
+		Str("content", m.Content).
+		Str("bot_id", s.State.User.ID).
+		Str("message_author_id", m.Author.ID).
+		Msg("received message")
+
+	if !d.isDirectedAtBot(m) {
+		logger.Debug().Msg("message not directed at bot")
+		return
+	}
+
+	// fmt.Println("XX msg", m.Content)
+	// spew.Dump(m)
+	if strings.Contains(m.Content, "ping") {
+		if ch, err := s.State.Channel(m.ChannelID); err != nil || !ch.IsThread() {
+			thread, err := s.MessageThreadStartComplex(m.ChannelID, m.ID, &discordgo.ThreadStart{
+				Name:                "Pong game with " + m.Author.Username,
+				AutoArchiveDuration: 60,
+				Invitable:           false,
+				RateLimitPerUser:    10,
+			})
+			if err != nil {
+				log.Err(err).Msg("failed to create thread")
+				return
+			}
+			_, err = s.ChannelMessageSend(thread.ID, "pong")
+			if err != nil {
+				log.Err(err).Msg("failed to send message")
+			}
+
+			m.ChannelID = thread.ID
+		} else {
+			_, err = s.ChannelMessageSendReply(m.ChannelID, "pong", m.Reference())
+			if err != nil {
+				log.Err(err).Msg("failed to send message")
+			}
+
+		}
+		games[m.ChannelID] = time.Now()
+		<-time.After(timeout)
+		if time.Since(games[m.ChannelID]) >= timeout {
+			archived := true
+			locked := true
+			_, err := s.ChannelEditComplex(m.ChannelID, &discordgo.ChannelEdit{
+				Archived: &archived,
+				Locked:   &locked,
+			})
+			if err != nil {
+				log.Err(err).Msg("failed to archive channel")
+			}
+		}
+	}
 }
