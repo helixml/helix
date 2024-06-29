be8b3e620e8e6882b7da5883b16ee0e8345cfc85
Karolis Rusenas <karolis@rusenas.dev>
2024-06-29 22:32:53 +0100

    handler

--- api/pkg/server/runner_handlers.go
+++ api/pkg/server/runner_handlers.go
@@ -7,7 +7,10 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
+	openai "github.com/lukemarsden/go-openai2"
+	"github.com/rs/zerolog/log"
 
+	"github.com/helixml/helix/api/pkg/data"
 	"github.com/helixml/helix/api/pkg/types"
 )
 
@@ -64,7 +67,94 @@ func (apiServer *HelixAPIServer) runnerLLMInferenceRequestHandler(res http.Respo
 		return nil, nil
 	}
 
-	// if nextSession is nil - we will write null to the runner and it is setup
-	// to regard that as an error (this means we don't need to write http error codes anymore)
-	return nextSession, nil
+	chatCompletionsRequest, err := sessionToChatCompletion(nextSession)
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.RunnerLLMInferenceRequest{
+		UserID:    nextSession.Owner,
+		SessionID: nextSession.ID,
+		Request:   chatCompletionsRequest,
+	}, nil
+}
+
+func sessionToChatCompletion(session *types.Session) (*openai.ChatCompletionRequest, error) {
+	var messages []openai.ChatCompletionMessage
+
+	// Adjust length
+	var interactions []*types.Interaction
+	if len(session.Interactions) > 10 {
+		first, err := data.GetFirstUserInteraction(session.Interactions)
+		if err != nil {
+			log.Err(err).Msg("error getting first user interaction")
+		} else {
+			interactions = append(interactions, first)
+			interactions = append(interactions, data.GetLastInteractions(session, 10)...)
+		}
+	} else {
+		interactions = session.Interactions
+	}
+
+	// Adding the system prompt first
+	if session.Metadata.SystemPrompt != "" {
+		messages = append(messages, openai.ChatCompletionMessage{
+			Role:    openai.ChatMessageRoleUser,
+			Content: session.Metadata.SystemPrompt,
+		})
+	}
+
+	for _, interaction := range interactions {
+		switch interaction.Creator {
+
+		case types.CreatorTypeUser:
+			messages = append(messages, openai.ChatCompletionMessage{
+				Role:    openai.ChatMessageRoleUser,
+				Content: interaction.Message,
+			})
+		case types.CreatorTypeSystem:
+			messages = append(messages, openai.ChatCompletionMessage{
+				Role:    openai.ChatMessageRoleAssistant,
+				Content: interaction.Message,
+			})
+		case types.CreatorTypeTool:
+			messages = append(messages, openai.ChatCompletionMessage{
+				Role:       openai.ChatMessageRoleUser,
+				Content:    interaction.Message,
+				ToolCalls:  interaction.ToolCalls,
+				ToolCallID: interaction.ToolCallID,
+			})
+		}
+	}
+
+	var (
+		responseFormat *openai.ChatCompletionResponseFormat
+		tools          []openai.Tool
+		toolChoice     any
+	)
+
+	// If the last interaction has response format, use it
+	last, _ := data.GetLastSystemInteraction(interactions)
+	if last != nil && last.ResponseFormat.Type == types.ResponseFormatTypeJSONObject {
+		responseFormat = &openai.ChatCompletionResponseFormat{
+			Type:   openai.ChatCompletionResponseFormatTypeJSONObject,
+			Schema: last.ResponseFormat.Schema,
+		}
+	}
+
+	if last != nil && len(last.Tools) > 0 {
+		tools = last.Tools
+		toolChoice = last.ToolChoice
+	}
+
+	// TODO: temperature, etc.
+
+	return &openai.ChatCompletionRequest{
+		Model:          string(session.ModelName),
+		Stream:         session.Metadata.Stream,
+		Messages:       messages,
+		ResponseFormat: responseFormat,
+		Tools:          tools,
+		ToolChoice:     toolChoice,
+	}, nil
 }
