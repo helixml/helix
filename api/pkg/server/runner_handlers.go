47ac94e74d3816d270dda47af258d70c7873e4d6
Karolis Rusenas <karolis@rusenas.dev>
2024-07-05 16:16:30 +0100

    move handlers

--- api/pkg/server/runner_handlers.go
+++ api/pkg/server/runner_handlers.go
@@ -1,9 +1,11 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -164,4 +166,141 @@ func sessionToChatCompletion(session *types.Session) (*openai.ChatCompletionRequ
 		Tools:          tools,
 		ToolChoice:     toolChoice,
 	}, nil
+}
+
+func (apiServer *HelixAPIServer) getNextRunnerSession(res http.ResponseWriter, req *http.Request) (*types.Session, error) {
+	vars := mux.Vars(req)
+	runnerID := vars["runnerid"]
+	if runnerID == "" {
+		return nil, fmt.Errorf("cannot get next session without runner id")
+	}
+
+	sessionMode, err := types.ValidateSessionMode(req.URL.Query().Get("mode"), true)
+	if err != nil {
+		return nil, err
+	}
+
+	sessionType, err := types.ValidateSessionType(req.URL.Query().Get("type"), true)
+	if err != nil {
+		return nil, err
+	}
+
+	modelName, err := types.TransformModelName(req.URL.Query().Get("model_name"), true)
+	if err != nil {
+		return nil, err
+	}
+
+	loraDir := req.URL.Query().Get("lora_dir")
+
+	memory := uint64(0)
+	memoryString := req.URL.Query().Get("memory")
+	if memoryString != "" {
+		memory, err = strconv.ParseUint(memoryString, 10, 64)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	// there are multiple entries for this param all of the format:
+	// model_name:mode
+	reject := []types.SessionFilterModel{}
+	rejectPairs, ok := req.URL.Query()["reject"]
+
+	if ok && len(rejectPairs) > 0 {
+		for _, rejectPair := range rejectPairs {
+			triple := strings.Split(rejectPair, ":")
+			var rejectModelName types.ModelName
+			var rejectModelMode types.SessionMode
+			var rejectLoraDir string
+			var err error
+			if len(triple) == 4 {
+				rejectModelName, err = types.TransformModelName(triple[0]+":"+triple[1], false)
+				if err != nil {
+					return nil, err
+				}
+				rejectModelMode, err = types.ValidateSessionMode(triple[2], false)
+				if err != nil {
+					return nil, err
+				}
+				rejectLoraDir = triple[3]
+			} else if len(triple) == 3 {
+				rejectModelName, err = types.TransformModelName(triple[0], false)
+				if err != nil {
+					return nil, err
+				}
+				rejectModelMode, err = types.ValidateSessionMode(triple[1], false)
+				if err != nil {
+					return nil, err
+				}
+				rejectLoraDir = triple[2]
+			} else {
+				return nil, fmt.Errorf("invalid reject pair: %s", rejectPair)
+			}
+			reject = append(reject, types.SessionFilterModel{
+				ModelName: rejectModelName,
+				Mode:      rejectModelMode,
+				LoraDir:   rejectLoraDir,
+			})
+		}
+	}
+
+	older := req.URL.Query().Get("older")
+
+	var olderDuration time.Duration
+	if older != "" {
+		olderDuration, err = time.ParseDuration(older)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	filter := types.SessionFilter{
+		Mode:      sessionMode,
+		Type:      sessionType,
+		ModelName: modelName,
+		Memory:    memory,
+		Reject:    reject,
+		LoraDir:   loraDir,
+		Older:     types.Duration(olderDuration),
+	}
+
+	// alow the worker to filter what tasks it wants
+	// if any of these values are defined then we will only consider those in the response
+	nextSession, err := apiServer.Controller.ShiftSessionQueue(req.Context(), filter, runnerID)
+	if err != nil {
+		return nil, err
+	}
+
+	// if nextSession is nil - we will write null to the runner and it is setup
+	// to regard that as an error (this means we don't need to write http error codes anymore)
+	return nextSession, nil
+}
+
+func (apiServer *HelixAPIServer) handleRunnerResponse(res http.ResponseWriter, req *http.Request) (*types.RunnerTaskResponse, error) {
+	taskResponse := &types.RunnerTaskResponse{}
+	err := json.NewDecoder(req.Body).Decode(taskResponse)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := apiServer.Controller.HandleRunnerResponse(req.Context(), taskResponse)
+	if err != nil {
+		log.Error().Err(err).Str("session_id", taskResponse.SessionID).Msg("failed to handle runner response")
+		return nil, err
+	}
+	return resp, nil
+}
+
+func (apiServer *HelixAPIServer) handleRunnerMetrics(res http.ResponseWriter, req *http.Request) (*types.RunnerState, error) {
+	runnerState := &types.RunnerState{}
+	err := json.NewDecoder(req.Body).Decode(runnerState)
+	if err != nil {
+		return nil, err
+	}
+
+	runnerState, err = apiServer.Controller.AddRunnerMetrics(req.Context(), runnerState)
+	if err != nil {
+		return nil, err
+	}
+	return runnerState, nil
 }
