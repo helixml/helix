fa3e691d1d6ba35593eabb2055dff26f4871381b
Karolis Rusenas <karolis@rusenas.dev>
2024-07-05 09:36:29 +0100

    move

--- api/pkg/runner/model_instance.go
+++ api/pkg/runner/model_instance.go
@@ -15,16 +15,13 @@ type ModelInstance interface {
 	GetState() (*types.ModelInstanceState, error)
 
 	Start(ctx context.Context) error
+	Stop() error
+	Done() <-chan bool
 
+	// TODO: remove all below
 	NextSession() *types.Session
 	SetNextSession(session *types.Session)
-
 	QueueSession(session *types.Session, isInitialSession bool)
 	GetQueuedSession() *types.Session
-
 	AssignSessionTask(ctx context.Context, session *types.Session) (*types.RunnerTask, error)
-
-	Stop() error
-
-	Done() <-chan bool
 }
