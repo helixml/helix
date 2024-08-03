45d6828adf31d258338223de3d921879781b1d57
Karolis Rusenas <karolis@rusenas.dev>
2024-08-03 20:40:32 +0300

    path split

--- api/pkg/server/session_handlers.go
+++ api/pkg/server/session_handlers.go
@@ -9,6 +9,60 @@ import (
 	"github.com/helixml/helix/api/pkg/types"
 )
 
+// startSessionHandler godoc
+// @Summary Start new text completion session
+// @Description Start new text completion session. Can be used to start or continue a session with the Helix API.
+// @Tags    chat
+
+// @Success 200 {object} types.OpenAIResponse
+// @Param request    body types.SessionChatRequest true "Request body with the message and model to start chat completion.")
+// @Router /api/v1/sessions/chat [post]
+// @Security BearerAuth
+func (s *HelixAPIServer) startChatSessionHandler(rw http.ResponseWriter, req *http.Request) {
+
+	var startReq types.SessionChatRequest
+	err := json.NewDecoder(io.LimitReader(req.Body, 10*MEGABYTE)).Decode(&startReq)
+	if err != nil {
+		http.Error(rw, "invalid request body: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if len(startReq.Messages) == 0 {
+		http.Error(rw, "messages must not be empty", http.StatusBadRequest)
+		return
+	}
+
+	// If more than 1, also not allowed just yet for simplification
+	if len(startReq.Messages) > 1 {
+		http.Error(rw, "only 1 message is allowed for now", http.StatusBadRequest)
+		return
+	}
+
+	ctx := req.Context()
+	user := getRequestUser(req)
+
+	status, err := s.Controller.GetStatus(req.Context(), user)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// For finetunes, legacy route
+	if startReq.LoraDir != "" {
+		s.startChatSessionLegacyHandler(req.Context(), user, &startReq, req, rw)
+		return
+	}
+
+	// Default to text
+	if startReq.Type == "" {
+		startReq.Type = types.SessionTypeText
+	}
+
+	if startReq.SessionID == "" {
+
+	}
+}
+
 // startLearnSessionHandler godoc
 // @Summary Start new fine tuning and/or rag source generation session
 // @Description Start new fine tuning and/or RAG source generation session
