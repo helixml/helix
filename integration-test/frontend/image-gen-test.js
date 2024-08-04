6c1d9facfa2ca4078018f02ed953bc01711d6863
Obi <obianuoobinna@gmail.com>
2024-08-04 16:14:41 +0100

    image generation test

---
+++ integration-test/frontend/image-gen-test.js
@@ -0,0 +1,46 @@
+import puppeteer from 'puppeteer'
+
+
+(async () => {
+  const browser = await puppeteer.launch({headless: false });
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1280, height: 800 });
+
+  await page.goto('http://app.tryhelix.ai');
+ 
+
+  await page.waitForSelector('#login-button', { visible: true });
+  await page.click('#login-button');
+  await page.screenshot({ path: 'screenshots/after-click-login-button.png', fullPage: true });
+ 
+  await page.waitForSelector('#username', { visible: true });
+  await page.type('#username', 'obianuoobinna@gmail.com');
+  await page.screenshot({ path: 'screenshots/after-type-username.png', fullPage: true });
+ 
+  await page.type('#password', 'password');
+  await page.screenshot({ path: 'screenshots/after-type-username.png', fullPage: true });
+
+  await page.click('#kc-login');
+  await page.screenshot({ path: 'screenshots/after-click-login.png', fullPage: true });
+
+  await page.waitForNavigation()
+
+  await page.waitForSelector('#create-button', { visible: true, timeout: 60000  });
+  await page.click('#create-button');
+  await page.screenshot({ path: 'screenshots/after-click-create-button.png', fullPage: true });
+
+
+  await page.waitForSelector('#textEntry', { visible: true });
+  await page.click('#textEntry');
+  await page.type('#textEntry', 'create an image for an interior design for a [adjective describing luxury] master bedroom, featuring [materials] furniture, [style keywords]');
+  await page.screenshot({ path: 'screenshots/after-typing-text-input.png', fullPage: true });
+
+  await page.click('#sendButton');
+  await page.waitForNetworkIdle().catch(() => {}); // works if page takes time to load
+  await page.screenshot({ path: 'screenshots/after-click-sendButton.png', fullPage: true });
+
+
+
+  await browser.close();
+})();
+
--- integration-test/frontend/test.js
+++ integration-test/frontend/test.js
@@ -1,5 +1,5 @@
 import puppeteer from 'puppeteer'
-import bluebird from 'bluebird'
+
 
 (async () => {
   const browser = await puppeteer.launch({headless: false });
