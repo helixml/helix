0f84a3a4bd9287c56f232246db513e15b5e61aa5
Karolis Rusenas <karolis@rusenas.dev>
2024-08-06 13:49:40 +0300

    client

--- api/pkg/openai/helix_openai_client.go
+++ api/pkg/openai/helix_openai_client.go
@@ -174,6 +174,32 @@ func (c *InternalHelixServer) CreateChatCompletionStream(ctx context.Context, re
 	return client.CreateChatCompletionStream(ctx, request)
 }
 
+// NewOpenAIStreamingAdapter returns a new OpenAI streaming adapter which allows
+// to write into the io.Writer and read from the stream directly
+func NewOpenAIStreamingAdapter(req openai.ChatCompletionRequest) (*openai.ChatCompletionStream, io.Writer, error) {
+	pr, pw := io.Pipe()
+
+	ht := &helixTransport{
+		reader: pr,
+		writer: pw,
+	}
+
+	// Create a fake HTTP client with a custom transport that will be feeding the stream
+	config := openai.DefaultConfig("helix")
+	config.HTTPClient = &http.Client{
+		Transport: ht,
+	}
+
+	client := openai.NewClientWithConfig(config)
+
+	stream, err := client.CreateChatCompletionStream(context.Background(), req)
+	if err != nil {
+		return nil, nil, fmt.Errorf("error creating chat completion stream: %w", err)
+	}
+
+	return stream, pw, nil
+}
+
 type helixTransport struct {
 	reader *io.PipeReader
 	writer *io.PipeWriter
