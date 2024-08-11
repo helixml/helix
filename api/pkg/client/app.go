86b32fe80c9011754a48128420a28b047da05932
Karolis Rusenas <karolis@rusenas.dev>
2024-08-11 22:05:06 +0100

    create app

--- api/pkg/client/app.go
+++ api/pkg/client/app.go
@@ -1,8 +1,11 @@
 package client
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
+	"net/http"
 
 	"github.com/helixml/helix/api/pkg/types"
 )
@@ -29,4 +32,34 @@ func (c *Client) ListApps(f *AppFilter) ([]*types.App, error) {
 	}
 
 	return apps, nil
+}
+
+func (c *Client) CreateApp(app *types.App) (*types.App, error) {
+	bts, err := json.Marshal(app)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := c.httpClient.Post(c.url+"/apps", "application/json", bytes.NewBuffer(bts))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("failed to create app, status code: %d", resp.StatusCode)
+	}
+
+	bts, err = io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var createdApp types.App
+	err = json.Unmarshal(bts, &createdApp)
+	if err != nil {
+		return nil, err
+	}
+
+	return &createdApp, nil
 }
