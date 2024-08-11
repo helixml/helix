c24b7d943ca1a31410eaf05e37f15647f8357ab5
Karolis Rusenas <karolis@rusenas.dev>
2024-08-11 22:29:24 +0100

    list

--- api/pkg/cli/app/list.go
+++ api/pkg/cli/app/list.go
@@ -12,13 +12,8 @@ import (
 // listCmd represents the list command
 var listCmd = &cobra.Command{
 	Use:   "list",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "List helix apps",
+	Long:  ``,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		apiClient, err := client.NewClientFromEnv()
 		if err != nil {
