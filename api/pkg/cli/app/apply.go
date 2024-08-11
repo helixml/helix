dc50481e12e4a5b8a3ca770062f886e6f3af56c1
Karolis Rusenas <karolis@rusenas.dev>
2024-08-11 21:37:27 +0100

    cli skeleton

---
+++ api/pkg/cli/app/apply.go
@@ -0,0 +1,36 @@
+package app
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// applyCmd represents the apply command
+var applyCmd = &cobra.Command{
+	Use:   "apply",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("apply called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(applyCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// applyCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// applyCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
