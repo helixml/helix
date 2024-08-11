ab18da627bafedb00e6affa12c04dab475beb00a
Karolis Rusenas <karolis@rusenas.dev>
2024-08-11 22:06:35 +0100

    client

--- api/pkg/client/client.go
+++ api/pkg/client/client.go
@@ -1,16 +1,27 @@
 package client
 
-import "net/http"
+import (
+	"net/http"
 
-// Client is the client for the helix api
-type Client struct {
+	"github.com/helixml/helix/api/pkg/types"
+)
+
+type Client interface {
+	CreateApp(app *types.App) (*types.App, error)
+	UpdateApp(app *types.App) (*types.App, error)
+	DeleteApp(appID string) error
+	ListApps(f *AppFilter) ([]*types.App, error)
+}
+
+// HelixClient is the client for the helix api
+type HelixClient struct {
 	httpClient *http.Client
 	apiKey     string
 	url        string
 }
 
-func NewClient(url, apiKey string) *Client {
-	return &Client{
+func NewClient(url, apiKey string) *HelixClient {
+	return &HelixClient{
 		httpClient: http.DefaultClient,
 		apiKey:     apiKey,
 		url:        url,
