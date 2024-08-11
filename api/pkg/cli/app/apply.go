36d4f012c22b9363e2a3e1c49618fe940b2b249a
Karolis Rusenas <karolis@rusenas.dev>
2024-08-11 23:02:13 +0100

    apply

--- api/pkg/cli/app/apply.go
+++ api/pkg/cli/app/apply.go
@@ -6,31 +6,36 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type applyOptions struct {
+	filename string
+}
+
 // applyCmd represents the apply command
 var applyCmd = &cobra.Command{
 	Use:   "apply",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Create or update an application",
+	Long:  `Create or update an application.`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		filename, err := cmd.Flags().GetString("filename")
+		if err != nil {
+			return err
+		}
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("apply called")
+		if filename == "" {
+			return fmt.Errorf("filename is required")
+		}
+
+		fmt.Println("apply called with filename: ", filename)
+		return nil
 	},
 }
 
+func NewApplyCmd() *cobra.Command {
+	return applyCmd
+}
+
 func init() {
-	rootCmd.AddCommand(applyCmd)
+	rootCmd.AddCommand(NewApplyCmd())
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// applyCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// applyCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	applyCmd.Flags().StringP("filename", "f", "", "Filename to apply")
 }
