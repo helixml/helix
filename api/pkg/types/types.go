244f692864a1e12a046f441a5fe91181876889e0
Karolis Rusenas <karolis@rusenas.dev>
2024-08-17 00:02:27 +0100

    types

--- api/pkg/types/types.go
+++ api/pkg/types/types.go
@@ -931,6 +931,10 @@ type AssistantConfig struct {
 	// the data entity ID that we have created for the lora fine tune
 	LoraID string `json:"lora_id" yaml:"lora_id"`
 
+	// Name of the knowledge, should be declared in the knowledge section
+	// of the AppHelixConfig struct
+	Knowledge string `json:"knowledge" yaml:"knowledge"`
+
 	// Template for determining if the request is actionable or informative
 	IsActionableTemplate string `json:"is_actionable_template" yaml:"is_actionable_template"`
 
@@ -955,6 +959,7 @@ type AppHelixConfig struct {
 	ExternalURL string            `json:"external_url" yaml:"external_url"`
 	Assistants  []AssistantConfig `json:"assistants" yaml:"assistants"`
 	Triggers    []Trigger         `json:"triggers" yaml:"triggers"`
+	Knowledge   []Knowledge       `json:"knowledge" yaml:"knowledge"`
 }
 
 type AppGithubConfigUpdate struct {
