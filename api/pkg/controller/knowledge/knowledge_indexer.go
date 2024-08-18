eac8e2ca75e34ad17ca2b039583330632e12aceb
Karolis Rusenas <karolis@rusenas.dev>
2024-08-18 16:32:32 +0100

    more

--- api/pkg/controller/knowledge/knowledge_indexer.go
+++ api/pkg/controller/knowledge/knowledge_indexer.go
@@ -3,7 +3,10 @@ package knowledge
 import (
 	"context"
 	"fmt"
+	"io"
+	"net/http"
 
+	"github.com/helixml/helix/api/pkg/extract"
 	"github.com/helixml/helix/api/pkg/store"
 	"github.com/helixml/helix/api/pkg/types"
 	"github.com/rs/zerolog/log"
@@ -65,8 +68,76 @@ func (r *Reconciler) extractText(ctx context.Context, k *types.Knowledge) (*inde
 	}
 }
 
-func (r *Reconciler) extractDataFromWeb(ctx context.Context, k *types.Knowledge) (*indexerData, error) {
+func (r *Reconciler) extractDataFromWeb(ctx context.Context, k *types.Knowledge) ([]*indexerData, error) {
+	if k.Source.Web == nil {
+		return nil, fmt.Errorf("no web source defined")
+	}
 
+	var result []*indexerData
+
+	if len(k.Source.Web.URLs) == 0 {
+		return result, nil
+	}
+
+	// Optional mode to disable text extractor and chunking,
+	// useful when the indexing server will know how to handle
+	// raw data directly
+	extractorEnabled := true
+
+	if k.RAGSettings.DisableChunking {
+		extractorEnabled = false
+	}
+
+	for _, u := range k.Source.Web.URLs {
+		if extractorEnabled {
+			extracted, err := r.extractor.Extract(ctx, &extract.ExtractRequest{
+				URL: u,
+			})
+			if err != nil {
+				return nil, fmt.Errorf("failed to extract data from %s, error: %w", u, err)
+			}
+
+			result = append(result, &indexerData{
+				Data:   []byte(extracted),
+				Source: u,
+			})
+
+			continue
+		}
+
+		bts, err := r.downloadDirectly(ctx, k, u)
+		if err != nil {
+			return nil, fmt.Errorf("failed to download data from %s, error: %w", u, err)
+		}
+
+		result = append(result, &indexerData{
+			Data:   bts,
+			Source: u,
+		})
+	}
+
+	return result, nil
+}
+
+func (r *Reconciler) downloadDirectly(ctx context.Context, k *types.Knowledge, u string) ([]byte, error) {
+	// Extractor and indexer disabled, downloading directly
+	req, err := http.NewRequest(http.MethodGet, u, http.NoBody)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get %s, error: %w", u, err)
+	}
+
+	resp, err := r.httpClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to download, error: %w", err)
+	}
+	defer resp.Body.Close()
+
+	bts, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read body, error: %w", err)
+	}
+
+	return bts, nil
 }
 
 func (r *Reconciler) indexData(ctx context.Context, k *types.Knowledge) error {
@@ -77,5 +148,6 @@ func (r *Reconciler) indexData(ctx context.Context, k *types.Knowledge) error {
 // This might be a text/html/pdf but it could also be something else
 // for example an sqlite database.
 type indexerData struct {
-	Data []byte
+	Source string
+	Data   []byte
 }
