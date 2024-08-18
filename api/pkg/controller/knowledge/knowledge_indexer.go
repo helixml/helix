52bc86584b227e1ff94ecb30ac9e9a5f63269bff
Karolis Rusenas <karolis@rusenas.dev>
2024-08-18 15:50:22 +0100

    indexer placeholder

--- api/pkg/controller/knowledge/knowledge_indexer.go
+++ api/pkg/controller/knowledge/knowledge_indexer.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/helixml/helix/api/pkg/store"
 	"github.com/helixml/helix/api/pkg/types"
+	"github.com/rs/zerolog/log"
 )
 
 func (r *Reconciler) index(ctx context.Context) error {
@@ -16,8 +17,30 @@ func (r *Reconciler) index(ctx context.Context) error {
 		return fmt.Errorf("failed to get knowledge entries, error: %w", err)
 	}
 
+	for _, k := range data {
+		r.wg.Add(1)
+
+		go func(knowledge *types.Knowledge) {
+			log.
+				Info().
+				Str("knowledge_id", knowledge.ID).
+				Msg("indexing knowledge")
+
+			err := r.indexKnowledge(ctx, knowledge)
+			if err != nil {
+				log.
+					Warn().
+					Err(err).
+					Str("knowledge_id", knowledge.ID).
+					Msg("failed to index knowledge")
+			}
+		}(k)
+	}
+
+	return nil
 }
 
 func (r *Reconciler) indexKnowledge(ctx context.Context, k *types.Knowledge) error {
-
+	// TODO
+	return nil
 }
