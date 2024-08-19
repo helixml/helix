2e9aaeb22ca11db18e700db92cbc9dbd58f2e486
Karolis Rusenas <karolis@rusenas.dev>
2024-08-19 08:40:56 -0400

    add aliases

--- api/pkg/cli/app/cmd.go
+++ api/pkg/cli/app/cmd.go
@@ -3,9 +3,10 @@ package app
 import "github.com/spf13/cobra"
 
 var rootCmd = &cobra.Command{
-	Use:   "app",
-	Short: "Helix app management",
-	Long:  `TODO`,
+	Use:     "app",
+	Short:   "Helix app management",
+	Aliases: []string{"a"},
+	Long:    `TODO`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// Do Stuff Here
 	},
