3040affbc3ef9200f86e6a103ba56378157f3335
Luke Marsden <me@lukemarsden.net>
2024-08-20 15:42:22 +0100

    factor json munging code into one place, add a test that covers the case we see where model includes backticks but not the word json at the start, make the test pass

--- api/pkg/tools/utils.go
+++ api/pkg/tools/utils.go
@@ -5,7 +5,12 @@ import (
 	"strings"
 )
 
-func unmarshalJSON(data string, v interface{}) error {
+func AttemptFixJSON(data string) string {
+	// sometimes LLM just gives us a single ``` line at the start; just strip that off
+	if strings.HasPrefix(data, "```\n") {
+		data = strings.Split(data, "```\n")[1]
+	}
+
 	if strings.Contains(data, "```json") {
 		data = strings.Split(data, "```json")[1]
 	}
@@ -18,10 +23,10 @@ func unmarshalJSON(data string, v interface{}) error {
 	// backslashes for now...
 	data = strings.Replace(data, "\\", "", -1)
 
-	// LLMs are sometimes bad at correct JSON escaping, trying to escape
-	// characters like _ that don't need to be escaped. Just remove all
-	// backslashes for now...
-	data = strings.Replace(data, "\\", "", -1)
+	return data
+}
 
-	return json.Unmarshal([]byte(data), v)
+func unmarshalJSON(data string, v interface{}) error {
+	fixedData := AttemptFixJSON(data)
+	return json.Unmarshal([]byte(fixedData), v)
 }
