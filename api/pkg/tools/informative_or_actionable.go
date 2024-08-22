14e5e37015d23721fb705704338c10f26d9ed9a0
Karolis Rusenas <karolis@rusenas.dev>
2024-08-22 00:02:27 -0400

    ctx setup

--- api/pkg/tools/informative_or_actionable.go
+++ api/pkg/tools/informative_or_actionable.go
@@ -112,18 +112,23 @@ func (c *ChainStrategy) isActionable(ctx context.Context, sessionID, interaction
 	}
 
 	// Required for the correct openai context to be set
-	ctx = oai.SetContextValues(ctx, "system", sessionID, interactionID)
+	ctx = oai.SetContextValues(ctx, &oai.ContextValues{
+		OwnerID:       "system",
+		SessionID:     sessionID,
+		InteractionID: interactionID,
+		Step:          types.LLMCallStepIsActionable,
+	})
 
 	resp, err := c.apiClient.CreateChatCompletion(ctx, req)
 	if err != nil {
 		return nil, fmt.Errorf("failed to get response from inference API: %w", err)
 	}
 
-	c.wg.Add(1)
-	go func() {
-		defer c.wg.Done()
-		c.logLLMCall(sessionID, interactionID, types.LLMCallStepIsActionable, &req, &resp, time.Since(started).Milliseconds())
-	}()
+	// c.wg.Add(1)
+	// go func() {
+	// 	defer c.wg.Done()
+	// 	c.logLLMCall(sessionID, interactionID, types.LLMCallStepIsActionable, &req, &resp, time.Since(started).Milliseconds())
+	// }()
 
 	var actionableResponse IsActionableResponse
 
