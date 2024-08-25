c92a47baf6ef398986169977ae4fd44e1c8a9ca3
Karolis Rusenas <karolis@rusenas.dev>
2024-08-25 10:43:56 -0400

    test crawler

--- api/pkg/controller/knowledge/crawler/default_test.go
+++ api/pkg/controller/knowledge/crawler/default_test.go
@@ -2,7 +2,7 @@ package crawler
 
 import (
 	"context"
-	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/helixml/helix/api/pkg/types"
@@ -25,5 +25,25 @@ func TestDefault_Crawl(t *testing.T) {
 	docs, err := d.Crawl(context.Background())
 	require.NoError(t, err)
 
-	fmt.Println(docs[0].Content)
+	const (
+		apiToolsText          = `By connecting to external APIs, your AI can access up-to-date information that may not be present in its training data`
+		privateDeploymentText = `The stack might take a minute to boot up`
+	)
+
+	var (
+		apiToolsTextFound          bool
+		privateDeploymentTextFound bool
+	)
+
+	for _, doc := range docs {
+		if strings.Contains(doc.Content, apiToolsText) {
+			apiToolsTextFound = true
+		}
+		if strings.Contains(doc.Content, privateDeploymentText) {
+			privateDeploymentTextFound = true
+		}
+	}
+
+	require.True(t, apiToolsTextFound, "api tools text not found")
+	require.True(t, privateDeploymentTextFound, "private deployment text not found")
 }
