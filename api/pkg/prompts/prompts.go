d4343c5d7c00c58dc6f618f28579e40772ec5077
Karolis Rusenas <karolis@rusenas.dev>
2024-08-26 23:15:26 +0100

    test coverage, templates

--- api/pkg/prompts/prompts.go
+++ api/pkg/prompts/prompts.go
@@ -3,11 +3,8 @@ package prompts
 import (
 	"bytes"
 	_ "embed"
-	"fmt"
 	"strings"
 	"text/template"
-
-	"gopkg.in/yaml.v3"
 
 	"github.com/helixml/helix/api/pkg/prompts/templates"
 )
@@ -33,43 +30,8 @@ type PromptConfig struct {
 	Prompts []Prompt `yaml:"prompts"`
 }
 
-//go:embed prompts.yaml
-var promptConfigString string
-
-var allPrompts []Prompt
-
-func getPrompts() ([]Prompt, error) {
-	if allPrompts != nil {
-		return allPrompts, nil
-	}
-	var config PromptConfig
-	err := yaml.Unmarshal([]byte(promptConfigString), &config)
-	if err != nil {
-		return nil, err
-	}
-	allPrompts = config.Prompts
-	return allPrompts, nil
-}
-
-func getPromptTemplate(name string) (string, error) {
-	prompts, err := getPrompts()
-	if err != nil {
-		return "", err
-	}
-	for _, prompt := range prompts {
-		if prompt.Name == name {
-			return prompt.Template, nil
-		}
-	}
-	return "", fmt.Errorf("could not find prompt with name %s", name)
-}
-
 // this prompt is applied as the system prompt for a session that has been fine-tuned on some documents
 func TextFinetuneSystemPrompt(documentIDs []string, documentGroupID string) (string, error) {
-	promptTemplate, err := getPromptTemplate("finetune-system-prompt")
-	if err != nil {
-		return "", err
-	}
 	tmplData := struct {
 		DocumentIDs   string
 		DocumentGroup string
@@ -79,9 +41,10 @@ func TextFinetuneSystemPrompt(documentIDs []string, documentGroupID string) (str
 		DocumentGroup: documentGroupID,
 		DocumentCount: len(documentIDs),
 	}
-	tmpl := template.Must(template.New("TextFinetuneSystemPrompt").Parse(promptTemplate))
+	tmpl := template.Must(template.New("TextFinetuneSystemPrompt").Parse(templates.FinetuningTemplate))
+
 	var buf bytes.Buffer
-	err = tmpl.Execute(&buf, tmplData)
+	err := tmpl.Execute(&buf, tmplData)
 	if err != nil {
 		return "", err
 	}
