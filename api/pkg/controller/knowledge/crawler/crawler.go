2098d5fc2c6ec706f770391ca06c6e3f3959e934
Karolis Rusenas <karolis@rusenas.dev>
2024-08-31 10:52:08 +0100

    crawler mocks

---
+++ api/pkg/controller/knowledge/crawler/crawler.go
@@ -0,0 +1,31 @@
+package crawler
+
+import (
+	"context"
+
+	"github.com/helixml/helix/api/pkg/types"
+	"github.com/rs/zerolog/log"
+)
+
+//go:generate mockgen -source $GOFILE -destination crawler_mocks.go -package $GOPACKAGE
+
+type Crawler interface {
+	Crawl(ctx context.Context) ([]*types.CrawledDocument, error)
+}
+
+func NewCrawler(k *types.Knowledge) (Crawler, error) {
+	switch {
+	case k.Source.Web.Crawler.Firecrawl != nil:
+		log.Info().
+			Str("knowledge_id", k.ID).
+			Str("knowledge_name", k.Name).
+			Msgf("Using firecrawl crawler")
+		return NewFirecrawl(k)
+	default:
+		log.Info().
+			Str("knowledge_id", k.ID).
+			Str("knowledge_name", k.Name).
+			Msgf("Using default Helix crawler")
+		return NewDefault(k)
+	}
+}
