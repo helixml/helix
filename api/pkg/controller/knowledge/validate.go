2ea7dfdee7df45079a9ecc92b684616845fafb4d
Karolis Rusenas <karolis@rusenas.dev>
2024-09-01 21:19:45 +0100

    validate parsing

--- api/pkg/controller/knowledge/validate.go
+++ api/pkg/controller/knowledge/validate.go
@@ -2,11 +2,26 @@ package knowledge
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/helixml/helix/api/pkg/types"
+
+	"github.com/robfig/cron/v3"
 )
 
 func Validate(k *types.AssistantKnowledge) error {
+	if k.RefreshSchedule != "" {
+		cronSchedule, err := cron.ParseStandard(k.RefreshSchedule)
+		if err != nil {
+			return fmt.Errorf("invalid refresh schedule: %w", err)
+		}
+
+		// Validate that the cron schedule is setup to run not more than once per 10 minutes
+		if cronSchedule.Next(time.Now()).Before(time.Now().Add(10 * time.Minute)) {
+			return fmt.Errorf("refresh schedule must run more than once per 10 minutes")
+		}
+	}
+
 	if k.Source.Web != nil {
 		if len(k.Source.Web.URLs) == 0 {
 			return fmt.Errorf("at least one url is required")
