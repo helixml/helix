414e0c9178b259c93512e277c131cdf40978b8ad
Karolis Rusenas <karolis@rusenas.dev>
2024-09-02 23:50:53 +0100

    ls

---
+++ api/pkg/cli/fs/cmd.go
@@ -0,0 +1,17 @@
+package fs
+
+import "github.com/spf13/cobra"
+
+var rootCmd = &cobra.Command{
+	Use:     "filesystem",
+	Short:   "Helix filesystem management",
+	Aliases: []string{"fs"},
+	Long:    `TODO`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// Do Stuff Here
+	},
+}
+
+func New() *cobra.Command {
+	return rootCmd
+}
