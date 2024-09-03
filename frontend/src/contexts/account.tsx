0d8075f11c124c411549387ff40d8e298ecdb9ed
Luke Marsden <me@lukemarsden.net>
2024-09-03 22:53:07 +0100

    don't ever use user's api key as a token, for some reason it doesn't work - and anyway when would a user not have a jwt?

--- frontend/src/contexts/account.tsx
+++ frontend/src/contexts/account.tsx
@@ -103,16 +103,13 @@ export const useAccountContext = (): IAccountContext => {
   }, [])
 
   const token = useMemo(() => {
-    if(apiKeys && apiKeys.length > 0) {
-      return apiKeys[0].key
-    } else if(user && user.token) {
+    if(user && user.token) {
       return user.token
     } else {
       return ''
     }
   }, [
     user,
-    apiKeys,
   ])
 
   const tokenUrlEscaped = useMemo(() => {
@@ -261,6 +258,7 @@ export const useAccountContext = (): IAccountContext => {
     initialized,
     user,
     token,
+    tokenUrlEscaped,
     admin,
     loggingOut,
     serverConfig,
