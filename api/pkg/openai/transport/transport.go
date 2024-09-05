0ca501b15cf1df7e500c0c28757660dee26abd67
Karolis Rusenas <karolis@rusenas.dev>
2024-09-05 10:06:06 +0100

    plumbing

--- api/pkg/openai/transport/transport.go
+++ api/pkg/openai/transport/transport.go
@@ -2,6 +2,7 @@ package transport
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -47,4 +48,27 @@ func (t *helixTransport) RoundTrip(r *http.Request) (*http.Response, error) {
 		StatusCode: 200,
 		Body:       readCloser,
 	}, nil
+}
+
+func WriteChatCompletionStream(w io.Writer, chunk *openai.ChatCompletionStreamResponse) error {
+	bts, err := json.Marshal(chunk)
+	if err != nil {
+		return fmt.Errorf("error marshalling chunk: %w", err)
+	}
+
+	return WriteChunk(w, bts)
+}
+
+func WriteChunk(w io.Writer, chunk []byte) error {
+	_, err := fmt.Fprintf(w, "data: %s\n\n", string(chunk))
+	if err != nil {
+		return fmt.Errorf("error writing chunk '%s': %w", string(chunk), err)
+	}
+
+	// Flush the ResponseWriter buffer to send the chunk immediately
+	if flusher, ok := w.(http.Flusher); ok {
+		flusher.Flush()
+	}
+
+	return nil
 }
