59c0d72a7aaebeed75fec1cdb080f68dac5c2d98
Luke Marsden <me@lukemarsden.net>
2024-09-11 21:15:43 -0700

    fix: use official caddy instructions

--- install.sh
+++ install.sh
@@ -525,11 +525,11 @@ EOF
                 echo "Caddy installation is only supported on Ubuntu. Please install and configure Caddy manually (check the install.sh script for details)."
             else
                 echo "Installing Caddy..."
-                sudo apt-get install -y debian-keyring debian-archive-keyring apt-transport-https
-                curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo apt-key add -
+                sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https curl
+                curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
                 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
-                sudo apt-get update
-                sudo apt-get install caddy
+                sudo apt update
+                sudo apt install caddy
 
                 # Create Caddyfile
                 CADDYFILE="/etc/caddy/Caddyfile"
