05c5a5af2c3303a1d37ef90802007a35bbf2674f
Karolis Rusenas <karolis@rusenas.dev>
2024-09-20 09:31:17 +0100

    app tsx

---
+++ frontend/src/hooks/useInterval.ts
@@ -0,0 +1,21 @@
+import { useEffect, useRef } from 'react';
+
+function useInterval(callback: () => void, delay: number | null) {
+  const savedCallback = useRef<() => void>();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    function tick() {
+      savedCallback.current?.();
+    }
+    if (delay !== null) {
+      const id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
+
+export default useInterval;
