f6fcad1ab9c12c945ab5f42ea59a3039fe5dac6a
Karolis Rusenas <karolis@rusenas.dev>
2024-09-23 23:08:13 +0100

    splitter test

---
+++ api/pkg/controller/knowledge/splitter_test.go
@@ -0,0 +1,28 @@
+package knowledge
+
+import (
+	"os"
+	"testing"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/helixml/helix/api/pkg/types"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSplitData_Markdown(t *testing.T) {
+	contents, err := os.ReadFile("./testdata/example_code.md")
+	require.NoError(t, err)
+
+	k := &types.Knowledge{}
+
+	chunks, err := splitData(k, []*indexerData{{
+		Source: "example_code.md",
+		Data:   contents,
+	}})
+	require.NoError(t, err)
+
+	assert.Equal(t, 1, len(chunks))
+
+	spew.Dump(chunks)
+}
