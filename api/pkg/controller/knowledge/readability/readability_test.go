85b0f2c2752647c84d1cd7314307e4fdd512b2db
Karolis Rusenas <karolis@rusenas.dev>
2024-09-23 23:28:21 +0100

    test cases

--- api/pkg/controller/knowledge/readability/readability_test.go
+++ api/pkg/controller/knowledge/readability/readability_test.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -16,7 +17,7 @@ func TestParseArticle(t *testing.T) {
 	article, err := parser.Parse(context.Background(), string(content), "https://www.theguardian.com/uk-news/2024/sep/13/plans-unveiled-for-cheaper-high-speed-alternative-to-scrapped-hs2-northern-leg")
 	require.NoError(t, err)
 
-	os.WriteFile("parsed.html", []byte(article.Content), 0o644)
+	assert.Contains(t, article.Content, "The 50-mile track would run from where the HS2 line is now due")
 }
 
 func TestParseArticleWithCodeBlock(t *testing.T) {
@@ -27,5 +28,6 @@ func TestParseArticleWithCodeBlock(t *testing.T) {
 	article, err := parser.Parse(context.Background(), string(content), "https://webhookrelay.com/v1/examples/transform/multipart-form-data/")
 	require.NoError(t, err)
 
-	os.WriteFile("parsed_code_block.html", []byte(article.Content), 0o644)
+	assert.Contains(t, article.Content, "transforming form data into JSON")
+	assert.Contains(t, article.Content, "SetRequestBody</span><span>(</span>encoded_payload<span>)</span></code></pre>")
 }
