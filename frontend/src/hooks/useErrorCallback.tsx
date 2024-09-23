2994521c25dacc1cc9f7d6328bf80200ce17f678
Phil Winder <phil@winder.ai>
2024-09-23 13:12:32 +0100

    chore(ui): print generic error message when error is undefined

--- frontend/src/hooks/useErrorCallback.tsx
+++ frontend/src/hooks/useErrorCallback.tsx
@@ -1,21 +1,26 @@
-import { useContext, useCallback } from 'react'
+import { useCallback, useContext } from 'react'
 
 import {
   SnackbarContext,
 } from '../contexts/snackbar'
 
 export const extractErrorMessage = (error: any): string => {
-  if(error.response && error.response.data) {
+  if (!error) {
+    // Print a stack trace if the error is undefined
+    console.error(new Error('Received an undefined error'))
+    return 'Unknown error'
+  }
+  if (error.response && error.response.data) {
     if (error.response.data.message || error.response.data.error) {
       return (error.response.data.message || error.response.data.error) as string
     }
     if (error.response.data) return error.response.data as string
     return error.toString()
   }
-  else if(error.error) {
+  else if (error.error) {
     return error.error
   }
-  else if(error.message) {
+  else if (error.message) {
     return error.message
   }
   else {
@@ -31,10 +36,10 @@ export function useErrorCallback<T = void>(handler: {
     try {
       const result = await handler()
       return result
-    } catch(e) {
+    } catch (e) {
       const errorMessage = extractErrorMessage(e)
       console.error(errorMessage)
-      if(snackbarActive !== false) snackbar.setSnackbar(errorMessage, 'error')
+      if (snackbarActive !== false) snackbar.setSnackbar(errorMessage, 'error')
     }
     return
   }, [
