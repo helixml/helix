cabfeef93798c3459041db42c52bae6fb7819021
Karolis Rusenas <karolis@rusenas.dev>
2024-10-13 10:41:07 +0100

    tests

--- api/pkg/controller/knowledge/browser/browser_test.go
+++ api/pkg/controller/knowledge/browser/browser_test.go
@@ -1,6 +1,7 @@
 package browser
 
 import (
+	"os"
 	"testing"
 
 	"github.com/go-rod/rod/lib/proto"
@@ -14,14 +15,17 @@ func TestBrowser_Get(t *testing.T) {
 	cfg := &config.ServerConfig{}
 	cfg.RAG.Crawler.ChromeURL = "http://127.0.0.1:9222"
 
-	browserManager := New(cfg)
+	if os.Getenv("CHROME_URL") != "" {
+		cfg.RAG.Crawler.ChromeURL = os.Getenv("CHROME_URL")
+	}
 
-	browser, err := browserManager.Get()
+	browserManager, err := New(cfg)
+	require.NoError(t, err)
+
+	browser, err := browserManager.GetBrowser()
 	require.NoError(t, err)
 
 	assert.NotNil(t, browser)
-
-	defer browserManager.Put(browser)
 
 	page, err := browser.Page(proto.TargetCreateTarget{URL: "https://docs.helix.ml/"})
 	require.NoError(t, err)
@@ -33,4 +37,44 @@ func TestBrowser_Get(t *testing.T) {
 	require.NoError(t, err)
 
 	assert.Contains(t, body, "Helix")
+}
+
+func TestBrowser_BrowsePages(t *testing.T) {
+	cfg := &config.ServerConfig{}
+	cfg.RAG.Crawler.ChromeURL = "http://127.0.0.1:9222"
+
+	if os.Getenv("CHROME_URL") != "" {
+		cfg.RAG.Crawler.ChromeURL = os.Getenv("CHROME_URL")
+	}
+
+	browserManager, err := New(cfg)
+	require.NoError(t, err)
+
+	page1, err := browserManager.GetPage(proto.TargetCreateTarget{URL: "https://docs.helix.ml/"})
+	require.NoError(t, err)
+	assert.NotNil(t, page1)
+
+	err = page1.WaitLoad()
+	require.NoError(t, err)
+
+	body, err := page1.HTML()
+	require.NoError(t, err)
+
+	assert.Contains(t, body, "Helix")
+
+	browserManager.PutPage(page1)
+
+	page2, err := browserManager.GetPage(proto.TargetCreateTarget{URL: "https://docs.helix.ml/helix/help/"})
+	require.NoError(t, err)
+
+	err = page2.WaitLoad()
+	require.NoError(t, err)
+
+	body, err = page2.HTML()
+	require.NoError(t, err)
+
+	assert.Contains(t, body, "Commercial Support")
+
+	browserManager.PutPage(page2)
+
 }
