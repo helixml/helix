66af636a35a2501cc8e91cdfea8eb25ccbf5b079
Karolis Rusenas <karolis@rusenas.dev>
2024-10-19 19:25:47 +0100

    type it in

--- api/pkg/cli/secret/update.go
+++ api/pkg/cli/secret/update.go
@@ -2,9 +2,11 @@ package secret
 
 import (
 	"fmt"
+	"os"
 	"strings"
 
 	"github.com/spf13/cobra"
+	"golang.org/x/term"
 
 	"github.com/helixml/helix/api/pkg/client"
 	"github.com/helixml/helix/api/pkg/types"
@@ -15,7 +17,6 @@ func init() {
 	updateCmd.Flags().StringP("name", "n", "", "Name of the secret")
 	updateCmd.Flags().StringP("value", "v", "", "New value of the secret")
 	_ = updateCmd.MarkFlagRequired("name")
-	_ = updateCmd.MarkFlagRequired("value")
 }
 
 var updateCmd = &cobra.Command{
@@ -29,6 +30,16 @@ var updateCmd = &cobra.Command{
 		name = strings.TrimSpace(name)
 		if name == "" {
 			return fmt.Errorf("secret name cannot be empty")
+		}
+
+		if value == "" {
+			fmt.Print("Enter new secret value (input will be hidden): ")
+			byteValue, err := term.ReadPassword(int(os.Stdin.Fd()))
+			if err != nil {
+				return fmt.Errorf("failed to read secret value: %w", err)
+			}
+			value = string(byteValue)
+			fmt.Println() // Print a newline after the hidden input
 		}
 
 		value = strings.TrimSpace(value)
