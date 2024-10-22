b9a494c925842f2a2438ceb6b5921efdc9502e67
Kai Davenport <kaiyadavenport@gmail.com>
2024-10-22 15:36:58 +0100

    operator working as stand alone go package inside operator folder

--- operator/internal/controller/aiapp_controller.go
+++ operator/internal/controller/aiapp_controller.go
@@ -24,7 +24,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
-	aispecv1alpha1 "github.com/helixml/helix/api/v1alpha1"
+	aispecv1alpha1 "github.com/helixml/helix/operator/api/v1alpha1"
 )
 
 // AIAppReconciler reconciles a AIApp object
--- operator/test/e2e/e2e_test.go
+++ operator/test/e2e/e2e_test.go
@@ -24,7 +24,7 @@ import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
-	"github.com/helixml/helix/test/utils"
+	"github.com/helixml/helix/operator/test/utils"
 )
 
 const namespace = "operator-system"
