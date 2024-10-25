ec2cbe0e249f95402ad4908b6b0d5e705453119e
Karolis Rusenas <karolis@rusenas.dev>
2024-10-25 23:22:58 +0100

    test

--- api/pkg/controller/knowledge/query/variations_test.go
+++ api/pkg/controller/knowledge/query/variations_test.go
@@ -6,6 +6,9 @@ func (suite *QuerySuite) TestCreateVariations() {
 	variations, err := suite.query.createVariations(suite.ctx, "How to make HTTP call with a function?", 8)
 	suite.NoError(err)
 
+	suite.Equal(8, len(variations))
+	// TODO: use an LLM to check if the variations are valid
+
 	fmt.Println(len(variations))
 	for _, variation := range variations {
 		fmt.Println(variation)
