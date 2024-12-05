c218ffddd0a402c851456097055d50d9df193d70
Phil Winder <phil@winder.ai>
2024-12-05 09:02:28 +0000

    refactor(image): make flux 40GB to fit in A100's

--- api/pkg/model/models.go
+++ api/pkg/model/models.go
@@ -194,7 +194,7 @@ func GetDefaultDiffusersModels() ([]*DiffusersGenericImage, error) {
 		{
 			Id:          Model_Diffusers_FluxDev,
 			Name:        "Flux 1 Dev",
-			Memory:      GB * 48,
+			Memory:      GB * 40,
 			Description: "Dev model, from Black Forest Labs",
 			Hide:        false,
 		},
