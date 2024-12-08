1f1bd23bd5bf8a91819ec41e231916e9d4187543
Luke Marsden <me@lukemarsden.net>
2024-12-08 07:55:00 +0000

    fix build on windows

--- api/pkg/util/copydir/copy_dir.go
+++ api/pkg/util/copydir/copy_dir.go
@@ -5,22 +5,10 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
-	"syscall"
 	"time"
 
 	"github.com/rs/zerolog/log"
 )
-
-func sameFilesystem(path1, path2 string) bool {
-	var stat1, stat2 syscall.Stat_t
-	if err := syscall.Stat(path1, &stat1); err != nil {
-		return false
-	}
-	if err := syscall.Stat(path2, &stat2); err != nil {
-		return false
-	}
-	return stat1.Dev == stat2.Dev
-}
 
 func CopyDir(dst, src string) error {
 	startTime := time.Now()
