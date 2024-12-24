bb1e7e535fc09401daf1a16a150cf9c7d94f2e06
nessie <nessie.molano@gmail.com>
2024-12-24 08:31:33 +0400

    relative?

--- integration-test/smoke/test_cases.go
+++ integration-test/smoke/test_cases.go
@@ -246,7 +246,7 @@ func createRagApp(browser *rod.Browser) error {
 
 	logStep("clicking on the upload file button")
 	upload := page.MustElement("input[type='file']")
-	upload.MustSetFiles("/Users/phil/code/helixml/helix/integration-test/data/smoke/hr-guide.pdf")
+	upload.MustSetFiles("../data/smoke/hr-guide.pdf")
 	page.MustReload()
 
 	logStep("Waiting for knowledge source to be ready")
