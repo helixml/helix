d3cc318b79b99d01e0011fbd6b36f95f4705e516
nessie <nessie.molano@gmail.com>
2024-12-26 17:39:59 +0400

    cli

---
+++ api/pkg/cli/mcp/mcp_configure.go
@@ -0,0 +1,3 @@
+package mcp
+
+// TODO: add a command that configures the mcp server in the claude config file
--- api/pkg/cli/mcp/mcp_proxy.go
+++ api/pkg/cli/mcp/mcp_proxy.go
@@ -1,14 +1,13 @@
 package mcp
 
 import (
-	"crypto/tls"
-	"log"
-	"net/http"
-	"net/http/httputil"
-	"net/url"
-	"os"
+	"fmt"
 
+	mcp_golang "github.com/metoro-io/mcp-golang"
+	"github.com/metoro-io/mcp-golang/transport/stdio"
 	"github.com/spf13/cobra"
+
+	"github.com/helixml/helix/api/pkg/client"
 )
 
 func init() {
@@ -19,25 +18,66 @@ var runProxyCmd = &cobra.Command{
 	Use:   "run",
 	Short: "Run Helix mpc (model context protocol) proxy",
 	Long:  `TODO`,
-	Run: func(*cobra.Command, []string) {
-		helixAPIKey := os.Getenv("HELIX_API_KEY")
-		helixURL := os.Getenv("HELIX_URL")
-
-		if helixAPIKey == "" || helixURL == "" {
-			log.Fatal("HELIX_API_KEY and HELIX_URL must be set")
-		}
-
-		u, err := url.Parse(helixURL)
+	RunE: func(cmd *cobra.Command, args []string) error {
+		apiClient, err := client.NewClientFromEnv()
 		if err != nil {
-			log.Fatal("HELIX_URL must be a valid URL")
+			return err
 		}
 
-		proxy := httputil.NewSingleHostReverseProxy(u)
-
-		proxy.Transport = &http.Transport{
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		srv := &ModelContextProtocolServer{
+			apiClient: apiClient,
 		}
 
-		//
+		return srv.Start()
 	},
+}
+
+type ModelContextProtocolServer struct {
+	apiClient client.Client
+}
+
+func (s *ModelContextProtocolServer) Start() error {
+	done := make(chan struct{})
+
+	server := mcp_golang.NewServer(stdio.NewStdioServerTransport())
+	err := server.RegisterTool("hello", "Say hello to a person", func(arguments MyFunctionsArguments) (*mcp_golang.ToolResponse, error) {
+		return mcp_golang.NewToolResponse(mcp_golang.NewTextContent(fmt.Sprintf("Hello, %server!", arguments.Submitter))), nil
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	err = server.RegisterPrompt("promt_test", "This is a test prompt", func(arguments Content) (*mcp_golang.PromptResponse, error) {
+		return mcp_golang.NewPromptResponse("description", mcp_golang.NewPromptMessage(mcp_golang.NewTextContent(fmt.Sprintf("Hello, %server!", arguments.Title)), mcp_golang.RoleUser)), nil
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	err = server.RegisterResource("test://resource", "resource_test", "This is a test resource", "application/json", func() (*mcp_golang.ResourceResponse, error) {
+		return mcp_golang.NewResourceResponse(mcp_golang.NewTextEmbeddedResource("test://resource", "This is a test resource", "application/json")), nil
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	err = server.Serve()
+	if err != nil {
+		panic(err)
+	}
+
+	<-done
+
+	return nil
+}
+
+// Tool arguments are just structs, annotated with jsonschema tags
+// More at https://mcpgolang.com/tools#schema-generation
+type Content struct {
+	Title       string  `json:"title" jsonschema:"required,description=The title to submit"`
+	Description *string `json:"description" jsonschema:"description=The description to submit"`
+}
+type MyFunctionsArguments struct {
+	Submitter string  `json:"submitter" jsonschema:"required,description=The name of the thing calling this tool (openai, google, claude, etc)"`
+	Content   Content `json:"content" jsonschema:"required,description=The content of the message"`
 }
