b48b6eed8cc656f2796959c9b84878a32e4c850c
nessie <nessie.molano@gmail.com>
2025-01-27 22:53:57 +0400

    move

--- api/pkg/store/postgres.go
+++ api/pkg/store/postgres.go
@@ -3,14 +3,10 @@ package store
 import (
 	"context"
 	"embed"
-	"encoding/json"
-	"errors"
 	"fmt"
 	reflect "reflect"
 	"strings"
 	"time"
-
-	"database/sql"
 
 	_ "github.com/doug-martin/goqu/v9/dialect/postgres"        // postgres query builder
 	_ "github.com/golang-migrate/migrate/v4/database/postgres" // postgres migrations
@@ -18,8 +14,8 @@ import (
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"gorm.io/gorm/schema"
 
-	"github.com/doug-martin/goqu/v9"
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/source/iofs"
 	"github.com/helixml/helix/api/pkg/config"
@@ -30,8 +26,6 @@ import (
 type PostgresStore struct {
 	cfg              config.Store
 	connectionString string
-	pgDb             *sql.DB
-	db               *goqu.Database
 
 	gdb *gorm.DB
 }
@@ -46,34 +40,9 @@ func NewPostgresStore(
 		return nil, err
 	}
 
-	// Read SSL setting from environment
-	sslSettings := "sslmode=disable"
-	if cfg.SSL {
-		sslSettings = "sslmode=require"
-	}
-
-	connectionString := fmt.Sprintf(
-		"postgres://%s:%s@%s:%d/%s?%s",
-		cfg.Username,
-		cfg.Password,
-		cfg.Host,
-		cfg.Port,
-		cfg.Database,
-		sslSettings,
-	)
-	pgDb, err := sql.Open("postgres", connectionString)
-	if err != nil {
-		return nil, err
-	}
-	dialect := goqu.Dialect("postgres")
-	db := dialect.DB(pgDb)
-
 	store := &PostgresStore{
-		connectionString: connectionString,
-		cfg:              cfg,
-		pgDb:             pgDb,
-		db:               db,
-		gdb:              gormDB,
+		cfg: cfg,
+		gdb: gormDB,
 	}
 
 	if cfg.AutoMigrate {
@@ -98,12 +67,13 @@ type MigrationScript struct {
 
 func (s *PostgresStore) autoMigrate() error {
 	err := s.gdb.WithContext(context.Background()).AutoMigrate(
+		&types.UserMeta{},
+		&types.Session{},
 		&types.App{},
 		&types.APIKey{},
 		&types.Tool{},
 		&types.Knowledge{},
 		&types.KnowledgeVersion{},
-		&types.SessionToolBinding{},
 		&types.DataEntity{},
 		&types.ScriptRun{},
 		&types.LLMCall{},
@@ -112,10 +82,6 @@ func (s *PostgresStore) autoMigrate() error {
 	)
 	if err != nil {
 		return err
-	}
-
-	if err := createFK(s.gdb, types.SessionToolBinding{}, types.Tool{}, "tool_id", "id", "CASCADE", "CASCADE"); err != nil {
-		log.Err(err).Msg("failed to add DB FK")
 	}
 
 	if err := createFK(s.gdb, types.APIKey{}, types.App{}, "app_id", "id", "CASCADE", "CASCADE"); err != nil {
@@ -212,169 +178,6 @@ type Scanner interface {
 	Scan(dest ...interface{}) error
 }
 
-// given an array of field names - return the indexes as a string
-// e.g. $1, $2, $3, $4
-func getValueIndexes(fields []string) string {
-	parts := []string{}
-	for i := range fields {
-		parts = append(parts, fmt.Sprintf("$%d", i+1))
-	}
-	return strings.Join(parts, ", ")
-}
-
-// given an array of field names - return the indexes as an update
-// start at the given offset
-// e.g. id = $2, name = $3
-func getKeyValueIndexes(fields []string, offset int) string {
-	parts := []string{}
-	for i, field := range fields {
-		parts = append(parts, fmt.Sprintf("%s = $%d", field, i+offset+1))
-	}
-	return strings.Join(parts, ", ")
-}
-
-var (
-	UsermetaFields = []string{
-		"id",
-		"config",
-	}
-
-	UsermetaFieldsString = strings.Join(UsermetaFields, ", ")
-)
-
-func scanUserMetaRow(row Scanner) (*types.UserMeta, error) {
-	user := &types.UserMeta{}
-	var config []byte
-	err := row.Scan(
-		&user.ID,
-		&config,
-	)
-	if err != nil {
-		if errors.Is(err, sql.ErrNoRows) {
-			return nil, ErrNotFound
-		}
-
-		return nil, err
-	}
-	err = json.Unmarshal(config, &user.Config)
-	if err != nil {
-		return nil, err
-	}
-	return user, nil
-}
-
-func getUserMetaValues(user *types.UserMeta) ([]interface{}, error) {
-	config, err := json.Marshal(user.Config)
-	if err != nil {
-		return nil, err
-	}
-	return []interface{}{
-		user.ID,
-		config,
-	}, nil
-}
-
-func (s *PostgresStore) GetUserMeta(
-	ctx context.Context,
-	userID string,
-) (*types.UserMeta, error) {
-	if userID == "" {
-		return nil, fmt.Errorf("userID cannot be empty")
-	}
-	row := s.pgDb.QueryRowContext(ctx, fmt.Sprintf(`
-		SELECT %s
-		FROM usermeta WHERE id = $1
-	`, UsermetaFieldsString), userID)
-
-	return scanUserMetaRow(row)
-}
-
-func (s *PostgresStore) CreateUserMeta(
-	ctx context.Context,
-	user types.UserMeta,
-) (*types.UserMeta, error) {
-	values, err := getUserMetaValues(&user)
-	if err != nil {
-		return nil, err
-	}
-	_, err = s.pgDb.ExecContext(ctx, fmt.Sprintf(`
-		INSERT INTO usermeta (
-			%s
-		) VALUES (
-			%s
-		)
-	`, UsermetaFieldsString, getValueIndexes(UsermetaFields)), values...)
-
-	if err != nil {
-		return nil, err
-	}
-	return &user, nil
-}
-
-func (s *PostgresStore) UpdateUserMeta(
-	ctx context.Context,
-	user types.UserMeta,
-) (*types.UserMeta, error) {
-	values, err := getUserMetaValues(&user)
-	if err != nil {
-		return nil, err
-	}
-	// prepend the ID to the values
-	values = append([]interface{}{user.ID}, values...)
-
-	_, err = s.pgDb.ExecContext(ctx, fmt.Sprintf(`
-		UPDATE usermeta SET
-			%s
-		WHERE id = $1
-	`, getKeyValueIndexes(UsermetaFields, 1)), values...)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return &user, nil
-}
-
-func (s *PostgresStore) EnsureUserMeta(
-	ctx context.Context,
-	user types.UserMeta,
-) (*types.UserMeta, error) {
-	existing, err := s.GetUserMeta(ctx, user.ID)
-	if err != nil || existing == nil {
-		return s.CreateUserMeta(ctx, user)
-	}
-	return s.UpdateUserMeta(ctx, user)
-}
-
-func (s *PostgresStore) UpdateSessionMeta(
-	ctx context.Context,
-	data types.SessionMetaUpdate,
-) (*types.Session, error) {
-	if data.Owner != "" {
-		_, err := s.pgDb.Exec(`
-		UPDATE session SET
-			name = $2,
-			owner = $3,
-			owner_type = $4
-		WHERE id = $1
-	`, data.ID, data.Name, data.Owner, data.OwnerType)
-		if err != nil {
-			return nil, err
-		}
-	} else {
-		_, err := s.pgDb.Exec(`
-		UPDATE session SET
-			name = $2
-		WHERE id = $1
-	`, data.ID, data.Name)
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	return s.GetSession(ctx, data.ID)
-}
-
 // Compile-time interface check:
 var _ Store = (*PostgresStore)(nil)
 
@@ -450,7 +253,15 @@ func connect(ctx context.Context, cfg config.Store) (*gorm.DB, error) {
 
 			log.Info().Msg("sql store connecting to DB")
 
-			db, err := gorm.Open(dialector, &gorm.Config{})
+			gormConfig := &gorm.Config{}
+
+			if cfg.Schema != "" {
+				gormConfig.NamingStrategy = schema.NamingStrategy{
+					TablePrefix: cfg.Schema + ".",
+				}
+			}
+
+			db, err := gorm.Open(dialector, gormConfig)
 			if err != nil {
 				time.Sleep(1 * time.Second)
 
