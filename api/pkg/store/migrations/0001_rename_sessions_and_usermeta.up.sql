7e30214a866b72da206758b3893e977827055989
nessie <nessie.molano@gmail.com>
2025-01-29 09:33:51 +0400

    check

--- api/pkg/store/migrations/0001_rename_sessions_and_usermeta.up.sql
+++ api/pkg/store/migrations/0001_rename_sessions_and_usermeta.up.sql
@@ -1,11 +1,26 @@
 -- Drop table 'session_tool_bindings' if exists (not used for a long time)
 DROP TABLE IF EXISTS session_tool_bindings;
 
--- rename the session table to sessions
-ALTER TABLE session RENAME TO sessions;
+-- rename the session table to sessions if it exists
+DO $$ 
+BEGIN
+    IF EXISTS (SELECT FROM pg_tables WHERE tablename = 'session') THEN
+        ALTER TABLE session RENAME TO sessions;
+    END IF;
+END $$;
 
 -- rename the usermeta table to user_meta
-ALTER TABLE usermeta RENAME TO user_meta;
+DO $$ 
+BEGIN
+    IF EXISTS (SELECT FROM pg_tables WHERE tablename = 'usermeta') THEN
+        ALTER TABLE usermeta RENAME TO user_meta;
+    END IF;
+END $$;
 
--- Rename table 'api_key' to 'api_keys'
-ALTER TABLE api_key RENAME TO api_keys;
+-- rename the api_key table to api_keys
+DO $$ 
+BEGIN
+    IF EXISTS (SELECT FROM pg_tables WHERE tablename = 'api_key') THEN
+        ALTER TABLE api_key RENAME TO api_keys;
+    END IF;
+END $$;
