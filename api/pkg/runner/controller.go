2798a5ed1a4e08b4022aefdc9bdf786cc7907c2f
Phil Winder <phil@winder.ai>
2025-02-06 17:32:17 +0000

    strip http

--- api/pkg/runner/controller.go
+++ api/pkg/runner/controller.go
@@ -3,6 +3,7 @@ package runner
 import (
 	"context"
 	"fmt"
+	"net/url"
 	"strings"
 
 	"github.com/google/uuid"
@@ -131,8 +132,14 @@ func NewRunner(
 		return nil, err
 	}
 
+	// Parse the APIHost to strip any protocol
+	parsedURL, err := url.Parse(options.APIHost)
+	if err != nil {
+		return nil, err
+	}
+
 	// Build the URL to the nats server
-	natsServerURL := fmt.Sprintf("ws://%s/api/v1/runner/%s/ws", options.APIHost, options.ID)
+	natsServerURL := fmt.Sprintf("ws://%s/api/v1/runner/%s/ws", parsedURL.Host, options.ID)
 	log.Info().Msgf("Connecting to nats server: %s", natsServerURL)
 	ps, err := pubsub.NewNatsClient(natsServerURL, options.APIToken)
 	if err != nil {
