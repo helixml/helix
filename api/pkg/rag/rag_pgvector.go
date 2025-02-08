190c0357f82b85c90c203168a742bd0f73314b4c
nessie <nessie.molano@gmail.com>
2025-02-08 23:26:21 +0400

    indexing

--- api/pkg/rag/rag_pgvector.go
+++ api/pkg/rag/rag_pgvector.go
@@ -7,6 +7,9 @@ import (
 	"github.com/helixml/helix/api/pkg/openai/manager"
 	"github.com/helixml/helix/api/pkg/store"
 	"github.com/helixml/helix/api/pkg/types"
+	"github.com/pgvector/pgvector-go"
+	"github.com/rs/zerolog/log"
+	"github.com/sashabaranov/go-openai"
 )
 
 type PGVector struct {
@@ -26,7 +29,54 @@ func NewPGVector(cfg *config.ServerConfig, providerManager manager.ProviderManag
 }
 
 func (p *PGVector) Index(ctx context.Context, indexReqs ...*types.SessionRAGIndexChunk) error {
-	return nil
+	embeddings, err := p.getEmbeddings(ctx, indexReqs)
+	if err != nil {
+		return err
+	}
+
+	return p.store.CreateKnowledgeEmbedding(ctx, embeddings...)
+}
+
+func (p *PGVector) getEmbeddings(ctx context.Context, indexReqs []*types.SessionRAGIndexChunk) ([]*types.KnowledgeEmbeddingItem, error) {
+	var embeddings []*types.KnowledgeEmbeddingItem
+
+	client, err := p.providerManager.GetClient(ctx, &manager.GetClientRequest{
+		Provider: p.cfg.Embeddings.Provider,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	for _, indexReq := range indexReqs {
+		generated, err := client.CreateEmbeddings(ctx, openai.EmbeddingRequest{
+			Model: openai.EmbeddingModel(p.cfg.RAG.PGVector.EmbeddingsModel),
+			Input: indexReq.Content,
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		if len(generated.Data) == 0 {
+			log.Error().
+				Str("knowledge_id", indexReq.DataEntityID).
+				Msg("no embeddings returned for indexReq")
+			continue
+		}
+
+		vector := pgvector.NewVector(generated.Data[0].Embedding)
+
+		embeddings = append(embeddings, &types.KnowledgeEmbeddingItem{
+			KnowledgeID:     indexReq.DataEntityID,
+			DocumentID:      indexReq.DocumentID,
+			DocumentGroupID: indexReq.DocumentGroupID,
+			Content:         indexReq.Content,
+			Embedding:       vector,
+			Source:          indexReq.Source,
+			Filename:        indexReq.Filename,
+		})
+	}
+
+	return embeddings, nil
 }
 
 func (p *PGVector) Query(ctx context.Context, q *types.SessionRAGQuery) ([]*types.SessionRAGResult, error) {
