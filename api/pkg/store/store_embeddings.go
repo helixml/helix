028df92ecc933395209ec3619ce8945fd647baa3
nessie <nessie.molano@gmail.com>
2025-02-08 22:01:23 +0400

    query

--- api/pkg/store/store_embeddings.go
+++ api/pkg/store/store_embeddings.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/helixml/helix/api/pkg/types"
+	"gorm.io/gorm/clause"
 )
 
 func (s *PostgresStore) CreateKnowledgeEmbedding(ctx context.Context, embeddings ...*types.KnowledgeEmbeddingItem) error {
@@ -44,4 +45,28 @@ func (s *PostgresStore) DeleteKnowledgeEmbedding(ctx context.Context, knowledgeI
 		return err
 	}
 	return nil
+}
+
+func (s *PostgresStore) QueryKnowledgeEmbeddings(ctx context.Context, q *types.KnowledgeEmbeddingQuery) ([]*types.KnowledgeEmbeddingItem, error) {
+	if q.KnowledgeID == "" {
+		return nil, fmt.Errorf("knowledge ID is required")
+	}
+
+	if len(q.Embedding.Slice()) == 0 {
+		return nil, fmt.Errorf("embedding is required")
+	}
+
+	if q.Limit == 0 {
+		q.Limit = 5
+	}
+
+	var items []*types.KnowledgeEmbeddingItem
+	err := s.gdb.WithContext(ctx).Where("knowledge_id = ?", q.KnowledgeID).Clauses(clause.OrderBy{
+		Expression: clause.Expr{SQL: "embedding <-> ?", Vars: []interface{}{q.Embedding}},
+	}).Limit(q.Limit).Find(&items).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return items, nil
 }
