4558c30111867d0ec83cb800a3295bedf1b34e9f
Luke Marsden <me@lukemarsden.net>
2025-02-08 23:00:29 +0000

    improve layout

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -35,117 +35,127 @@ const Layout: FC = ({
     if (!account.serverConfig?.version || !account.serverConfig?.latest_version) {
       return false
     }
+    // Return false if version is "<unknown>"
+    if (account.serverConfig.version === "<unknown>") {
+      return false
+    }
+    // Return false if version doesn't have 2 dots (not semver)
+    if ((account.serverConfig.version.match(/\./g) || []).length !== 2) {
+      return false
+    }
     return account.serverConfig.version !== account.serverConfig.latest_version
   }, [account.serverConfig?.version, account.serverConfig?.latest_version])
 
   return (
-    <Box
-      id="root-container"
-      sx={{
-        height: '100%',
-        display: 'flex',
-      }}
-      component="div"
-    >
-      <CssBaseline />
-      {
-        router.meta.drawer && (
-          <Drawer
-            variant={ isBigScreen ? "permanent" : "temporary" }
-            open={ isBigScreen || account.mobileMenuOpen }
-            onClose={ () => account.setMobileMenuOpen(false) }
-            sx={{
-              height: '100%',
-              '& .MuiDrawer-paper': {
-                backgroundColor: lightTheme.backgroundColor,
-                position: 'relative',
-                whiteSpace: 'nowrap',
-                width: isBigScreen ? themeConfig.drawerWidth : themeConfig.smallDrawerWidth,
-                transition: theme.transitions.create('width', {
-                  easing: theme.transitions.easing.sharp,
-                  duration: theme.transitions.duration.enteringScreen,
-                }),
-                boxSizing: 'border-box',
-                overflowX: 'hidden',
-                height: '100%',
-                overflowY: 'auto',
-              },
-            }}
-          >
-            <Sidebar>
-              <SessionsMenu
-                onOpenSession={ () => {
-                  account.setMobileMenuOpen(false)
-                }}
-              />
-            </Sidebar>
-          </Drawer>
-        )
-      }
-      <Box
-        component="main"
-        sx={{
-          backgroundColor: (theme) => {
-            if(router.meta.background) return router.meta.background
-            return lightTheme.backgroundColor
-          },
-          flexGrow: 1,
-          height: '100%',
-          display: 'flex',
-          flexDirection: 'column',
-        }}
-      >
-        <Collapse in={showVersionBanner && hasNewVersion} sx={{backgroundColor: 'black', position: 'relative', paddingRight: '15px'}}>
-          <Alert
-            severity="info"
-            sx={{
-              borderRadius: 0,
-            }}
-            onClose={() => setShowVersionBanner(false)}
-          >
-            A new version of Helix ({account.serverConfig?.latest_version}) is available! You are currently running version {account.serverConfig?.version}. Learn more <a style={{color: 'white'}} href={`https://github.com/helixml/helix/releases/${account.serverConfig?.latest_version}`} target="_blank" rel="noopener noreferrer">here</a>.
-          </Alert>
-        </Collapse>
-        <Box
-          component="div"
+    <>
+      <Collapse in={showVersionBanner && hasNewVersion}>
+        <Alert
+          severity="info"
           sx={{
+            borderRadius: 0,
+          }}
+          onClose={() => setShowVersionBanner(false)}
+        >
+          A new version of Helix ({account.serverConfig?.latest_version}) is available! You are currently running version {account.serverConfig?.version}. Learn more <a style={{color: 'white'}} href={`https://github.com/helixml/helix/releases/${account.serverConfig?.latest_version}`} target="_blank" rel="noopener noreferrer">here</a>.
+        </Alert>
+      </Collapse>
+      <Box
+        id="root-container"
+        sx={{
+          height: showVersionBanner && hasNewVersion ? 'calc(100% - 48px)' : '100%',
+          display: 'flex',
+        }}
+        component="div"
+      >
+        <CssBaseline />
+        {
+          router.meta.drawer && (
+            <Drawer
+              variant={ isBigScreen ? "permanent" : "temporary" }
+              open={ isBigScreen || account.mobileMenuOpen }
+              onClose={ () => account.setMobileMenuOpen(false) }
+              sx={{
+                height: '100%',
+                '& .MuiDrawer-paper': {
+                  backgroundColor: lightTheme.backgroundColor,
+                  position: 'relative',
+                  whiteSpace: 'nowrap',
+                  width: isBigScreen ? themeConfig.drawerWidth : themeConfig.smallDrawerWidth,
+                  transition: theme.transitions.create('width', {
+                    easing: theme.transitions.easing.sharp,
+                    duration: theme.transitions.duration.enteringScreen,
+                  }),
+                  boxSizing: 'border-box',
+                  overflowX: 'hidden',
+                  height: '100%',
+                  overflowY: 'auto',
+                },
+              }}
+            >
+              <Sidebar>
+                <SessionsMenu
+                  onOpenSession={ () => {
+                    account.setMobileMenuOpen(false)
+                  }}
+                />
+              </Sidebar>
+            </Drawer>
+          )
+        }
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) => {
+              if(router.meta.background) return router.meta.background
+              return lightTheme.backgroundColor
+            },
             flexGrow: 1,
-            backgroundColor: theme.palette.mode === 'light' ? themeConfig.lightBackgroundColor : themeConfig.darkBackgroundColor,
             height: '100%',
-            minHeight: '100%',
+            display: 'flex',
+            flexDirection: 'column',
           }}
         >
-          { account.loggingOut ? null : children }
-        </Box>
-      </Box>
-      <Snackbar />
-      <GlobalLoading />
-      {
-        account.showLoginWindow && (
-          <Window
-            open
-            size="md"
-            title="Please login to continue"
-            onCancel={ () => {
-              account.setShowLoginWindow(false)
+          <Box
+            component="div"
+            sx={{
+              flexGrow: 1,
+              backgroundColor: theme.palette.mode === 'light' ? themeConfig.lightBackgroundColor : themeConfig.darkBackgroundColor,
+              height: '100%',
+              minHeight: '100%',
             }}
-            onSubmit={ () => {
-              account.onLogin()
-            }}
-            withCancel
-            cancelTitle="Cancel"
-            submitTitle="Login / Register"
           >
-            <Typography gutterBottom>
-              You can login with your Google account or with your email address.
-            </Typography>
-            <Typography>
-              We will keep what you've done here for you, so you may continue where you left off.
-            </Typography>
-          </Window>
-        )
-      }
-    </Box>
+            { account.loggingOut ? null : children }
+          </Box>
+        </Box>
+        <Snackbar />
+        <GlobalLoading />
+        {
+          account.showLoginWindow && (
+            <Window
+              open
+              size="md"
+              title="Please login to continue"
+              onCancel={ () => {
+                account.setShowLoginWindow(false)
+              }}
+              onSubmit={ () => {
+                account.onLogin()
+              }}
+              withCancel
+              cancelTitle="Cancel"
+              submitTitle="Login / Register"
+            >
+              <Typography gutterBottom>
+                You can login with your Google account or with your email address.
+              </Typography>
+              <Typography>
+                We will keep what you've done here for you, so you may continue where you left off.
+              </Typography>
+            </Window>
+          )
+        }
+      </Box>
+    </>
   )
 }
 
