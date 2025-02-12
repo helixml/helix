9bd742e282a11345f9a264ffff34e4bf2447bb59
nessie <nessie.molano@gmail.com>
2025-02-13 00:04:06 +0400

    1536

--- api/pkg/rag/rag_pgvector_test.go
+++ api/pkg/rag/rag_pgvector_test.go
@@ -91,3 +91,50 @@ func (suite *PGVectorTestSuite) TestIndex_384_gte_small() {
 	})
 	suite.Require().NoError(err)
 }
+
+func (suite *PGVectorTestSuite) TestIndex_1536_text_embedding_3_small() {
+	suite.mockProvider.EXPECT().GetClient(suite.ctx, &manager.GetClientRequest{
+		Provider: "vllm",
+	}).Return(suite.mockClient, nil)
+
+	suite.pg.cfg.RAG.PGVector.EmbeddingsModel = "text-embedding-3-small"
+
+	suite.mockClient.EXPECT().CreateEmbeddings(suite.ctx, oai.EmbeddingRequest{
+		Model: "text-embedding-3-small",
+		Input: "test-content",
+	}).Return(oai.EmbeddingResponse{
+		Data: []oai.Embedding{
+			{
+				Embedding: []float32{0.1, 0.2, 0.3},
+			},
+		},
+	}, nil)
+
+	vector := pgvector.NewVector([]float32{0.1, 0.2, 0.3})
+
+	suite.mockEmbeddingsStore.EXPECT().CreateKnowledgeEmbedding(suite.ctx, gomock.Any()).DoAndReturn(func(ctx context.Context, embeddings ...*types.KnowledgeEmbeddingItem) error {
+		suite.Equal(embeddings[0].Embedding1536, &vector)
+
+		suite.Nil(embeddings[0].Embedding384)
+		suite.Nil(embeddings[0].Embedding512)
+		suite.Nil(embeddings[0].Embedding1024)
+		suite.Nil(embeddings[0].Embedding3584)
+
+		suite.Equal("test-data-entity-id", embeddings[0].KnowledgeID)
+		suite.Equal("test-document-id", embeddings[0].DocumentID)
+		suite.Equal("test-document-group-id", embeddings[0].DocumentGroupID)
+		suite.Equal("test-content", embeddings[0].Content)
+		suite.Equal("test-source", embeddings[0].Source)
+
+		return nil
+	})
+
+	err := suite.pg.Index(suite.ctx, &types.SessionRAGIndexChunk{
+		DataEntityID:    "test-data-entity-id",
+		DocumentID:      "test-document-id",
+		DocumentGroupID: "test-document-group-id",
+		Source:          "test-source",
+		Content:         "test-content",
+	})
+	suite.Require().NoError(err)
+}
