36b6f79c79c7802f095a90cded5ac378e024878f
Phil Winder <phil@winder.ai>
2025-02-13 21:54:35 +0000

    add running to stale check to

--- api/pkg/scheduler/slot.go
+++ api/pkg/scheduler/slot.go
@@ -36,6 +36,11 @@ func NewSlot(runnerID string, work *Workload, staleTimeout TimeoutFunc, errorTim
 
 // True if the model is not active and hasn't been active for at least ModelTTL
 func (s *Slot) IsStale() bool {
+	// If work is not running yet, check for error timeout (it might never have started)
+	if !s.isRunning {
+		return s.isErrorFunc(s.RunnerID, s.LastActivityTime)
+	}
+
 	// If work is active, check for error timeout
 	if s.isActive {
 		if s.isErrorFunc(s.RunnerID, s.LastActivityTime) {
