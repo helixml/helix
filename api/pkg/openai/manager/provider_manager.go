41e1936141194cdd0e8d37b4b502e0d06d845dce
nessie <nessie.molano@gmail.com>
2025-02-15 23:33:00 +0400

    use providers from the database

--- api/pkg/openai/manager/provider_manager.go
+++ api/pkg/openai/manager/provider_manager.go
@@ -13,11 +13,13 @@ import (
 	"github.com/helixml/helix/api/pkg/config"
 	"github.com/helixml/helix/api/pkg/openai"
 	"github.com/helixml/helix/api/pkg/openai/logger"
+	"github.com/helixml/helix/api/pkg/store"
 	"github.com/helixml/helix/api/pkg/types"
 )
 
 type GetClientRequest struct {
-	Provider types.Provider
+	Provider string
+	Owner    string
 }
 
 //go:generate mockgen -source $GOFILE -destination manager_mocks.go -package $GOPACKAGE
@@ -35,14 +37,15 @@ type providerClient struct {
 }
 
 type MultiClientManager struct {
-	cfg       *config.ServerConfig
-	logStores []logger.LogStore
-	clients   map[types.Provider]*providerClient
-	clientsMu *sync.RWMutex
-	wg        sync.WaitGroup
+	cfg             *config.ServerConfig
+	store           store.Store
+	logStores       []logger.LogStore
+	globalClients   map[types.Provider]*providerClient
+	globalClientsMu *sync.RWMutex
+	wg              sync.WaitGroup
 }
 
-func NewProviderManager(cfg *config.ServerConfig, helixInference openai.Client, logStores ...logger.LogStore) *MultiClientManager {
+func NewProviderManager(cfg *config.ServerConfig, store store.Store, helixInference openai.Client, logStores ...logger.LogStore) *MultiClientManager {
 	clients := make(map[types.Provider]*providerClient)
 
 	if cfg.Providers.OpenAI.APIKey != "" {
@@ -95,10 +98,11 @@ func NewProviderManager(cfg *config.ServerConfig, helixInference openai.Client, 
 	clients[types.ProviderHelix] = &providerClient{client: loggedClient}
 
 	mcm := &MultiClientManager{
-		cfg:       cfg,
-		logStores: logStores,
-		clients:   clients,
-		clientsMu: &sync.RWMutex{},
+		cfg:             cfg,
+		store:           store,
+		logStores:       logStores,
+		globalClients:   clients,
+		globalClientsMu: &sync.RWMutex{},
 	}
 
 	return mcm
@@ -172,9 +176,9 @@ func (m *MultiClientManager) updateClientAPIKeyFromFile(provider types.Provider,
 
 	newKey := strings.TrimSpace(string(bts))
 
-	m.clientsMu.RLock()
-	client, ok := m.clients[provider]
-	m.clientsMu.RUnlock()
+	m.globalClientsMu.RLock()
+	client, ok := m.globalClients[provider]
+	m.globalClientsMu.RUnlock()
 
 	if ok && client.client.APIKey() == newKey {
 		// Nothing to do
@@ -199,19 +203,19 @@ func (m *MultiClientManager) updateClientAPIKeyFromFile(provider types.Provider,
 
 	loggedClient := logger.Wrap(m.cfg, provider, openaiClient, m.logStores...)
 
-	m.clientsMu.Lock()
-	m.clients[provider] = &providerClient{client: loggedClient}
-	m.clientsMu.Unlock()
+	m.globalClientsMu.Lock()
+	m.globalClients[provider] = &providerClient{client: loggedClient}
+	m.globalClientsMu.Unlock()
 
 	return nil
 }
 
 func (m *MultiClientManager) ListProviders(_ context.Context) ([]types.Provider, error) {
-	m.clientsMu.RLock()
-	defer m.clientsMu.RUnlock()
+	m.globalClientsMu.RLock()
+	defer m.globalClientsMu.RUnlock()
 
-	providers := make([]types.Provider, 0, len(m.clients))
-	for provider := range m.clients {
+	providers := make([]types.Provider, 0, len(m.globalClients))
+	for provider := range m.globalClients {
 		providers = append(providers, provider)
 	}
 
@@ -219,17 +223,50 @@ func (m *MultiClientManager) ListProviders(_ context.Context) ([]types.Provider,
 }
 
 func (m *MultiClientManager) GetClient(_ context.Context, req *GetClientRequest) (openai.Client, error) {
-	m.clientsMu.RLock()
-	defer m.clientsMu.RUnlock()
+	m.globalClientsMu.RLock()
+	defer m.globalClientsMu.RUnlock()
 
-	client, ok := m.clients[req.Provider]
-	if !ok {
-		availableProviders := make([]string, 0, len(m.clients))
-		for provider := range m.clients {
-			availableProviders = append(availableProviders, string(provider))
-		}
-		return nil, fmt.Errorf("no client found for provider: %s, available providers: [%s]", req.Provider, strings.Join(availableProviders, ", "))
+	client, ok := m.globalClients[types.Provider(req.Provider)]
+	if ok {
+		return client.client, nil
 	}
 
-	return client.client, nil
+	userProviders, err := m.store.ListProviderEndpoints(context.Background(), &store.ListProviderEndpointsQuery{
+		Owner:      req.Owner,
+		WithGlobal: true,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	for _, provider := range userProviders {
+		if provider.Name == req.Provider || provider.ID == req.Provider {
+			return m.initializeClient(provider)
+		}
+	}
+
+	// Check if the provider is a global one
+	availableProviders := make([]string, 0, len(m.globalClients))
+	for provider := range m.globalClients {
+		availableProviders = append(availableProviders, string(provider))
+	}
+	return nil, fmt.Errorf("no client found for provider: %s, available providers: [%s]", req.Provider, strings.Join(availableProviders, ", "))
+
+}
+
+func (m *MultiClientManager) initializeClient(endpoint *types.ProviderEndpoint) (openai.Client, error) {
+	apiKey := endpoint.APIKey
+	if endpoint.APIKeyFromFile != "" {
+		bts, err := os.ReadFile(endpoint.APIKeyFromFile)
+		if err != nil {
+			return nil, fmt.Errorf("error reading API key file '%s': %w", endpoint.APIKeyFromFile, err)
+		}
+		apiKey = strings.TrimSpace(string(bts))
+	}
+
+	openaiClient := openai.New(apiKey, endpoint.BaseURL)
+
+	loggedClient := logger.Wrap(m.cfg, types.Provider(endpoint.Name), openaiClient, m.logStores...)
+
+	return loggedClient, nil
 }
