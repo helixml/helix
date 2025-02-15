5c5e7f62960242430a4dd26afcf235d12690dba7
nessie <nessie.molano@gmail.com>
2025-02-15 16:37:50 +0400

    delete

--- api/pkg/cli/provider/delete.go
+++ api/pkg/cli/provider/delete.go
@@ -2,6 +2,7 @@ package provider
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/spf13/cobra"
 
@@ -13,10 +14,11 @@ func init() {
 }
 
 var deleteCmd = &cobra.Command{
-	Use:   "delete [id]",
-	Short: "Delete a provider endpoint",
-	Long:  ``,
-	Args:  cobra.ExactArgs(1),
+	Use:     "delete [id]",
+	Short:   "Delete a provider endpoint",
+	Aliases: []string{"rm"},
+	Long:    ``,
+	Args:    cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		apiClient, err := client.NewClientFromEnv()
 		if err != nil {
@@ -24,6 +26,21 @@ var deleteCmd = &cobra.Command{
 		}
 
 		id := args[0]
+
+		// If provider ID does not start with "pe_" then look up by name
+		if !strings.HasPrefix(id, "pe_") {
+			providers, err := apiClient.ListProviderEndpoints(cmd.Context())
+			if err != nil {
+				return fmt.Errorf("failed to get provider endpoint: %w", err)
+			}
+
+			for _, provider := range providers {
+				if provider.Name == id {
+					id = provider.ID
+					break
+				}
+			}
+		}
 
 		err = apiClient.DeleteProviderEndpoint(cmd.Context(), id)
 		if err != nil {
