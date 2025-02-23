dfea01b438e90c31bf03f0380b553b4690ad3d23
nessie <nessie.molano@gmail.com>
2025-02-23 18:54:05 +0400

    org handlers

---
+++ api/pkg/server/authz.go
@@ -0,0 +1,31 @@
+package server
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/helixml/helix/api/pkg/store"
+	"github.com/helixml/helix/api/pkg/types"
+)
+
+// authorizeOrgOwner used to check if the user is an owner of the organization to perform certain actions
+// such as creating, updating teams, updating or deleting organization
+func (apiServer *HelixAPIServer) authorizeOrgOwner(ctx context.Context, user *types.User, orgID string) error {
+	memberships, err := apiServer.Store.ListOrganizationMemberships(ctx, &store.ListOrganizationMembershipsQuery{
+		OrganizationID: orgID,
+		UserID:         user.ID,
+	})
+	if err != nil {
+		return err
+	}
+
+	if len(memberships) == 0 {
+		return fmt.Errorf("user is not a member of this organization")
+	}
+
+	if memberships[0].Role != types.OrganizationRoleOwner {
+		return fmt.Errorf("user is not an owner of this organization")
+	}
+
+	return nil
+}
