f7ab23d0408e78c10bab6e4422ca4cac213559d4
nessie <nessie.molano@gmail.com>
2025-02-23 19:38:13 +0400

    add org member

--- api/pkg/server/organization_member_handlers.go
+++ api/pkg/server/organization_member_handlers.go
@@ -1,10 +1,12 @@
 package server
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	"github.com/helixml/helix/api/pkg/store"
+	"github.com/helixml/helix/api/pkg/types"
 	"github.com/rs/zerolog/log"
 )
 
@@ -37,4 +39,56 @@ func (apiServer *HelixAPIServer) listOrganizationMembers(rw http.ResponseWriter,
 	}
 
 	writeResponse(rw, members, http.StatusOK)
+}
+
+// addOrganizationMember godoc
+// @Summary Add an organization member
+// @Description Add a member to an organization
+// @Tags    organizations
+// @Success 200 {object} types.OrganizationMembership
+// @Router /api/v1/organizations/{id}/members [post]
+// @Security BearerAuth
+func (apiServer *HelixAPIServer) addOrganizationMember(rw http.ResponseWriter, r *http.Request) {
+	user := getRequestUser(r)
+	orgID := mux.Vars(r)["id"]
+
+	// Check if user has access to view members
+	err := apiServer.authorizeOrgMember(r.Context(), user, orgID)
+	if err != nil {
+		log.Err(err).Msg("error authorizing org owner")
+		http.Error(rw, "Could not authorize org owner: "+err.Error(), http.StatusForbidden)
+		return
+	}
+
+	var req types.AddOrganizationMemberRequest
+	err = json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		log.Err(err).Msg("error decoding request body")
+		http.Error(rw, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	// Find user
+	newMember, err := apiServer.Store.GetUser(r.Context(), &store.GetUserQuery{
+		Email: req.UserEmail,
+	})
+	if err != nil {
+		log.Err(err).Msg("error getting user")
+		http.Error(rw, "Internal server error: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Create membership
+	membership, err := apiServer.Store.CreateOrganizationMembership(r.Context(), &types.OrganizationMembership{
+		OrganizationID: orgID,
+		UserID:         newMember.ID,
+		Role:           types.OrganizationRoleMember,
+	})
+	if err != nil {
+		log.Err(err).Msg("error creating organization membership")
+		http.Error(rw, "Internal server error: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	writeResponse(rw, membership, http.StatusCreated)
 }
