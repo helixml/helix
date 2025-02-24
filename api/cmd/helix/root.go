b71895a11ce2466e3005f1605fabcf2197ac0b6f
nessie <nessie.molano@gmail.com>
2025-02-24 13:01:21 +0400

    register

--- api/cmd/helix/root.go
+++ api/cmd/helix/root.go
@@ -12,6 +12,7 @@ import (
 	"github.com/helixml/helix/api/pkg/cli/mcp"
 	"github.com/helixml/helix/api/pkg/cli/organization"
 	"github.com/helixml/helix/api/pkg/cli/provider"
+	"github.com/helixml/helix/api/pkg/cli/roles"
 	"github.com/helixml/helix/api/pkg/cli/secret"
 )
 
@@ -38,6 +39,7 @@ func NewRootCmd() *cobra.Command {
 	RootCmd.AddCommand(mcp.New())
 	RootCmd.AddCommand(provider.New())
 	RootCmd.AddCommand(organization.New())
+	RootCmd.AddCommand(roles.New())
 	// Commands available on all platforms
 	RootCmd.AddCommand(newServeCmd())
 	RootCmd.AddCommand(newVersionCommand())
