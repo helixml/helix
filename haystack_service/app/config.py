fcdd4c46f550885c3956a523b75c1597b9218707
Luke Marsden <me@lukemarsden.net>
2025-02-25 02:33:06 +0000

    tidy

--- haystack_service/app/config.py
+++ haystack_service/app/config.py
@@ -7,13 +7,13 @@ class Settings:
     PGVECTOR_TABLE: str = os.getenv("PGVECTOR_TABLE", "haystack_documents")
     
     # Document processing
-    CHUNK_SIZE: int = int(os.getenv("CHUNK_SIZE", "500"))
-    CHUNK_OVERLAP: int = int(os.getenv("CHUNK_OVERLAP", "50"))
+    CHUNK_SIZE: int = int(os.getenv("CHUNK_SIZE", "250"))
+    CHUNK_OVERLAP: int = int(os.getenv("CHUNK_OVERLAP", "25"))
     
     # Embedding model settings
     EMBEDDING_DIM: int = int(os.getenv("RAG_HAYSTACK_EMBEDDINGS_DIM", "384"))  # GTE-small is 384 dimensions
-    RAG_HAYSTACK_EMBEDDINGS_MODEL: str = os.getenv("RAG_HAYSTACK_EMBEDDINGS_MODEL", "thenlper/gte-small")
-    RAG_HAYSTACK_EMBEDDINGS_MAX_TOKENS: int = int(os.getenv("RAG_HAYSTACK_EMBEDDINGS_MAX_TOKENS", "512"))
+    EMBEDDINGS_MODEL: str = os.getenv("RAG_HAYSTACK_EMBEDDINGS_MODEL", "thenlper/gte-small")
+    EMBEDDINGS_MAX_TOKENS: int = int(os.getenv("RAG_HAYSTACK_EMBEDDINGS_MAX_TOKENS", "512"))
     VLLM_BASE_URL: str = os.environ["VLLM_BASE_URL"]
     VLLM_API_KEY: str = os.getenv("VLLM_API_KEY", "EMPTY")
     
