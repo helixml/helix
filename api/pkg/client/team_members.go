098083168fbd96b8cffd03aec4f9bf287b4d853f
nessie <nessie.molano@gmail.com>
2025-02-25 09:31:06 +0400

    team methods

---
+++ api/pkg/client/team_members.go
@@ -0,0 +1,47 @@
+package client
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/helixml/helix/api/pkg/types"
+)
+
+// ListTeamMembers lists all members of a team
+func (c *HelixClient) ListTeamMembers(ctx context.Context, organizationID, teamID string) ([]*types.TeamMembership, error) {
+	url := fmt.Sprintf("/organizations/%s/teams/%s/members", organizationID, teamID)
+
+	var members []*types.TeamMembership
+	err := c.makeRequest(ctx, http.MethodGet, url, nil, &members)
+	if err != nil {
+		return nil, fmt.Errorf("error listing team members: %w", err)
+	}
+
+	return members, nil
+}
+
+// AddTeamMember adds a new member to a team
+func (c *HelixClient) AddTeamMember(ctx context.Context, organizationID, teamID, userID string) (*types.TeamMembership, error) {
+	url := fmt.Sprintf("/organizations/%s/teams/%s/members/%s", organizationID, teamID, userID)
+
+	var membership *types.TeamMembership
+	err := c.makeRequest(ctx, http.MethodPost, url, nil, &membership)
+	if err != nil {
+		return nil, fmt.Errorf("error adding team member: %w", err)
+	}
+
+	return membership, nil
+}
+
+// RemoveTeamMember removes a member from a team
+func (c *HelixClient) RemoveTeamMember(ctx context.Context, organizationID, teamID, userID string) error {
+	url := fmt.Sprintf("/organizations/%s/teams/%s/members/%s", organizationID, teamID, userID)
+
+	err := c.makeRequest(ctx, http.MethodDelete, url, nil, nil)
+	if err != nil {
+		return fmt.Errorf("error removing team member: %w", err)
+	}
+
+	return nil
+}
