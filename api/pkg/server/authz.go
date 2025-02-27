365ec6ed929cf7ec98d50c8974f64665b0021488
nessie <nessie.molano@gmail.com>
2025-02-27 13:18:39 +0400

    get membership

--- api/pkg/server/authz.go
+++ api/pkg/server/authz.go
@@ -10,44 +10,44 @@ import (
 
 // authorizeOrgOwner used to check if the user is an owner of the organization to perform certain actions
 // such as creating, updating teams, updating or deleting organization
-func (apiServer *HelixAPIServer) authorizeOrgOwner(ctx context.Context, user *types.User, orgID string) error {
+func (apiServer *HelixAPIServer) authorizeOrgOwner(ctx context.Context, user *types.User, orgID string) (*types.OrganizationMembership, error) {
 	memberships, err := apiServer.Store.ListOrganizationMemberships(ctx, &store.ListOrganizationMembershipsQuery{
 		OrganizationID: orgID,
 		UserID:         user.ID,
 	})
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	if len(memberships) == 0 {
-		return fmt.Errorf("user is not a member of this organization")
+		return nil, fmt.Errorf("user is not a member of this organization")
 	}
 
 	if memberships[0].Role != types.OrganizationRoleOwner {
-		return fmt.Errorf("user is not an owner of this organization")
+		return nil, fmt.Errorf("user is not an owner of this organization")
 	}
 
-	return nil
+	return memberships[0], nil
 }
 
 // deleting used to check if the user is a member of the organization to perform certain actions
 // such as listing teams, listing members, etc
-func (apiServer *HelixAPIServer) authorizeOrgMember(ctx context.Context, user *types.User, orgID string) error {
+func (apiServer *HelixAPIServer) authorizeOrgMember(ctx context.Context, user *types.User, orgID string) (*types.OrganizationMembership, error) {
 	memberships, err := apiServer.Store.ListOrganizationMemberships(ctx, &store.ListOrganizationMembershipsQuery{
 		OrganizationID: orgID,
 		UserID:         user.ID,
 	})
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	if len(memberships) == 0 {
-		return fmt.Errorf("user is not a member of this organization")
+		return nil, fmt.Errorf("user is not a member of this organization")
 	}
 
 	// Both roles (owner or member) can list teams and members
 
-	return nil
+	return memberships[0], nil
 }
 
 func (apiServer *HelixAPIServer) authorizeUserToApp(ctx context.Context, user *types.User, app *types.App, action types.Action) error {
