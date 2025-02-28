c71bd3e2e3d17bec85039da08e634ba8585063aa
nessie <nessie.molano@gmail.com>
2025-02-28 11:27:12 +0400

    team test

--- api/pkg/server/access_grant_handlers_test.go
+++ api/pkg/server/access_grant_handlers_test.go
@@ -274,3 +274,77 @@ func (suite *AppAccessGrantSuite) TestListAppAccessGrants_OrgMember_HasDirectAcc
 	suite.Equal(http.StatusOK, rec.Code)
 	suite.Contains(rec.Body.String(), `access_grant_id_test`)
 }
+
+func (suite *AppAccessGrantSuite) TestListAppAccessGrants_OrgMember_HasTeamAccess() {
+	app := &types.App{
+		ID:             "app_id_test",
+		OrganizationID: suite.orgID,
+	}
+
+	suite.store.EXPECT().GetApp(gomock.Any(), app.ID).Return(app, nil)
+
+	orgMembership := &types.OrganizationMembership{
+		OrganizationID: app.OrganizationID,
+		Role:           types.OrganizationRoleMember,
+	}
+
+	suite.store.EXPECT().GetOrganizationMembership(gomock.Any(), &store.GetOrganizationMembershipQuery{
+		OrganizationID: app.OrganizationID,
+		UserID:         suite.userID,
+	}).Return(orgMembership, nil)
+
+	suite.store.EXPECT().ListTeams(gomock.Any(), &store.ListTeamsQuery{
+		OrganizationID: app.OrganizationID,
+		UserID:         suite.userID,
+	}).Return([]*types.Team{
+		{
+			ID: "team_id_test",
+		},
+	}, nil) // One team
+
+	suite.store.EXPECT().ListAccessGrants(gomock.Any(), &store.ListAccessGrantsQuery{
+		OrganizationID: app.OrganizationID,
+		UserID:         suite.userID,
+		ResourceID:     app.ID,
+		ResourceType:   types.ResourceAccessGrants,
+		TeamIDs:        []string{"team_id_test"},
+	}).Return([]*types.AccessGrant{
+		{
+			ResourceType: types.ResourceAccessGrants,
+			Roles: []types.Role{
+				{
+					Config: types.Config{
+						Rules: []types.Rule{
+							{
+								Resources: []types.Resource{types.ResourceAccessGrants},
+								Actions:   []types.Action{types.ActionGet},
+							},
+						},
+					},
+				},
+			},
+		},
+	}, nil) // Read access to access grants
+
+	suite.store.EXPECT().ListAccessGrants(gomock.Any(), &store.ListAccessGrantsQuery{
+		OrganizationID: app.OrganizationID,
+		ResourceID:     app.ID,
+	}).Return([]*types.AccessGrant{
+		{
+			ID: "access_grant_id_test",
+		},
+	}, nil)
+
+	rec := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/api/v1/apps/app_id_test/access-grants", http.NoBody)
+	req = req.WithContext(suite.authCtx)
+	vars := map[string]string{
+		"id": "app_id_test",
+	}
+	req = mux.SetURLVars(req, vars)
+
+	suite.server.listAppAccessGrants(rec, req)
+
+	suite.Equal(http.StatusOK, rec.Code)
+	suite.Contains(rec.Body.String(), `access_grant_id_test`)
+}
