1c7f4f947792b788e02943448667969050760f69
Phil Winder <phil@winder.ai>
2025-03-06 11:01:00 +0000

    test(auth): add auth tests

--- api/pkg/auth/auth.go
+++ api/pkg/auth/auth.go
@@ -3,7 +3,9 @@ package auth
 import (
 	"context"
 
+	"github.com/coreos/go-oidc"
 	jwt "github.com/golang-jwt/jwt/v5"
+	"golang.org/x/oauth2"
 
 	"github.com/helixml/helix/api/pkg/types"
 )
@@ -15,4 +17,15 @@ type Authenticator interface {
 
 type AuthenticatorOIDC interface {
 	ValidateUserToken(ctx context.Context, accessToken string) (*types.User, error)
+}
+
+type OIDC interface {
+	AuthenticatorOIDC
+	GetAuthURL(state, nonce string) string
+	Exchange(ctx context.Context, code string) (*oauth2.Token, error)
+	VerifyIDToken(ctx context.Context, token *oauth2.Token) (*oidc.IDToken, error)
+	VerifyAccessToken(ctx context.Context, accessToken string) error
+	RefreshAccessToken(ctx context.Context, refreshToken string) (*oauth2.Token, error)
+	GetUserInfo(ctx context.Context, accessToken string) (*UserInfo, error)
+	GetLogoutURL(redirectURI string) string
 }
