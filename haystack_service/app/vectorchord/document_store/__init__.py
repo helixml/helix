bb9ed4a0332d7e55c5d507fcf015862dd4ae913a
Luke Marsden <me@lukemarsden.net>
2025-03-10 19:27:26 +0000

    passing test

---
+++ haystack_service/app/vectorchord/document_store/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-FileCopyrightText: 2023-present deepset GmbH <info@deepset.ai> and HelixML, Inc <luke@helix.ml>
+#
+# SPDX-License-Identifier: Apache-2.0
+from .document_store import VectorchordDocumentStore
+
+__all__ = ["VectorchordDocumentStore"]
