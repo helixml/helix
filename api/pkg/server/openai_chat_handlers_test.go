20d691edf4e5091ce3e95e95083252461c2e42fd
nessie <nessie.molano@gmail.com>
2025-03-20 16:01:24 +0400

    tests

--- api/pkg/server/openai_chat_handlers_test.go
+++ api/pkg/server/openai_chat_handlers_test.go
@@ -998,7 +998,8 @@ func (suite *OpenAIChatSuite) TestChatCompletions_App_Streaming() {
 		},
 	}
 
-	suite.store.EXPECT().GetAppWithTools(gomock.Any(), "app123").Return(app, nil).Times(1)
+	suite.store.EXPECT().GetApp(gomock.Any(), "app123").Return(app, nil).Times(1)
+	suite.store.EXPECT().GetAppWithTools(gomock.Any(), "app123").Return(app, nil).Times(2)
 	suite.store.EXPECT().ListSecrets(gomock.Any(), &store.ListSecretsQuery{
 		Owner: suite.userID,
 	}).Return([]*types.Secret{}, nil)
