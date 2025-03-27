b154fff095f70c65b733ce70817fd16a39426bdf
nessie <nessie.molano@gmail.com>
2025-03-27 10:54:42 +0400

    types, app update

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -602,6 +602,7 @@ export interface IAssistantConfig {
   rag_source_id?: string;
   lora_id?: string;
   is_actionable_template?: string;
+  is_actionable_history_length?: number;
   apis?: IAssistantApi[];
   gptscripts?: IAssistantGPTScript[];
   zapier?: IAssistantZapier[];
@@ -765,6 +766,8 @@ export interface IAppFlatState {
   model?: string
   provider?: string
   knowledge?: IKnowledgeSource[] // Added knowledge parameter
+  is_actionable_template?: string;
+  is_actionable_history_length?: number;
   apiTools?: IAssistantApi[]
   zapierTools?: IAssistantZapier[]
   gptscriptTools?: IAssistantGPTScript[]
