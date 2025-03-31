393b7d0d95260029a2de4dcfb02bc33f5ac6825b
nessie <nessie.molano@gmail.com>
2025-03-31 21:41:14 +0400

    cleanup

--- api/cmd/helix/serve.go
+++ api/cmd/helix/serve.go
@@ -51,7 +51,6 @@ func NewServeConfig() (*config.ServerConfig, error) {
 
 	serverConfig.Janitor.AppURL = serverConfig.WebServer.URL
 	serverConfig.Stripe.AppURL = serverConfig.WebServer.URL
-	serverConfig.WebServer.LocalFilestorePath = serverConfig.FileStore.LocalFSPath
 
 	if serverConfig.GitHub.Enabled {
 		if serverConfig.GitHub.ClientID == "" {
