17da3b193f9bcde86beb6d4805036c4375bc9cce
nessie <nessie.molano@gmail.com>
2025-04-06 18:22:43 +0400

    more funcs

--- frontend/src/services/providersService.ts
+++ frontend/src/services/providersService.ts
@@ -1,5 +1,6 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import useApi from '../hooks/useApi';
+import { TypesProviderEndpoint, RequestParams, TypesUpdateProviderEndpoint } from '../api/api';
 
 export const providersQueryKey = (loadModels: boolean = false) => [
   "providers",
@@ -20,5 +21,42 @@ export function useListProviders(loadModels: boolean = false) {
     },
     enabled: true,
     staleTime: 2 * 60 * 1000,
+  });
+}
+
+export function useCreateProviderEndpoint() {
+  const api = useApi()
+  const apiClient = api.getApiClient()  
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    
+
+    mutationFn: async (providerEndpoint: Partial<TypesProviderEndpoint>) => {
+      const result = await apiClient.v1ProviderEndpointsCreate(providerEndpoint as RequestParams)
+      return result.data
+    },
+    onSuccess: () => {
+      // Invalidate provider queries to refetch the list
+      queryClient.invalidateQueries({ queryKey: providersQueryKey() })
+      queryClient.invalidateQueries({ queryKey: providersQueryKey(true) })
+    }
+  });
+}
+
+export function useUpdateProviderEndpoint(id: string, providerEndpoint: Partial<TypesUpdateProviderEndpoint>) {
+  const api = useApi()
+  const apiClient = api.getApiClient()  
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async () => {
+      const result = await apiClient.v1ProviderEndpointsUpdate(id, providerEndpoint as RequestParams)
+      return result.data
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: providersQueryKey() })
+      queryClient.invalidateQueries({ queryKey: providersQueryKey(true) })
+    }
   });
 }
