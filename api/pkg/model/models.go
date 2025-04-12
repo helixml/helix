e29ff95a887910fbee310c8fa31d2e3408bf6eaa
Luke Marsden <me@lukemarsden.net>
2025-04-12 09:48:08 +0100

    revert

--- api/pkg/model/models.go
+++ api/pkg/model/models.go
@@ -463,11 +463,10 @@ func GetDefaultVLLMModels() ([]*VLLMGenericText, error) {
 			ContextLength: 8192,
 			Description:   "Small embedding model for RAG, from MrLight",
 			Args: []string{
-				"--task", "text-embedding",
+				"--task", "embed",
 				"--max-model-len", "8192",
 				"--trust-remote-code",
 				"--chat-template", "examples/template_dse_qwen2_vl.jinja",
-				"--openai-compatible-embeddings",
 			},
 			Hide: false,
 		},
