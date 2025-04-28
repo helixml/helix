381b174d9d6e6eb11eaf612f7216aed2fdbf999d
nessie <nessie.molano@gmail.com>
2025-04-28 16:55:59 +0400

    list

--- api/pkg/server/helix_model_handlers.go
+++ api/pkg/server/helix_model_handlers.go
@@ -1,12 +1,39 @@
 package server
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/helixml/helix/api/pkg/store"
+	"github.com/helixml/helix/api/pkg/types"
+	"github.com/rs/zerolog/log"
+)
 
 func (apiServer *HelixAPIServer) listHelixModels(rw http.ResponseWriter, r *http.Request) {
+	q := &store.ListModelsQuery{}
 
+	if r.URL.Query().Get("type") != "" {
+		q.Type = types.ModelType(r.URL.Query().Get("type"))
+	}
+
+	if r.URL.Query().Get("name") != "" {
+		q.Name = r.URL.Query().Get("name")
+	}
+
+	if r.URL.Query().Get("runtime") != "" {
+		q.Runtime = types.ModelRuntimeType(r.URL.Query().Get("runtime"))
+	}
+
+	models, err := apiServer.Store.ListModels(r.Context(), q)
+	if err != nil {
+		log.Error().Err(err).Msg("error listing helix models")
+		http.Error(rw, "Internal server error: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	writeResponse(rw, models, http.StatusOK)
 }
 
-func (apiServer *HelixAPIServer) createHelixModels(rw http.ResponseWriter, r *http.Request) {
+func (apiServer *HelixAPIServer) createHelixModel(rw http.ResponseWriter, r *http.Request) {
 
 }
 
