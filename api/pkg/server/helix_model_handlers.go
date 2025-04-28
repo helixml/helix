78400181350677c7e7c823d2c14d930ee9c733b0
nessie <nessie.molano@gmail.com>
2025-04-28 16:46:31 +0400

    skeleton

---
+++ api/pkg/server/helix_model_handlers.go
@@ -0,0 +1,19 @@
+package server
+
+import "net/http"
+
+func (apiServer *HelixAPIServer) listHelixModels(rw http.ResponseWriter, r *http.Request) {
+
+}
+
+func (apiServer *HelixAPIServer) createHelixModels(rw http.ResponseWriter, r *http.Request) {
+
+}
+
+func (apiServer *HelixAPIServer) updateHelixModel(rw http.ResponseWriter, r *http.Request) {
+
+}
+
+func (apiServer *HelixAPIServer) deleteHelixModel(rw http.ResponseWriter, r *http.Request) {
+
+}
