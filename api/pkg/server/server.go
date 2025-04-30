735419540917407fa36cdf3f839199153d1c3c4f
nessie <nessie.molano@gmail.com>
2025-05-01 00:40:45 +0400

    fix ID matching

--- api/pkg/server/server.go
+++ api/pkg/server/server.go
@@ -450,8 +450,8 @@ func (apiServer *HelixAPIServer) registerRoutes(_ context.Context) (*mux.Router,
 	authRouter.HandleFunc("/helix-models", apiServer.listHelixModels).Methods(http.MethodGet)
 	// only admins can create, update, or delete helix models
 	adminRouter.HandleFunc("/helix-models", apiServer.createHelixModel).Methods(http.MethodPost)
-	adminRouter.HandleFunc("/helix-models/{id}", apiServer.updateHelixModel).Methods(http.MethodPut)
-	adminRouter.HandleFunc("/helix-models/{id}", apiServer.deleteHelixModel).Methods(http.MethodDelete)
+	adminRouter.HandleFunc("/helix-models/{id:.*}", apiServer.updateHelixModel).Methods(http.MethodPut)
+	adminRouter.HandleFunc("/helix-models/{id:.*}", apiServer.deleteHelixModel).Methods(http.MethodDelete)
 
 	// all these routes are secured via runner tokens
 	insecureRouter.HandleFunc("/runner/{runner_id}/ws", func(w http.ResponseWriter, r *http.Request) {
