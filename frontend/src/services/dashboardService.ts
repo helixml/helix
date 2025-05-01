f58b420f9174e22ec5e8f0ebfaf515a93fd469ab
nessie <nessie.molano@gmail.com>
2025-05-01 17:20:03 +0400

    dashboard svc

---
+++ frontend/src/services/dashboardService.ts
@@ -0,0 +1,21 @@
+import { useQuery } from '@tanstack/react-query'
+import useApi from '../hooks/useApi';
+
+export const dashboardQueryKey = () => [
+  "dashboard"
+];
+
+export function useGetDashboardData() {
+  const api = useApi()
+  const apiClient = api.getApiClient()  
+
+  return useQuery({
+    queryKey: dashboardQueryKey(),
+    queryFn: async () => {
+      const result = await apiClient.v1DashboardList()
+      return result.data
+    },
+    enabled: true,
+    staleTime: 1000, // 1 second
+  });
+}
