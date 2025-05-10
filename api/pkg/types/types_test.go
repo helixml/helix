74efded40966ea464b2ca20a1d9eb30fd158165b
nessie <nessie.molano@gmail.com>
2025-05-10 17:28:23 +0400

    tests

--- api/pkg/types/types_test.go
+++ api/pkg/types/types_test.go
@@ -86,3 +86,42 @@ func Test_SessionChatRequest_ImageURL_AndText(t *testing.T) {
 	require.True(t, ok)
 	require.Equal(t, "Hello, world!", message)
 }
+
+func Test_GetMessageMultiContentPart_TextOnly(t *testing.T) {
+	interaction := &Interaction{
+		Content: MessageContent{
+			Parts: []any{TextPart{
+				Type: "text",
+				Text: "Hello, world!",
+			}},
+		},
+	}
+
+	parts := interaction.GetMessageMultiContentPart()
+	require.Equal(t, 1, len(parts))
+	require.Equal(t, "text", string(parts[0].Type))
+	require.Equal(t, "Hello, world!", parts[0].Text)
+}
+
+func Test_GetMessageMultiContentPart_WithImageURL(t *testing.T) {
+	interaction := &Interaction{
+		Content: MessageContent{
+			Parts: []any{TextPart{
+				Type: "text",
+				Text: "Hello, world!",
+			},
+				ImageURLPart{
+					Type:     "image_url",
+					ImageURL: ImageURLData{URL: "https://example.com/image.png"},
+				},
+			},
+		},
+	}
+
+	parts := interaction.GetMessageMultiContentPart()
+	require.Equal(t, 2, len(parts))
+	require.Equal(t, "text", string(parts[0].Type))
+	require.Equal(t, "Hello, world!", parts[0].Text)
+	require.Equal(t, "image_url", string(parts[1].Type))
+	require.Equal(t, "https://example.com/image.png", parts[1].ImageURL.URL)
+}
