62d6c1a2902d386d989dd367f35923f79162584d
nessie <nessie.molano@gmail.com>
2025-05-19 15:49:14 +0400

    xx

--- api/pkg/agent/llm_client.go
+++ api/pkg/agent/llm_client.go
@@ -40,70 +40,16 @@ func NewLLM(apiKey string, baseURL string, reasoningModel string, generationMode
 	}
 }
 
-// func optsWithIds(ctx context.Context, opts []option.RequestOption, useHeader bool) []option.RequestOption {
-// 	identifiers := make(map[string]interface{})
-
-// 	if sessionID, ok := ctx.Value(ContextKey("sessionID")).(string); ok {
-// 		if useHeader {
-// 			identifiers["custom_identifier"] = sessionID
-// 		} else {
-// 			opts = append(opts, option.WithJSONSet("custom_identifier", sessionID))
-// 		}
-// 	}
-
-// 	if customerID, ok := ctx.Value(ContextKey("customerID")).(string); ok {
-// 		if useHeader {
-// 			identifiers["customer_identifier"] = customerID
-// 		} else {
-// 			opts = append(opts, option.WithJSONSet("customer_identifier", customerID))
-// 		}
-// 	}
-
-// 	if extraMeta, ok := ctx.Value(ContextKey("extra")).(map[string]string); ok {
-// 		if useHeader {
-// 			identifiers["metadata"] = extraMeta
-// 		} else {
-// 			metadata := make(map[string]string)
-// 			for key, value := range extraMeta {
-// 				metadata[key] = value
-// 			}
-// 			opts = append(opts, option.WithJSONSet("metadata", metadata))
-// 		}
-// 	} else if useHeader {
-// 		identifiers["metadata"] = make(map[string]string)
-// 	}
-
-// 	if useHeader && len(identifiers) > 0 {
-// 		jsonData, err := json.Marshal(identifiers)
-// 		if err != nil {
-// 			// If marshaling fails, we'll just return the opts without the header
-// 			return opts
-// 		}
-// 		opts = append(opts, option.WithHeader("X-Data-Keywordsai-Params", base64.StdEncoding.EncodeToString(jsonData)))
-// 	}
-
-// 	return opts
-// }
-
 // TODO failures like too long, non-processable etc from the LLM needs to be handled
 func (c *LLM) New(ctx context.Context, params openai.ChatCompletionNewParams) (*openai.ChatCompletion, error) {
-	opts := []option.RequestOption{}
-	// opts = optsWithIds(ctx, opts, false)
-	return c.client.Chat.Completions.New(ctx, params, opts...)
+
+	return c.client.Chat.Completions.New(ctx, params)
 }
 
 func (c *LLM) NewStreaming(ctx context.Context, params openai.ChatCompletionNewParams) *ssestream.Stream[openai.ChatCompletionChunk] {
-	opts := []option.RequestOption{}
-	// opts = optsWithIds(ctx, opts, false)
-	return c.client.Chat.Completions.NewStreaming(ctx, params, opts...)
-}
 
-// NewResponse creates a new response using OpenAI's Response API
-// func (c *LLM) NewResponse(ctx context.Context, params responses.ResponseNewParams) (*responses.Response, error) {
-// 	opts := []option.RequestOption{}
-// 	// opts = optsWithIds(ctx, opts, true)
-// 	return c.client.Responses.New(ctx, params, opts...)
-// }
+	return c.client.Chat.Completions.NewStreaming(ctx, params)
+}
 
 func GenerateSchema[T any]() interface{} {
 	reflector := jsonschema.Reflector{
