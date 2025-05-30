fae4d75100a68aaae0a2e4f5c58454c06d809538
nessie <nessie.molano@gmail.com>
2025-05-30 13:25:36 +0400

    colours

--- frontend/src/components/app/AppUsage.tsx
+++ frontend/src/components/app/AppUsage.tsx
@@ -310,8 +310,8 @@ const AppLogsTable: FC<AppLogsTableProps> = ({ appId }) => {
           >
             <defs>
               <linearGradient id="usageGradient" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="0%" stopColor="#00c8ff" stopOpacity={0.5} />
-                <stop offset="100%" stopColor="#070714" stopOpacity={0.1} />
+                <stop offset="0%" stopColor="#00c8ff" stopOpacity={0.8} />
+                <stop offset="100%" stopColor="#6f00ff" stopOpacity={0.1} />
               </linearGradient>
             </defs>
           </LineChart>
