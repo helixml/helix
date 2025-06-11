55c7c3543efa48f443590a1ec9957e533a4c09d8
nessie <nessie.molano@gmail.com>
2025-06-11 09:25:12 +0400

    comment

--- frontend/src/components/app/PreviewPanel.tsx
+++ frontend/src/components/app/PreviewPanel.tsx
@@ -231,6 +231,11 @@ const PreviewPanel: React.FC<PreviewPanelProps> = ({
         borderRight: '1px solid #303047',
         borderBottom: '1px solid #303047',
         overflow: 'hidden',
+        // Not sure why we need to set this to
+        // 35 to achieve the same rounded corners as the
+        // tab contents in App.tsx (there they are 8)
+        borderTopRightRadius: 35,
+        borderBottomRightRadius: 35,
       }}
     >
       {!app && image && (
