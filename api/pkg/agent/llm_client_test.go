e73b461278633da546cfcc6145c287e90fd798a5
nessie <nessie.molano@gmail.com>
2025-06-21 12:02:09 +0400

    test

--- api/pkg/agent/llm_client_test.go
+++ api/pkg/agent/llm_client_test.go
@@ -2,6 +2,7 @@ package agent
 
 import (
 	"context"
+	"strings"
 	"testing"
 
 	helix_openai "github.com/helixml/helix/api/pkg/openai"
@@ -93,6 +94,40 @@ func (s *LLMClientTestSuite) Test_LLMClient_NewToolCall() {
 
 	// Check that the tool call ID is set
 	s.Require().Equal("tool_call_id", llm.Choices[0].Message.ToolCalls[0].ID)
+	// Check function name
+	s.Require().Equal("func-1", llm.Choices[0].Message.ToolCalls[0].Function.Name)
+}
+
+func (s *LLMClientTestSuite) Test_LLMClient_NewToolCall_NoID() {
+	s.openaiClient.EXPECT().CreateChatCompletion(context.Background(), gomock.Any()).Return(openai.ChatCompletionResponse{
+		Choices: []openai.ChatCompletionChoice{
+			{
+				Message: openai.ChatCompletionMessage{
+					Role: openai.ChatMessageRoleAssistant,
+					ToolCalls: []openai.ToolCall{
+						{
+							ID: "", // No ID
+							Function: openai.FunctionCall{
+								Name: "func-1",
+							},
+						},
+					},
+				},
+			},
+		},
+	}, nil)
+
+	llm, err := s.llm.New(context.Background(), s.llm.GenerationModel, openai.ChatCompletionRequest{
+		Model: "gpt-4o-mini",
+	})
+
+	s.Require().NoError(err)
+	s.Require().NotNil(llm)
+
+	// Check that the tool call ID is set
+	s.Require().NotEmpty(llm.Choices[0].Message.ToolCalls[0].ID)
+	// It needs to start with call_
+	s.Require().True(strings.HasPrefix(llm.Choices[0].Message.ToolCalls[0].ID, "call_"))
 	// Check function name
 	s.Require().Equal("func-1", llm.Choices[0].Message.ToolCalls[0].Function.Name)
 }
