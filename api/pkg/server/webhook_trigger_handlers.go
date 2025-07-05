51089a17ee87fa56a3b99d87f706f3e23a9135e4
nessie <nessie.molano@gmail.com>
2025-07-05 19:26:34 +0400

    placeholder

--- api/pkg/server/webhook_trigger_handlers.go
+++ api/pkg/server/webhook_trigger_handlers.go
@@ -1,14 +1,37 @@
 package server
 
-import "net/http"
+import (
+	"io"
+	"net/http"
 
-// webhookTriggerHandler godoc
-// @Summary Webhook trigger handler
-// @Description Handles webhook trigger requests from Azure DevOps, GitHub, etc.
-// @Tags    webhook-triggers
-// @Accept  json
-// @Produce json
-// @Param   id path string true "Trigger configuration ID"
+	"github.com/helixml/helix/api/pkg/store"
+	"github.com/rs/zerolog/log"
+)
+
+// webhookTriggerHandler - webhook trigger handler for Azure DevOps, GitHub, etc.
+// Body can be dynamic and configuration will be looked up through the trigger configuration ID.
 func (s *HelixAPIServer) webhookTriggerHandler(w http.ResponseWriter, r *http.Request) {
+	id := getID(r)
+
+	triggerConfig, err := s.Store.GetTriggerConfiguration(r.Context(), &store.GetTriggerConfigurationQuery{
+		ID: id,
+	})
+	if err != nil {
+		writeErrResponse(w, err, http.StatusNotFound)
+		return
+	}
+
+	defer r.Body.Close()
+
+	bts, err := io.ReadAll(r.Body)
+	if err != nil {
+		writeErrResponse(w, err, http.StatusInternalServerError)
+		return
+	}
+
+	log.Info().
+		Str("payload", string(bts)).
+		Any("trigger_config", triggerConfig).
+		Msgf("Received webhook trigger for trigger configuration %s", id)
 
 }
