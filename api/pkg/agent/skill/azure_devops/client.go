9f5081aac59b5a47364eaa5063b30e6718f9a010
nessie <nessie.molano@gmail.com>
2025-07-09 12:30:53 +0400

    client setup

--- api/pkg/agent/skill/azure_devops/client.go
+++ api/pkg/agent/skill/azure_devops/client.go
@@ -1,17 +1,54 @@
 package azuredevops
 
 import (
+	"context"
+	"fmt"
+
+	"github.com/helixml/helix/api/pkg/types"
 	"github.com/microsoft/azure-devops-go-api/azuredevops/v7"
+	"github.com/microsoft/azure-devops-go-api/azuredevops/v7/git"
 )
 
-type azureDevOpsClient struct {
+type AzureDevOpsClient struct { //nolint:revive
 	connection *azuredevops.Connection
 }
 
-func newAzureDevOpsClient(organizationURL string, personalAccessToken string) *azureDevOpsClient {
+func NewAzureDevOpsClientFromApp(app *types.App) (*AzureDevOpsClient, error) {
+	// Find credentials in the app spec
+	for _, tool := range app.Config.Helix.Assistants[0].Tools {
+		if tool.Config.AzureDevOps != nil &&
+			tool.Config.AzureDevOps.Enabled &&
+			tool.Config.AzureDevOps.OrganizationURL != "" &&
+			tool.Config.AzureDevOps.PersonalAccessToken != "" {
+			return NewAzureDevOpsClient(tool.Config.AzureDevOps.OrganizationURL, tool.Config.AzureDevOps.PersonalAccessToken), nil
+		}
+	}
+
+	return nil, fmt.Errorf("no Azure DevOps credentials found")
+}
+
+func NewAzureDevOpsClient(organizationURL string, personalAccessToken string) *AzureDevOpsClient {
 	connection := azuredevops.NewPatConnection(organizationURL, personalAccessToken)
 
-	return &azureDevOpsClient{
+	return &AzureDevOpsClient{
 		connection: connection,
 	}
+}
+
+func (c *AzureDevOpsClient) GetComments(ctx context.Context, repositoryID string, pullRequestID int, threadID int) ([]git.Comment, error) {
+	gitClient, err := git.NewClient(ctx, c.connection)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create Azure DevOps client: %w", err)
+	}
+
+	comments, err := gitClient.GetComments(ctx, git.GetCommentsArgs{
+		RepositoryId:  &repositoryID,
+		PullRequestId: &pullRequestID,
+		ThreadId:      &threadID,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return *comments, nil
 }
