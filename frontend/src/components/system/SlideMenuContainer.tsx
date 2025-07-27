33acf9237d85ce12318f086d9cc8648610db6773
nessie <nessie.molano@gmail.com>
2025-07-27 13:49:54 +0400

    styling

--- frontend/src/components/system/SlideMenuContainer.tsx
+++ frontend/src/components/system/SlideMenuContainer.tsx
@@ -1,7 +1,5 @@
 import React, { FC, ReactNode } from 'react'
 import Box from '@mui/material/Box'
-import useTheme from '@mui/material/styles/useTheme'
-import useThemeConfig from '../../hooks/useThemeConfig'
 
 interface SlideMenuContainerProps {
   children: ReactNode;
@@ -11,9 +9,6 @@ interface SlideMenuContainerProps {
 const SlideMenuContainer: FC<SlideMenuContainerProps> = ({ 
   children,  
 }) => {
-  const theme = useTheme()
-  const themeConfig = useThemeConfig()
-
   return (
     <Box
       sx={{
@@ -22,21 +17,6 @@ const SlideMenuContainer: FC<SlideMenuContainerProps> = ({
         overflow: 'auto', // Prevent container from growing beyond parent
         display: 'flex',
         flexDirection: 'column',
-        '&::-webkit-scrollbar': {
-          width: '4px',
-          borderRadius: '8px',
-          my: 2,
-        },
-        '&::-webkit-scrollbar-track': {
-          background: theme.palette.mode === 'light' ? themeConfig.lightBackgroundColor : themeConfig.darkScrollbar,
-        },
-        '&::-webkit-scrollbar-thumb': {
-          background: theme.palette.mode === 'light' ? themeConfig.lightBackgroundColor : themeConfig.darkScrollbarThumb,
-          borderRadius: '8px',
-        },
-        '&::-webkit-scrollbar-thumb:hover': {
-          background: theme.palette.mode === 'light' ? themeConfig.lightBackgroundColor : themeConfig.darkScrollbarHover,
-        },
       }}
     >
       {children}
