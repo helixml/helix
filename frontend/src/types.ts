49e34ffd9e680cfb95b9a10a9ae619cddb347375
nessie <nessie.molano@gmail.com>
2025-07-29 01:05:31 +0400

    new types

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -13,6 +13,7 @@ import {
   TypesToolWebSearchConfig,
   TypesAssistantAzureDevOps,
   TypesTrigger,
+  TypesSession,  
 } from './api/api'
 
 export type ISessionCreator = 'system' | 'user' | 'assistant'
@@ -180,100 +181,100 @@ export interface IInteractionMessage {
   content: string,
 }
 
-export interface IInteraction {
-  id: string,
-  created: string,
-  updated: string,
-  scheduled: string,
-  completed: string,
-  creator: ISessionCreator,
-  mode: ISessionMode,
-  runner: string,
-  message: string,
-  content: TypesMessageContent,
-  display_message: string,
-  progress: number,
-  files: string[],
-  finished: boolean,
-  metadata: Record<string, string>,
-  state: IInteractionState,
-  status: string,
-  error: string,
-  lora_dir: string,
-  data_prep_chunks: Record<string, IDataPrepChunk[]>,
-  data_prep_stage: ITextDataPrepStage,
-  data_prep_limited: boolean,
-  data_prep_limit: number,
-}
+// export interface IInteraction {
+//   id: string,
+//   created: string,
+//   updated: string,
+//   scheduled: string,
+//   completed: string,
+//   creator: ISessionCreator,
+//   mode: ISessionMode,
+//   runner: string,
+//   message: string,
+//   content: TypesMessageContent,
+//   display_message: string,
+//   progress: number,
+//   files: string[],
+//   finished: boolean,
+//   metadata: Record<string, string>,
+//   state: IInteractionState,
+//   status: string,
+//   error: string,
+//   lora_dir: string,
+//   data_prep_chunks: Record<string, IDataPrepChunk[]>,
+//   data_prep_stage: ITextDataPrepStage,
+//   data_prep_limited: boolean,
+//   data_prep_limit: number,
+// }
 
-export interface ISessionOrigin {
-  type: ISessionOriginType,
-  cloned_session_id?: string,
-  cloned_interaction_id?: string,
-}
+// export interface ISessionOrigin {
+//   type: ISessionOriginType,
+//   cloned_session_id?: string,
+//   cloned_interaction_id?: string,
+// }
 
-export interface ISessionConfig {
-  original_mode: ISessionMode,
-  origin: ISessionOrigin,
-  avatar: string,
-  priority: boolean,
-  document_ids: Record<string, string>,
-  document_group_id: string,
-  session_rag_results: ISessionRAGResult[],
-  manually_review_questions: boolean,
-  system_prompt: string,
-  helix_version: string,
-  eval_run_id: string,
-  eval_user_score: string,
-  eval_user_reason: string,
-  eval_manual_score: string,
-  eval_manual_reason: string,
-  eval_automatic_score: string,
-  eval_automatic_reason: string,
-  eval_original_user_prompts: string[],
-  rag_source_data_entity_id: string,
-}
+// export interface ISessionConfig {
+//   original_mode: ISessionMode,
+//   origin: ISessionOrigin,
+//   avatar: string,
+//   priority: boolean,
+//   document_ids: Record<string, string>,
+//   document_group_id: string,
+//   session_rag_results: ISessionRAGResult[],
+//   manually_review_questions: boolean,
+//   system_prompt: string,
+//   helix_version: string,
+//   eval_run_id: string,
+//   eval_user_score: string,
+//   eval_user_reason: string,
+//   eval_manual_score: string,
+//   eval_manual_reason: string,
+//   eval_automatic_score: string,
+//   eval_automatic_reason: string,
+//   eval_original_user_prompts: string[],
+//   rag_source_data_entity_id: string,
+// }
 
-export interface ISession {
-  id: string,
-  name: string,
-  created: string,
-  updated: string,
-  parent_session: string,
-  parent_app: string,
-  config: ISessionConfig,
-  mode: ISessionMode,
-  type: ISessionType,
-  model_name: string,
-  provider: string,
-  lora_dir: string,
-  interactions: IInteraction[],
-  owner: string,
-  owner_type: IOwnerType,
-}
+// export interface ISession {
+//   id: string,
+//   name: string,
+//   created: string,
+//   updated: string,
+//   parent_session: string,
+//   parent_app: string,
+//   config: ISessionConfig,
+//   mode: ISessionMode,
+//   type: ISessionType,
+//   model_name: string,
+//   provider: string,
+//   lora_dir: string,
+//   interactions: IInteraction[],
+//   owner: string,
+//   owner_type: IOwnerType,
+// }
 
-export interface IBotForm {
-  name: string,
-}
+// export interface IBotForm {
+//   name: string,
+// }
 
-export interface IBotConfig {
-}
+// export interface IBotConfig {
+// }
 
-export interface IBot {
-  id: string,
-  name: string,
-  created: string,
-  updated: string,
-  owner: string,
-  owner_type: IOwnerType,
-  config: IBotConfig,
-}
+// export interface IBot {
+//   id: string,
+//   name: string,
+//   created: string,
+//   updated: string,
+//   owner: string,
+//   owner_type: IOwnerType,
+//   config: IBotConfig,
+// }
 
 export interface IWebsocketEvent {
   type: IWebSocketEventType,
   session_id: string,
   owner: string,
-  session?: ISession,
+  session?: TypesSession,
   worker_task_response?: IWorkerTaskResponse,
   step_info?: TypesStepInfo,
 }
@@ -383,7 +384,7 @@ export interface LLMInferenceRequest {
 }
 
 export interface ISlotAttributesWorkload {
-  Session: ISession,
+  Session: TypesSession,
   LLMInferenceRequest: LLMInferenceRequest,
 }
 
