6583fbba9f6db4a030086a256c0835840a111a96
nessie <nessie.molano@gmail.com>
2025-07-29 00:49:24 +0400

    sessions react hooks

--- frontend/src/services/sessionService.ts
+++ frontend/src/services/sessionService.ts
@@ -6,6 +6,15 @@ export const sessionStepsQueryKey = (id: string) => [
   id
 ];
 
+export const getSessionQueryKey = (id: string) => [
+  "session",
+  id
+];
+
+export const listSessionsQueryKey = () => [
+  "sessions"
+];
+
 // useListSessionSteps returns the steps for a session, it includes
 // steps for all interactions in the session
 export function useListSessionSteps(sessionId: string, options?: { enabled?: boolean }) {
@@ -15,6 +24,28 @@ export function useListSessionSteps(sessionId: string, options?: { enabled?: boo
   return useQuery({
     queryKey: sessionStepsQueryKey(sessionId),
     queryFn: () => apiClient.v1SessionsStepInfoDetail(sessionId),
+    enabled: options?.enabled ?? true
+  })
+}
+
+export function useGetSession(sessionId: string, options?: { enabled?: boolean }) {
+  const api = useApi()
+  const apiClient = api.getApiClient()
+
+  return useQuery({
+    queryKey: getSessionQueryKey(sessionId),
+    queryFn: () => apiClient.v1SessionsDetail(sessionId),
+    enabled: options?.enabled ?? true
+  })
+}
+
+export function useListSessions(options?: { enabled?: boolean }) {
+  const api = useApi()
+  const apiClient = api.getApiClient()
+
+  return useQuery({
+    queryKey: listSessionsQueryKey(),
+    queryFn: () => apiClient.v1SessionsList(),
     enabled: options?.enabled ?? true
   })
 }
