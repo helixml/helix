723c06e8f61c8d79b60b8d1b89ac152b9bde2d87
nessie <nessie.molano@gmail.com>
2025-08-12 00:11:12 +0400

    tests

---
+++ api/pkg/stripe/stripe_invoices_test.go
@@ -0,0 +1,44 @@
+package stripe
+
+import (
+	"encoding/json"
+	"os"
+	"testing"
+
+	"github.com/helixml/helix/api/pkg/config"
+	"github.com/helixml/helix/api/pkg/store"
+	"github.com/helixml/helix/api/pkg/types"
+
+	"github.com/stretchr/testify/require"
+	stripe "github.com/stripe/stripe-go/v76"
+	"go.uber.org/mock/gomock"
+)
+
+func Test_handleInvoicePaymentPaidEvent(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	store := store.NewMockStore(ctrl)
+	s := NewStripe(config.Stripe{}, store)
+
+	bts, err := os.ReadFile("testdata/paid.json")
+	require.NoError(t, err)
+
+	var event stripe.Event
+	err = json.Unmarshal(bts, &event)
+	require.NoError(t, err)
+
+	// should get wallet
+	wallet := &types.Wallet{
+		ID: "123",
+	}
+	store.EXPECT().GetWalletByStripeCustomerID(gomock.Any(), "cus_SqicesZoU7LrDR").Return(wallet, nil)
+
+	// should update wallet balance
+	store.EXPECT().UpdateWalletBalance(gomock.Any(), "123", float64(20), types.TransactionMetadata{
+		TransactionType: types.TransactionTypeSubscription,
+	}).Return(wallet, nil)
+
+	err = s.handleInvoicePaymentPaidEvent(event)
+	require.NoError(t, err)
+}
