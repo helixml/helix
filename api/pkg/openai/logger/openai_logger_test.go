c927a143080fa7b9ccd017ae286538b968b76fa8
nessie <nessie.molano@gmail.com>
2025-08-12 12:21:17 +0400

    tests

--- api/pkg/openai/logger/openai_logger_test.go
+++ api/pkg/openai/logger/openai_logger_test.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/helixml/helix/api/pkg/model"
 	oai "github.com/helixml/helix/api/pkg/openai"
+	"github.com/helixml/helix/api/pkg/store"
 	"github.com/helixml/helix/api/pkg/types"
 )
 
@@ -124,6 +125,100 @@ func Test_logLLMCall_WithoutBillingLogger(t *testing.T) {
 	mockClient.EXPECT().CreateChatCompletion(ctx, *req).Return(*resp, nil)
 
 	mockClient.EXPECT().BaseURL().Return("https://api.openai.com/v1")
+
+	// Test that CreateChatCompletion works without a billing logger
+	// This will call logLLMCall internally
+	result, err := mw.CreateChatCompletion(ctx, *req)
+	require.NoError(t, err)
+	assert.Equal(t, resp.Choices[0].Message.Content, result.Choices[0].Message.Content)
+
+	// Wait for the goroutine to complete
+	mw.wg.Wait()
+}
+
+func Test_logLLMCall_WithBillingLogger_User(t *testing.T) {
+	enc, err := tokenizer.Get(tokenizer.Cl100kBase)
+	require.NoError(t, err)
+
+	ctx := context.Background()
+
+	ctrl := gomock.NewController(t)
+	mockClient := oai.NewMockClient(ctrl)
+	modelInfoProvider, err := model.NewBaseModelInfoProvider()
+	require.NoError(t, err)
+	store := store.NewMockStore(ctrl)
+
+	billingLogger, err := NewBillingLogger(store, true)
+	require.NoError(t, err)
+
+	vals := &oai.ContextValues{
+		OwnerID:       "user_123",
+		InteractionID: "interaction_123",
+	}
+
+	ctx = oai.SetContextValues(ctx, vals)
+
+	// Create a LoggingMiddleware without a billing logger
+	mw := &LoggingMiddleware{
+		defaultCodec: enc,
+		// billingLogger is nil by default
+		provider:          types.ProviderOpenAI, // Set a default provider
+		modelInfoProvider: modelInfoProvider,
+		billingLogger:     billingLogger,
+		client:            mockClient,
+	}
+
+	// Create test request and response
+	req := &openai.ChatCompletionRequest{
+		Model: "gpt-3.5-turbo",
+		Messages: []openai.ChatCompletionMessage{
+			{Role: "user", Content: "Hello, world!"},
+		},
+	}
+
+	resp := &openai.ChatCompletionResponse{
+		Choices: []openai.ChatCompletionChoice{
+			{
+				Message: openai.ChatCompletionMessage{Content: "Hello, world!"},
+			},
+		},
+		Usage: openai.Usage{
+			PromptTokens:     40000,
+			CompletionTokens: 40000,
+			TotalTokens:      80000,
+		},
+	}
+
+	// Create a mock model info provider
+
+	mockClient.EXPECT().CreateChatCompletion(ctx, *req).Return(*resp, nil)
+
+	mockClient.EXPECT().BaseURL().Return("https://api.openai.com/v1")
+
+	// We will expect a wallet get and transaction
+	store.EXPECT().GetWalletByUser(gomock.Any(), "user_123").Return(&types.Wallet{
+		ID:      "wallet_123",
+		Balance: 100,
+	}, nil)
+
+	store.EXPECT().UpdateWalletBalance(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).DoAndReturn(
+		func(_ context.Context, walletID string, amount float64, metadata types.TransactionMetadata) (*types.Wallet, error) {
+			// Validate amount
+			assert.Equal(t, -0.08, amount)
+
+			// Wallet
+			assert.Equal(t, "wallet_123", walletID)
+
+			// Validate metadata
+			assert.Equal(t, "interaction_123", metadata.InteractionID)
+			assert.Equal(t, types.TransactionTypeUsage, metadata.TransactionType)
+
+			return &types.Wallet{
+				ID:      walletID,
+				Balance: 100 - amount,
+			}, nil
+		},
+	)
 
 	// Test that CreateChatCompletion works without a billing logger
 	// This will call logLLMCall internally
