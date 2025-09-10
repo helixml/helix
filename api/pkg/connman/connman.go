46ef029038a93d865c0d56544e6269cf0e4210e9
Luke Marsden <me@lukemarsden.net>
2025-09-11 00:34:13 +0100

    wip

--- api/pkg/connman/connman.go
+++ api/pkg/connman/connman.go
@@ -17,18 +17,21 @@ var (
 // It's pretty simple now but it'll be responsible for more
 // once we support running multiple controllers
 type ConnectionManager struct {
-	deviceDialers map[string]*revdial.Dialer
-	lock          sync.RWMutex
+	deviceDialers     map[string]*revdial.Dialer
+	deviceConnections map[string]net.Conn // Raw connections for simple TCP forwarding
+	lock              sync.RWMutex
 }
 
 func New() *ConnectionManager {
 	return &ConnectionManager{
-		deviceDialers: make(map[string]*revdial.Dialer),
+		deviceDialers:     make(map[string]*revdial.Dialer),
+		deviceConnections: make(map[string]net.Conn),
 	}
 }
 
 func (m *ConnectionManager) Set(key string, conn net.Conn) {
 	m.lock.Lock()
+	// Use proper revdial.Dialer for multiplexing multiple logical connections
 	m.deviceDialers[key] = revdial.NewDialer(conn, "/revdial")
 	m.lock.Unlock()
 }
@@ -42,5 +45,6 @@ func (m *ConnectionManager) Dial(ctx context.Context, key string) (net.Conn, err
 	}
 	m.lock.RUnlock()
 
+	// Use revdial.Dialer to create a new logical connection
 	return dialer.Dial(ctx)
 }
