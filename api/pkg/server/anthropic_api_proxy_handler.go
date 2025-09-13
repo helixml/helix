b2129010ee24082187275f918241de503090d94e
nessie <nessie.molano@gmail.com>
2025-09-13 23:09:15 +0100

    set context

--- api/pkg/server/anthropic_api_proxy_handler.go
+++ api/pkg/server/anthropic_api_proxy_handler.go
@@ -1,11 +1,13 @@
 package server
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"strings"
 
 	"github.com/helixml/helix/api/pkg/anthropic"
+	oai "github.com/helixml/helix/api/pkg/openai"
 	"github.com/helixml/helix/api/pkg/store"
 	"github.com/helixml/helix/api/pkg/system"
 	"github.com/helixml/helix/api/pkg/types"
@@ -14,7 +16,17 @@ import (
 )
 
 func (s *HelixAPIServer) anthropicAPIProxyHandler(w http.ResponseWriter, r *http.Request) {
-	endpoint, err := s.getProviderEndpoint(r)
+	user := getRequestUser(r)
+	if user == nil {
+		http.Error(w, "user is required", http.StatusUnauthorized)
+		return
+	}
+
+	// Claude Code allows setting headers: https://docs.anthropic.com/en/docs/claude-code/settings
+	provider := r.Header.Get("X-Provider")
+	orgID := r.Header.Get("X-Org-ID")
+
+	endpoint, err := s.getProviderEndpoint(r.Context(), user, orgID, provider)
 	if err != nil {
 		log.Error().Err(err).Msg("failed to get provider endpoint")
 
@@ -22,14 +34,23 @@ func (s *HelixAPIServer) anthropicAPIProxyHandler(w http.ResponseWriter, r *http
 		return
 	}
 
-	var orgID string
 	if endpoint.OwnerType == types.OwnerTypeOrg {
 		orgID = endpoint.Owner
 	} else {
 		orgID = ""
 	}
 
-	hasEnoughBalance, err := s.Controller.HasEnoughBalance(r.Context(), getRequestUser(r), orgID, endpoint.BillingEnabled)
+	ctx := oai.SetContextValues(r.Context(), &oai.ContextValues{
+		OwnerID:       user.ID,
+		SessionID:     "n/a",
+		InteractionID: "n/a",
+	})
+
+	ctx = oai.SetContextOrganizationID(ctx, orgID)
+
+	r = r.WithContext(ctx)
+
+	hasEnoughBalance, err := s.Controller.HasEnoughBalance(ctx, getRequestUser(r), orgID, endpoint.BillingEnabled)
 	if err != nil {
 		log.Error().Err(err).Msg("failed to check balance")
 		http.Error(w, "Failed to check balance: "+err.Error(), http.StatusInternalServerError)
@@ -46,21 +67,13 @@ func (s *HelixAPIServer) anthropicAPIProxyHandler(w http.ResponseWriter, r *http
 	s.anthropicProxy.ServeHTTP(w, r)
 }
 
-func (s *HelixAPIServer) getProviderEndpoint(r *http.Request) (*types.ProviderEndpoint, error) {
-	user := getRequestUser(r)
-	if user == nil {
-		return nil, fmt.Errorf("user is required")
-	}
-
-	provider := r.Header.Get("X-Provider")
-	orgID := r.Header.Get("X-Org-ID")
-
+func (s *HelixAPIServer) getProviderEndpoint(ctx context.Context, user *types.User, orgID, provider string) (*types.ProviderEndpoint, error) {
 	if provider == "" {
 		return nil, fmt.Errorf("provider ID is required")
 	}
 
 	if orgID != "" {
-		_, err := s.authorizeOrgMember(r.Context(), user, orgID)
+		_, err := s.authorizeOrgMember(ctx, user, orgID)
 		if err != nil {
 			return nil, fmt.Errorf("error authorizing org member: %w", err)
 		}
@@ -75,7 +88,7 @@ func (s *HelixAPIServer) getProviderEndpoint(r *http.Request) (*types.ProviderEn
 			q.Name = provider
 		}
 
-		return s.Store.GetProviderEndpoint(r.Context(), q)
+		return s.Store.GetProviderEndpoint(ctx, q)
 	}
 
 	q := &store.GetProviderEndpointsQuery{
@@ -88,5 +101,5 @@ func (s *HelixAPIServer) getProviderEndpoint(r *http.Request) (*types.ProviderEn
 		q.Name = provider
 	}
 
-	return s.Store.GetProviderEndpoint(r.Context(), q)
+	return s.Store.GetProviderEndpoint(ctx, q)
 }
