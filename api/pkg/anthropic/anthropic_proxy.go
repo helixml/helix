5aee6c1b4ffc0adb30721e2c841c66fb6bd8de55
nessie <nessie.molano@gmail.com>
2025-09-14 18:23:28 +0100

    move

--- api/pkg/anthropic/anthropic_proxy.go
+++ api/pkg/anthropic/anthropic_proxy.go
@@ -25,36 +25,6 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-type providerEndpointContextKeyType string
-type startTimeContextKeyType string
-
-var providerEndpointContextKey providerEndpointContextKeyType = "provider_endpoint"
-var startTimeContextKey startTimeContextKeyType = "start_time"
-
-func SetRequestProviderEndpoint(req *http.Request, endpoint *types.ProviderEndpoint) *http.Request {
-	return req.WithContext(context.WithValue(req.Context(), providerEndpointContextKey, endpoint))
-}
-
-func GetRequestProviderEndpoint(ctx context.Context) *types.ProviderEndpoint {
-	endpointIntf := ctx.Value(providerEndpointContextKey)
-	if endpointIntf == nil {
-		return nil
-	}
-	return endpointIntf.(*types.ProviderEndpoint)
-}
-
-func setStartTime(req *http.Request, time time.Time) *http.Request {
-	return req.WithContext(context.WithValue(req.Context(), startTimeContextKey, time))
-}
-
-func getStartTime(req *http.Request) (time.Time, bool) {
-	startTimeIntf := req.Context().Value(startTimeContextKey)
-	if startTimeIntf == nil {
-		return time.Now(), false
-	}
-	return startTimeIntf.(time.Time), true
-}
-
 type Proxy struct {
 	store                 store.Store
 	anthropicReverseProxy *httputil.ReverseProxy
