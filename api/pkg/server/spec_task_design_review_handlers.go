5a6115ec17de764bfce7d0decdaf37202ccf19b4
Luke Marsden <luke@helix.ml>
2026-03-02 15:54:07 +0000

    fix: Declare now variable in request_changes case block
    Spec-Ref: helix-specs@d4681f38c:001412_when-moving-to-the-next

--- api/pkg/server/spec_task_design_review_handlers.go
+++ api/pkg/server/spec_task_design_review_handlers.go
@@ -10,7 +10,6 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
-	"github.com/helixml/helix/api/pkg/ptr"
 	"github.com/helixml/helix/api/pkg/services"
 	"github.com/helixml/helix/api/pkg/store"
 	"github.com/helixml/helix/api/pkg/system"
@@ -267,7 +266,8 @@ func (s *HelixAPIServer) submitDesignReview(w http.ResponseWriter, r *http.Reque
 
 	case "request_changes":
 		review.Status = types.SpecTaskDesignReviewStatusChangesRequested
-		review.RejectedAt = ptr.To(time.Now())
+		now := time.Now()
+		review.RejectedAt = &now
 		review.OverallComment = req.OverallComment
 
 		specTask.Status = types.TaskStatusSpecRevision
