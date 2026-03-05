cbdc3f05cc1dae02846919b947db0ce8a29f398b
nessie <nessie.molano@gmail.com>
2026-03-05 11:49:30 +0400

    tests

--- api/pkg/agent/optimus/optimus.go
+++ api/pkg/agent/optimus/optimus.go
@@ -20,25 +20,62 @@ func NewOptimusAgentApp(cfg OptimusConfig) *types.App {
 	appName := "Optimus (" + cfg.ProjectName + ")"
 
 	defaultAssistant := cfg.DefaultApp.Config.Helix.Assistants[0]
+	systemSettings := cfg.SystemSettings
+	if systemSettings == nil {
+		systemSettings = &types.SystemSettings{}
+	}
+	reasoningModelProvider := systemSettings.OptimusReasoningModelProvider
+	reasoningModel := systemSettings.OptimusReasoningModel
+	generationModelProvider := systemSettings.OptimusGenerationModelProvider
+	generationModel := systemSettings.OptimusGenerationModel
+	smallReasoningModelProvider := systemSettings.OptimusSmallReasoningModelProvider
+	smallReasoningModel := systemSettings.OptimusSmallReasoningModel
+	smallGenerationModelProvider := systemSettings.OptimusSmallGenerationModelProvider
+	smallGenerationModel := systemSettings.OptimusSmallGenerationModel
+
+	if reasoningModelProvider == "" {
+		reasoningModelProvider = defaultAssistant.Provider
+	}
+	if reasoningModel == "" {
+		reasoningModel = defaultAssistant.Model
+	}
+	if generationModelProvider == "" {
+		generationModelProvider = defaultAssistant.Provider
+	}
+	if generationModel == "" {
+		generationModel = defaultAssistant.Model
+	}
+	if smallReasoningModelProvider == "" {
+		smallReasoningModelProvider = defaultAssistant.Provider
+	}
+	if smallReasoningModel == "" {
+		smallReasoningModel = defaultAssistant.Model
+	}
+	if smallGenerationModelProvider == "" {
+		smallGenerationModelProvider = defaultAssistant.Provider
+	}
+	if smallGenerationModel == "" {
+		smallGenerationModel = defaultAssistant.Model
+	}
 
 	assistant := types.AssistantConfig{
 		Name:     appName,
 		Provider: defaultAssistant.Provider,
 		Model:    defaultAssistant.Model,
 
-		ReasoningModelProvider: cfg.SystemSettings.OptimusReasoningModelProvider,
-		ReasoningModel:         cfg.SystemSettings.OptimusReasoningModel,
-		ReasoningModelEffort:   cfg.SystemSettings.OptimusReasoningModelEffort,
+		ReasoningModelProvider: reasoningModelProvider,
+		ReasoningModel:         reasoningModel,
+		ReasoningModelEffort:   systemSettings.OptimusReasoningModelEffort,
 
-		GenerationModelProvider: cfg.SystemSettings.OptimusGenerationModelProvider,
-		GenerationModel:         cfg.SystemSettings.OptimusGenerationModel,
+		GenerationModelProvider: generationModelProvider,
+		GenerationModel:         generationModel,
 
-		SmallReasoningModelProvider: cfg.SystemSettings.OptimusSmallReasoningModelProvider,
-		SmallReasoningModel:         cfg.SystemSettings.OptimusSmallReasoningModel,
-		SmallReasoningModelEffort:   cfg.SystemSettings.OptimusSmallReasoningModelEffort,
+		SmallReasoningModelProvider: smallReasoningModelProvider,
+		SmallReasoningModel:         smallReasoningModel,
+		SmallReasoningModelEffort:   systemSettings.OptimusSmallReasoningModelEffort,
 
-		SmallGenerationModelProvider: cfg.SystemSettings.OptimusSmallGenerationModelProvider,
-		SmallGenerationModel:         cfg.SystemSettings.OptimusSmallGenerationModel,
+		SmallGenerationModelProvider: smallGenerationModelProvider,
+		SmallGenerationModel:         smallGenerationModel,
 
 		AgentType:    types.AgentTypeHelixAgent,
 		SystemPrompt: templates.OptimusTemplate,
