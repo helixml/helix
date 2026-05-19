b6706401fc6ba74404bc357a4edb489adccd10a9
nessie <nessie.molano@gmail.com>
2026-05-19 19:30:35 +0400

    test(api): avoid fake api key secret match

--- api/pkg/server/org_apikey_handlers_test.go
+++ api/pkg/server/org_apikey_handlers_test.go
@@ -291,7 +291,7 @@ func TestDeleteOrgAPIKey_OwnerCanDeleteAnyKey(t *testing.T) {
 
 	orgID := "org_123"
 	ownerID := "user_owner"
-	keyStr := "hl-key-abc123"
+	keyStr := "test-api-key-abc123"
 
 	expectResolveOrganizationByID(mockStore, orgID)
 
@@ -331,7 +331,7 @@ func TestDeleteOrgAPIKey_MemberCanOnlyDeleteOwnKey(t *testing.T) {
 
 	orgID := "org_123"
 	memberID := "user_member"
-	keyStr := "hl-key-abc123"
+	keyStr := "test-api-key-abc123"
 
 	expectResolveOrganizationByID(mockStore, orgID)
 
@@ -369,7 +369,7 @@ func TestDeleteOrgAPIKey_KeyFromDifferentOrgRejected(t *testing.T) {
 
 	orgID := "org_123"
 	ownerID := "user_owner"
-	keyStr := "hl-key-abc123"
+	keyStr := "test-api-key-abc123"
 
 	expectResolveOrganizationByID(mockStore, orgID)
 
